33cad4cfe8efbe81b06a5c4ced4c1815199a2ad7
Radu Ioan Fericean <radu@fericean.ro>
2012-08-03 13:18:01 +0300

    hangup without session (session is null)

--- sessionmanager/sessiondelegate.go
+++ sessionmanager/sessiondelegate.go
@@ -72,7 +72,7 @@ func (rsd *SessionDelegate) OnChannelAnswer(ev Event, s *Session) {
 }
 
 func (rsd *SessionDelegate) OnChannelHangupComplete(ev Event, s *Session) {
-	if s != nil && len(s.CallCosts) == 0 {
+	if s == nil && len(s.CallCosts) == 0 {
 		return // why would we have 0 callcosts
 	}
 	lastCC := s.CallCosts[len(s.CallCosts)-1]
