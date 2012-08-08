9984c4db6328369c02114ab7a9461d77215ca462
Radu Ioan Fericean <radu@fericean.ro>
2012-08-08 17:51:03 +0300

    exit code for build script taken from the go test commands

--- build_and_test.sh
+++ build_and_test.sh
@@ -5,6 +5,14 @@ go install github.com/cgrates/cgrates/cmd/cgr-loader
 go install github.com/cgrates/cgrates/cmd/cgr-console
 
 go test github.com/cgrates/cgrates/timespans
+ts=$?
 go test github.com/cgrates/cgrates/sessionmanager
+sm=$?
 go test github.com/cgrates/cgrates/balancer
+bl=$?
 go test github.com/cgrates/cgrates/cmd/cgr-rater
+cr=$?
+
+exit $ts || $sm || $bl || $cr
+
+
