72d2d8182ced46264639905ce26e4f329c753478
Radu Ioan Fericean <radu@fericean.ro>
2013-05-12 12:36:31 +0300

    map storage for tests

--- rater/calldesc.go
+++ rater/calldesc.go
@@ -45,9 +45,9 @@ var (
 	Logger    LoggerInterface
 	db_server = "127.0.0.1"
 	//db_server = "192.168.0.17"
-	//storageGetter, _ = NewMapStorage()
+	storageGetter, _ = NewMapStorage()
 	//storageGetter, _ = NewMongoStorage(db_server, "27017", "cgrates_test", "", "")
-	storageGetter, _ = NewRedisStorage(db_server+":6379", 11, "")
+	//storageGetter, _ = NewRedisStorage(db_server+":6379", 11, "")
 	//storageGetter, _ = NewRedigoStorage(db_server+":6379", 11, "")
 	storageLogger = storageGetter
 	debitPeriod   = 10 * time.Second
