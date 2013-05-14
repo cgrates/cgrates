6776525c97d914653dc66fdd12f0ceff1059472a
Radu Ioan Fericean <radu@fericean.ro>
2013-05-14 14:25:22 +0300

    Install packages that are dependencies of the test first

--- test.sh
+++ test.sh
@@ -1,5 +1,14 @@
 #! /usr/bin/env sh
 
+go test -i github.com/cgrates/cgrates/rater
+go test -i github.com/cgrates/cgrates/sessionmanager
+go test -i github.com/cgrates/cgrates/config
+go test -i github.com/cgrates/cgrates/cmd/cgr-rater
+go test -i github.com/cgrates/cgrates/inotify
+go test -i github.com/cgrates/cgrates/mediator
+go test -i github.com/cgrates/fsock
+
+
 go test github.com/cgrates/cgrates/rater
 ts=$?
 go test github.com/cgrates/cgrates/sessionmanager
