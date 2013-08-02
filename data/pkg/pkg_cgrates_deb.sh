10f46f348a7a1b02efd02547afcdadea26dd0baf
DanB <danb@cgrates.org>
2013-08-02 17:36:47 +0200

    Fixups pkg

--- data/pkg/pkg_cgrates_deb.sh
+++ data/pkg/pkg_cgrates_deb.sh
@@ -12,7 +12,7 @@ PKG_DIR=/tmp/cgrates_$1
 rm -rf $PKG_DIR
 cp -r skel $PKG_DIR
 chmod -R a-s $PKG_DIR/DEBIAN/
-cp ../conf/cgrates.cfg $PKG_DIR/etc/cgrates/
+cp ../conf/cgrates.cfg $PKG_DIR/etc/cgrates/cgrates.cfg
 cp -r ../../data/ $PKG_DIR/usr/share/cgrates/
 cp /usr/local/goapps/bin/cgr-* $PKG_DIR/usr/bin/
 dpkg-deb --build $PKG_DIR
