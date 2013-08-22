c2913b6fe9bb1bb41f94845b2aab87e7ddef86ec
Radu Ioan Fericean <radu@fericean.ro>
2013-08-22 14:36:27 +0300

    simplifyed code for minute buckets

--- engine/simple_serializer.go
+++ engine/simple_serializer.go
@@ -580,8 +580,7 @@ func (mb *MinuteBucket) Store() (result string, err error) {
 }
 
 func (mb *MinuteBucket) Restore(input string) error {
-	input = strings.TrimSpace(input)
-	elements := strings.Split(input, ";")
+	elements := strings.Split(strings.TrimSpace(input), ";")
 	if len(elements) > 0 && len(elements) != 5 {
 		return notEnoughElements("MinuteBucket", input)
 	}
