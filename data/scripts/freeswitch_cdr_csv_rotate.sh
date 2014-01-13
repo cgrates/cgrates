9feadce3627bfb92cb02a469c0b669ee594fce6f
DanB <danb@cgrates.org>
2014-01-13 20:57:18 +0100

    Fixups freeswitch rotate script, tutorial config

--- data/scripts/freeswitch_cdr_csv_rotate.sh
+++ data/scripts/freeswitch_cdr_csv_rotate.sh
@@ -5,8 +5,7 @@ CGR_CDRC_IN_DIR=/var/log/cgrates/cdr/cdrc/in
 
 /usr/bin/fs_cli -x "cdr_csv rotate"
 
-find $FS_CDR_CSV_DIR -maxdepth 1 -mindepth 1 -not -name *.csv -print0 | xargs -0 mv -t $CGR_CDRC_IN_DIR
+find $FS_CDR_CSV_DIR -maxdepth 1 -mindepth 1 -not -name *.csv -exec chown cgrates:cgrates '{}' \; -exec mv '{}' $CGR_CDRC_IN_DIR \;
 
 exit 0
-
 
