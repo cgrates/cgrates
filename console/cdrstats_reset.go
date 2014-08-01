0b7fcf216757b6475808c7f6a47463bd0fc56deb
Radu Ioan Fericean <radu@fericean.ro>
2014-08-01 20:27:55 +0300

    first draft for reload and reset queues

---
+++ console/cdrstats_reset.go
@@ -0,0 +1,58 @@
+/*
+Rating system designed to be used in VoIP Carriers World
+Copyright (C) 2013 ITsysCOM
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+package console
+
+import "github.com/cgrates/cgrates/apier"
+
+func init() {
+	c := &CmdCdrResetQueues{
+		name:      "cdrstats_reset",
+		rpcMethod: "CDRStatsV1.ResetQueues",
+	}
+	commands[c.Name()] = c
+	c.CommandExecuter = &CommandExecuter{c}
+}
+
+// Commander implementation
+type CmdCdrResetQueues struct {
+	name      string
+	rpcMethod string
+	rpcParams *apier.AttrReloadQueues
+	*CommandExecuter
+}
+
+func (self *CmdCdrResetQueues) Name() string {
+	return self.name
+}
+
+func (self *CmdCdrResetQueues) RpcMethod() string {
+	return self.rpcMethod
+}
+
+func (self *CmdCdrResetQueues) RpcParams() interface{} {
+	if self.rpcParams == nil {
+		self.rpcParams = &apier.AttrReloadQueues{}
+	}
+	return self.rpcParams
+}
+
+func (self *CmdCdrResetQueues) RpcResult() interface{} {
+	var s string
+	return &s
+}
