1d622cbd14bf9cc1314c234e1ff857079e057af6
DanB <danb@cgrates.org>
2014-08-18 13:41:32 +0200

    Fix package script to properly include freeswitch tutorial samples

--- pkg/pkg_cgrates_deb.sh
+++ pkg/pkg_cgrates_deb.sh
@@ -6,9 +6,9 @@ mkdir -p $WORK_DIR
 cp -r debian $WORK_DIR/debian
 cd $WORK_DIR
 git clone https://github.com/cgrates/cgrates.git src/github.com/cgrates/cgrates
-tar xvzf src/github.com/cgrates/cgrates/data/tutorials/fs_csv/freeswitch/etc/freeswitch_conf.tar.gz
+tar xvzf src/github.com/cgrates/cgrates/data/tutorials/fs_csv/freeswitch/etc/freeswitch_conf.tar.gz -C src/github.com/cgrates/cgrates/data/tutorials/fs_csv/freeswitch/etc/
 rm src/github.com/cgrates/cgrates/data/tutorials/fs_csv/freeswitch/etc/freeswitch_conf.tar.gz
-tar xvzf src/github.com/cgrates/cgrates/data/tutorials/fs_json/freeswitch/etc/freeswitch_conf.tar.gz
+tar xvzf src/github.com/cgrates/cgrates/data/tutorials/fs_json/freeswitch/etc/freeswitch_conf.tar.gz -C src/github.com/cgrates/cgrates/data/tutorials/fs_json/freeswitch/etc/
 rm src/github.com/cgrates/cgrates/data/tutorials/fs_json/freeswitch/etc/freeswitch_conf.tar.gz
 dpkg-buildpackage -us -uc
-rm -rf $WORK_DIR
+#rm -rf $WORK_DIR
