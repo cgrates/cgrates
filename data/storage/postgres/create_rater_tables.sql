d73382eb6ecca160b0aefb9c5a317917784b7a8f
d4rkstar <d4rkstar@gmail.com>
2014-10-14 06:36:14 +0200

    Update create_rater_tables.sql
    fixed the schema

--- data/storage/postgres/create_rater_tables.sql
+++ data/storage/postgres/create_rater_tables.sql
@@ -1,24 +1,25 @@
-CREATE TABLE ratingprofile IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS ratingprofile (
 	id SERIAL PRIMARY KEY,
-	fallbackkey VARCHAR(512),
+	fallbackkey VARCHAR(512)
 );
-CREATE TABLE ratingdestinations IF NOT EXISTS (
-	id SERIAL PRIMARY KEY,
-	ratingprofile INTEGER REFERENCES ratingprofile(id) ON DELETE CASCADE,
-	destination INTEGER REFERENCES destination(id) ON DELETE CASCADE
-);
-CREATE TABLE destination IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS destination (
 	id SERIAL PRIMARY KEY,
 	ratingprofile INTEGER REFERENCES ratingprofile(id) ON DELETE CASCADE,
 	name VARCHAR(512),
 	prefixes TEXT
 );
-CREATE TABLE activationprofile  IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS ratingdestinations (
+	id SERIAL PRIMARY KEY,
+	ratingprofile INTEGER REFERENCES ratingprofile(id) ON DELETE CASCADE,
+	destination INTEGER REFERENCES destination(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS activationprofile  (
 	id SERIAL PRIMARY KEY,
 	destination INTEGER REFERENCES destination(id) ON DELETE CASCADE,
 	activationtime TIMESTAMP
 );
-CREATE TABLE interval IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS interval (
 	id SERIAL PRIMARY KEY,
 	activationprofile INTEGER REFERENCES activationprofile(id) ON DELETE CASCADE,
 	years TEXT,
@@ -33,7 +34,7 @@ CREATE TABLE interval IF NOT EXISTS(
 	pricedunits FLOAT8,
 	rateincrements FLOAT8
 );
-CREATE TABLE minutebucket IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS minutebucket (
 	id SERIAL PRIMARY KEY,
 	destination INTEGER REFERENCES destination(id) ON DELETE CASCADE,
 	seconds FLOAT8,
@@ -41,18 +42,27 @@ CREATE TABLE minutebucket IF NOT EXISTS(
 	price FLOAT8,
 	percent FLOAT8
 );
-CREATE TABLE unitcounter IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS unitcounter (
 	id SERIAL PRIMARY KEY,
 	direction TEXT,
 	balance TEXT,
 	units FLOAT8
 );
-CREATE TABLE unitcounterbucket IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS unitcounterbucket (
 	id SERIAL PRIMARY KEY,
 	unitcounter INTEGER REFERENCES unitcounter(id) ON DELETE CASCADE,
 	minutebucket INTEGER REFERENCES minutebucket(id) ON DELETE CASCADE
 );
-CREATE TABLE actiontrigger IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS action (
+	id SERIAL PRIMARY KEY,
+	minutebucket INTEGER REFERENCES minutebucket(id) ON DELETE CASCADE,
+	actiontype TEXT,
+	balance TEXT,
+	direction TEXT,
+	units FLOAT8,
+	weight FLOAT8
+);
+CREATE TABLE IF NOT EXISTS actiontrigger (
 	id SERIAL PRIMARY KEY,
 	destination INTEGER REFERENCES destination(id) ON DELETE CASCADE,
 	actions INTEGER REFERENCES action(id) ON DELETE CASCADE,
@@ -62,20 +72,20 @@ CREATE TABLE actiontrigger IF NOT EXISTS(
 	weight FLOAT8,
 	executed BOOL
 );
-CREATE TABLE balance IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS balance (
 	id SERIAL PRIMARY KEY,
-	name TEXT;
+	name TEXT,
 	value FLOAT8
 );
-CREATE TABLE userbalance IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS userbalance (
 	id SERIAL PRIMARY KEY,
 	unitcounter INTEGER REFERENCES unitcounter(id) ON DELETE CASCADE,
-	minutebucket INTEGER REFERENCES minutebucket(id) ON DELETE CASCADE
+	minutebucket INTEGER REFERENCES minutebucket(id) ON DELETE CASCADE,
 	actiontriggers INTEGER REFERENCES actiontrigger(id) ON DELETE CASCADE,
 	balances INTEGER REFERENCES balance(id) ON DELETE CASCADE,
 	type TEXT
 );
-CREATE TABLE actiontiming IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS actiontiming (
 	id SERIAL PRIMARY KEY,
 	tag TEXT,
 	userbalances INTEGER REFERENCES userbalance(id) ON DELETE CASCADE,
@@ -83,19 +93,11 @@ CREATE TABLE actiontiming IF NOT EXISTS(
 	actions INTEGER REFERENCES action(id) ON DELETE CASCADE,
 	weight FLOAT8
 );
-CREATE TABLE action IF NOT EXISTS(
-	id SERIAL PRIMARY KEY,
-	minutebucket INTEGER REFERENCES minutebucket(id) ON DELETE CASCADE,
-	actiontype TEXT,
-	balance TEXT,
-	direction TEXT,
-	units FLOAT8,
-	weight FLOAT8
-);
 
-CREATE TABLE sharedgroup IF NOT EXISTS(
+
+CREATE TABLE IF NOT EXISTS sharedgroup (
 	id SERIAL PRIMARY KEY,
 	account TEXT,
 	strategy TEXT,
-	ratesubject TEXT,
+	ratesubject TEXT
 );
