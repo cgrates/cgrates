29821162a2e834dbd7957ae694a1ba1be690480a
Radu Ioan Fericean <radu@fericean.ro>
2014-11-27 19:52:11 +0200

    eliminated engine test race

--- engine/history_test.go
+++ engine/history_test.go
@@ -27,7 +27,7 @@ import (
 
 func TestHistoryRatinPlans(t *testing.T) {
 	scribe := historyScribe.(*history.MockScribe)
-	buf := scribe.BufMap[history.RATING_PROFILES_FN]
+	buf := scribe.GetBuffer(history.RATING_PROFILES_FN)
 	if !strings.Contains(buf.String(), `{"Id":"*out:vdf:0:minu","RatingPlanActivations":[{"ActivationTime":"2012-01-01T00:00:00Z","RatingPlanId":"EVENING","FallbackKeys":null}]}`) {
 		t.Error("Error in destination history content:", buf.String())
 	}
@@ -35,7 +35,7 @@ func TestHistoryRatinPlans(t *testing.T) {
 
 func TestHistoryDestinations(t *testing.T) {
 	scribe := historyScribe.(*history.MockScribe)
-	buf := scribe.BufMap[history.DESTINATIONS_FN]
+	buf := scribe.GetBuffer(history.DESTINATIONS_FN)
 	expected := `[{"Id":"ALL","Prefixes":["49","41","43"]},
 {"Id":"DST_UK_Mobile_BIG5","Prefixes":["447956"]},
 {"Id":"GERMANY","Prefixes":["49"]},
