79750c637436ac397936759d512e23cbb3e0bb62
Radu Ioan Fericean <radu@fericean.ro>
2015-03-12 13:35:06 +0200

    fix shared group member ids issue

--- engine/account.go
+++ engine/account.go
@@ -129,13 +129,15 @@ func (ub *Account) debitBalanceAction(a *Action, reset bool) error {
 		a.Balance.Value = -a.Balance.Value
 		a.Balance.dirty = true // Mark the balance as dirty since we have modified and it should be checked by action triggers
 		ub.BalanceMap[id] = append(ub.BalanceMap[id], a.Balance)
-		if a.Balance.SharedGroup != "" {
-			// add shared group member
-			sg, err := accountingStorage.GetSharedGroup(a.Balance.SharedGroup, false)
-			if err != nil || sg == nil {
-				//than problem
-				Logger.Warning(fmt.Sprintf("Could not get shared group: %v", a.Balance.SharedGroup))
-			} else {
+	}
+	if a.Balance.SharedGroup != "" {
+		// add shared group member
+		sg, err := accountingStorage.GetSharedGroup(a.Balance.SharedGroup, false)
+		if err != nil || sg == nil {
+			//than problem
+			Logger.Warning(fmt.Sprintf("Could not get shared group: %v", a.Balance.SharedGroup))
+		} else {
+			if !utils.IsSliceMember(sg.MemberIds, ub.Id) {
 				// add member and save
 				sg.MemberIds = append(sg.MemberIds, ub.Id)
 				accountingStorage.SetSharedGroup(sg)
