21853f2bb743d8eab2b60e34c90dafefb4918a0e
Radu Ioan Fericean <radu@fericean.ro>
2015-05-05 18:43:08 +0300

    working devel docker image

--- data/docker/devel/start.sh
+++ data/docker/devel/start.sh
@@ -3,27 +3,26 @@ export GOPATH=/root/code
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
 /etc/init.d/mysql start
-/usr/share/cgrates/tutorials/fs_csv/freeswitch/etc/init.d/freeswitch start
-mysqladmin -u root password CGRateS.org
+/etc/init.d/postgresql start
+/etc/init.d/redis-server start
 
 # create a link to data dir
 ln -s /root/code/src/github.com/cgrates/cgrates/data /usr/share/cgrates
-
-# expand freeswitch json conf
-tar xzf /usr/share/cgrates/tutorials/fs_json/freeswitch/etc/freeswitch_conf.tar.gz
-    
-# expand freeswitch csv 
-tar xzf /usr/share/cgrates/tutorials/fs_csv/freeswitch/etc/freeswitch_conf.tar.gz
-    
 # create link to cgrates dir
 ln -s /root/code/src/github.com/cgrates/cgrates /root/cgr
+
+cd /usr/share/cgrates/storage/mysql && ./setup_cgr_db.sh root CGRateS.org
+cd /usr/share/cgrates/storage/postgres && ./setup_cgr_db.sh
+
+# build and install cgrates
+/root/cgr/update_external_libs.sh
+go install github.com/cgrates/cgrates
 
 # create cgr-engine link
 ln -s /root/code/bin/cgr-engine /usr/bin/cgr-engine
 
-cd /usr/share/cgrates/storage/mysql; ./setup_cgr_db.sh root CGRateS.org localhost
-cd /
+# expand freeswitch conf
+cd /usr/share/cgrates/tutorials/fs_evsock/freeswitch/etc/ && tar xzf freeswitch_conf.tar.gz       
 
-cgr-engine -config /root/cgr/data/conf/cgrates.cfg &
 
 bash --rcfile /root/.bashrc
