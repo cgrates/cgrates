5e20dadfbaf337ed0c6e609fc372bc1d1c366a00
Radu Ioan Fericean <radu@fericean.ro>
2015-08-03 13:23:05 +0300

    make sure account is not cached

--- engine/rateinterval.go
+++ engine/rateinterval.go
@@ -376,8 +376,7 @@ func (il RateIntervalList) Swap(i, j int) {
 
 // we need higher weights earlyer in the list
 func (il RateIntervalList) Less(j, i int) bool {
-	return il[i].Weight < il[j].Weight ||
-		il[i].Timing.StartTime > il[j].Timing.StartTime
+	return il[i].Weight < il[j].Weight //|| il[i].Timing.StartTime > il[j].Timing.StartTime
 }
 
 func (il RateIntervalList) Sort() {
