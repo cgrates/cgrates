36d8bac0b688a8398e0973750d758b5cae8f4c2e
DanB <danb@cgrates.org>
2015-08-20 15:20:10 +0200

    Adding LogCallCost method to CdrsV1 RPC

--- apier/v1/cdrsv1.go
+++ apier/v1/cdrsv1.go
@@ -72,3 +72,11 @@ func (self *CdrsV1) RateCdrs(attrs utils.AttrRateCdrs, reply *string) error {
 	*reply = utils.OK
 	return nil
 }
+
+func (self *CdrsV1) LogCallCost(ccl *engine.CallCostLog, reply *string) error {
+	if err := self.CdrSrv.LogCallCost(ccl); err != nil {
+		return utils.NewErrServerError(err)
+	}
+	*reply = utils.OK
+	return nil
+}
