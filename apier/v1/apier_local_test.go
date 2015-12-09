d493952d541cc0fe11aa2cad18ea6a8760a2f0c5
Radu Ioan Fericean <radu@fericean.ro>
2015-12-09 19:21:54 +0200

    fix local test

--- apier/v1/apier_local_test.go
+++ apier/v1/apier_local_test.go
@@ -735,8 +735,8 @@ func TestApierSetRatingProfile(t *testing.T) {
 	} else if reply != "OK" {
 		t.Error("Calling ApierV1.SetRatingProfile got reply: ", reply)
 	}
-	// Calling the second time should raise EXISTS
-	if err := rater.Call("ApierV1.SetRatingProfile", rpf, &reply); err == nil || err.Error() != "EXISTS" {
+	// Calling the second time should not raise EXISTS
+	if err := rater.Call("ApierV1.SetRatingProfile", rpf, &reply); err != nil {
 		t.Error("Unexpected result on duplication: ", err.Error())
 	}
 	time.Sleep(10 * time.Millisecond) // Give time for cache reload
