b6c4b4842cd747e18a2687e8a85acdd6ad909f15
Radu Ioan Fericean <radu@fericean.ro>
2016-03-11 21:49:57 +0200

    started limiter component

--- engine/models.go
+++ engine/models.go
@@ -343,6 +343,7 @@ type TpUser struct {
 	AttributeName  string  `index:"3" re:""`
 	AttributeValue string  `index:"4" re:""`
 	Weight         float64 `index:"5" re:""`
+	CreatedAt      time.Time
 }
 
 func (tu *TpUser) GetId() string {
@@ -395,6 +396,20 @@ func (ta *TpAlias) SetId(id string) error {
 
 func (ta *TpAlias) GetId() string {
 	return utils.ConcatenatedKey(ta.Direction, ta.Tenant, ta.Category, ta.Account, ta.Subject, ta.Context)
+}
+
+type TpLimiter struct {
+	Id             int64
+	Tpid           string
+	LimiterID      string    `index:"0" re:""`
+	ResourceID     string    `index:"1" re:""`
+	Filter         string    `index:"2" re:""`
+	TTL            string    `index:"3" re:""`
+	TimingIDs      string    `index:"4" re:""`
+	ActivationTime string    `index:"5" re:""`
+	Limit          float64   `index:"6" re:""`
+	ActionTriggers string    `index:"7" re:""`
+	CreatedAt      time.Time `index:"8" re:""`
 }
 
 type TBLCDRs struct {
