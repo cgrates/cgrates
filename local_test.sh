393d893ef7505368d0fec5bdae3f21d68756008f
Radu Ioan Fericean <radu@fericean.ro>
2016-03-25 15:11:02 +0200

    make tests use glide novendor

--- local_test.sh
+++ local_test.sh
@@ -1,32 +1,5 @@
 #! /usr/bin/env sh
 ./test.sh
 gen=$?
-echo 'go test github.com/cgrates/cgrates/apier/v1 -local'
-go test github.com/cgrates/cgrates/apier/v1 -local
-ap1=$?
-echo 'go test github.com/cgrates/cgrates/apier/v2 -local'
-go test github.com/cgrates/cgrates/apier/v2 -local
-ap2=$?
-echo 'go test github.com/cgrates/cgrates/engine -local -integration'
-go test github.com/cgrates/cgrates/engine -local -integration
-en=$?
-echo 'go test github.com/cgrates/cgrates/cdrc -local'
-go test github.com/cgrates/cgrates/cdrc -local
-cdrc=$?
-echo 'go test github.com/cgrates/cgrates/config -local'
-go test github.com/cgrates/cgrates/config -local
-cfg=$?
-echo 'go test github.com/cgrates/cgrates/utils -local'
-go test github.com/cgrates/cgrates/utils -local
-utl=$?
-echo 'go test github.com/cgrates/cgrates/general_tests -local -integration'
-go test github.com/cgrates/cgrates/general_tests -local -integration
-gnr=$?
-echo 'go test github.com/cgrates/cgrates/agents -integration'
-go test github.com/cgrates/cgrates/agents -integration
-agts=$?
-echo 'go test github.com/cgrates/cgrates/sessionmanager -integration'
-go test github.com/cgrates/cgrates/sessionmanager -integration
-smg=$?
-
-exit $gen && $ap1 && $ap2 && $en && $cdrc && $cfg && $utl && $gnr && $agts && $smg
+go test -local -integration $(glide novendor)
+exit $gen && $?
