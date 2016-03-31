c1837564633e348ed1784d1e3e11a16eb94d9ad3
Radu Ioan Fericean <radu@fericean.ro>
2016-03-31 22:03:29 +0300

    fix gob error

--- apier/v1/cdrsv1.go
+++ apier/v1/cdrsv1.go
@@ -59,8 +59,8 @@ func (self *CdrsV1) RateCdrs(attrs utils.AttrRateCdrs, reply *string) error {
 	return nil
 }
 
-func (self *CdrsV1) StoreSMCost(smCost *engine.SMCost, reply *string) error {
-	if err := self.CdrSrv.StoreSMCost(smCost, false); err != nil {
+func (self *CdrsV1) StoreSMCost(attr *engine.AttrCDRSStoreSMCost, reply *string) error {
+	if err := self.CdrSrv.StoreSMCost(attr.Cost, attr.CheckDuplicate); err != nil {
 		return utils.NewErrServerError(err)
 	}
 	*reply = utils.OK
