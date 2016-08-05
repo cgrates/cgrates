3f9cdf68d11ab5464adb2585f7fb37ba87defe31
DanB <danb@cgrates.org>
2016-08-05 17:33:21 +0200

    Do not load TP over the docker

--- data/docker/osips_training/start.sh
+++ data/docker/osips_training/start.sh
@@ -11,7 +11,7 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf /etc/mysql/my.cnf
 cd /usr/share/cgrates/storage/mysql && ./setup_cgr_db.sh root CGRateS.org
 
 # load tariff plan data
-cd /root/cgr/data/tariffplans/osips_training; cgr-loader
+#cd /root/cgr/data/tariffplans/osips_training; cgr-loader
 
 cd /root/cgr
 DISABLE_AUTO_UPDATE="true" zsh
