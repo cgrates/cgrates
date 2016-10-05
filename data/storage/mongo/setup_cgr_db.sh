d2f2a556736adda61fbbde00905dc556254593c7
Razvan Crainea <razvan@opensips.org>
2016-10-05 15:05:50 +0300

    add support for mongo 2.4
    createUser is only available in 2.6, so for older mongo versions we need
    to use deprecated addUser

--- data/storage/mongo/setup_cgr_db.sh
+++ data/storage/mongo/setup_cgr_db.sh
@@ -1,7 +1,29 @@
 #! /usr/bin/env sh
 
+USER="cgrates"
+PASSWORD="CGRateS.org"
 
-mongo --quiet create_user.js
+MONGO_VERSION=$(mongo --version | sed 's/.* \([0-9]\.[0-9\]\).*/\1/')
+MONGO_MAJOR=$(echo $MONGO_VERSION | cut -d '.' -f 1)
+MONGO_MINOR=$(echo $MONGO_VERSION | cut -d '.' -f 2)
+
+# up to mongo 2.6 create user is done by addUser
+if [ $MONGO_MAJOR -eq 2 -a $MONGO_MINOR -lt 6 ]; then
+	FUNC=addUser
+	ROLES="[ 'userAdminAnyDatabase' ]"
+else
+	FUNC=createUser
+	ROLES="[{ role: 'userAdminAnyDatabase', db: 'admin' }]"
+fi
+mongo --quiet --eval "
+db = db.getSiblingDB('admin');
+db.$FUNC(
+  {
+    user: '$USER',
+    pwd: '$PASSWORD',
+    roles: $ROLES
+  }
+)"
 cu=$?
 
 if [ $cu = 0 ]; then
