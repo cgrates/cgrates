6a0044b063659e74ee7d3749858daf0470259609
DanB <danb@cgrates.org>
2016-10-12 19:46:00 +0200

    ApierV2.SetAccountActionTriggers, tests for APIerV2

---
+++ apier/v2/apierv2_it_test.go
@@ -0,0 +1,216 @@
+/*
+Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
+Copyright (C) ITsysCOM GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+package v2
+
+import (
+	"flag"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+	"path"
+	"testing"
+
+	"github.com/cgrates/cgrates/apier/v1"
+	"github.com/cgrates/cgrates/config"
+	"github.com/cgrates/cgrates/engine"
+	"github.com/cgrates/cgrates/utils"
+)
+
+var testIT = flag.Bool("integration", false, "Perform the tests only on local test environment, not by default.")
+
+var apierCfgPath string
+var apierCfg *config.CGRConfig
+var apierRPC *rpc.Client
+
+func TestApierV2itLoadConfig(t *testing.T) {
+	if !*testIT {
+		return
+	}
+	apierCfgPath = path.Join(*dataDir, "conf", "samples", "tutmysql")
+	if apierCfg, err = config.NewCGRConfigFromFolder(tpCfgPath); err != nil {
+		t.Error(err)
+	}
+}
+
+// Remove data in both rating and accounting db
+func TestApierV2itResetDataDb(t *testing.T) {
+	if !*testIT {
+		return
+	}
+	if err := engine.InitDataDb(apierCfg); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Wipe out the cdr database
+func TestApierV2itResetStorDb(t *testing.T) {
+	if !*testIT {
+		return
+	}
+	if err := engine.InitStorDb(apierCfg); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Start CGR Engine
+func TestApierV2itStartEngine(t *testing.T) {
+	if !*testIT {
+		return
+	}
+	if _, err := engine.StopStartEngine(apierCfgPath, 200); err != nil { // Mongo requires more time to start
+		t.Fatal(err)
+	}
+}
+
+// Connect rpc client to rater
+func TestApierV2itRpcConn(t *testing.T) {
+	if !*testIT {
+		return
+	}
+	apierRPC, err = jsonrpc.Dial("tcp", apierCfg.RPCJSONListen) // We connect over JSON so we can also troubleshoot if needed
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestApierV2itAddBalance(t *testing.T) {
+	if !*testIT {
+		return
+	}
+	attrs := &utils.AttrSetBalance{
+		Tenant:      "cgrates.org",
+		Account:     "dan",
+		BalanceType: utils.MONETARY,
+		BalanceID:   utils.StringPointer(utils.META_DEFAULT),
+		Value:       utils.Float64Pointer(5.0),
+		Weight:      utils.Float64Pointer(10.0),
+	}
+	var reply string
+	if err := apierRPC.Call("ApierV2.SetBalance", attrs, &reply); err != nil {
+		t.Fatal(err)
+	}
+	var acnt engine.Account
+	if err := apierRPC.Call("ApierV2.GetAccount", &utils.AttrGetAccount{Tenant: "cgrates.org", Account: "dan"}, &acnt); err != nil {
+		t.Error(err)
+	} else if acnt.BalanceMap[utils.MONETARY][0].Value != 5.0 {
+		t.Errorf("Unexpected balance received: %+v", acnt.BalanceMap[utils.MONETARY][0])
+	}
+}
+
+func TestApierV2itSetAction(t *testing.T) {
+	if !*testIT {
+		return
+	}
+	attrs := utils.AttrSetActions{ActionsId: "DISABLE_ACCOUNT", Actions: []*utils.TPAction{
+		&utils.TPAction{Identifier: engine.DISABLE_ACCOUNT, Weight: 10.0},
+	}}
+	var reply string
+	if err := apierRPC.Call("ApierV2.SetActions", attrs, &reply); err != nil {
+		t.Error(err)
+	}
+	var acts map[string]engine.Actions
+	if err := apierRPC.Call("ApierV2.GetActions", AttrGetActions{ActionIDs: []string{attrs.ActionsId}}, &acts); err != nil {
+		t.Error(err)
+	} else if len(acts) != 1 {
+		t.Errorf("Received actions: %+v", acts)
+	}
+}
+
+func TestApierV2itSetAccountActionTriggers(t *testing.T) {
+	if !*testIT {
+		return
+	}
+	attrs := AttrSetAccountActionTriggers{
+		Tenant:         "cgrates.org",
+		Account:        "dan",
+		GroupID:        utils.StringPointer("MONITOR_MAX_BALANCE"),
+		ThresholdType:  utils.StringPointer(utils.TRIGGER_MAX_BALANCE),
+		ThresholdValue: utils.Float64Pointer(50),
+		BalanceID:      utils.StringPointer(utils.META_DEFAULT),
+		BalanceType:    utils.StringPointer(utils.MONETARY),
+		ActionsID:      utils.StringPointer("DISABLE_ACCOUNT"),
+	}
+	var reply string
+	if err := apierRPC.Call("ApierV2.SetAccountActionTriggers", attrs, &reply); err != nil {
+		t.Error(err)
+	}
+	var ats engine.ActionTriggers
+	if err := apierRPC.Call("ApierV2.GetAccountActionTriggers", v1.AttrAcntAction{Tenant: "cgrates.org", Account: "dan"}, &ats); err != nil {
+		t.Error(err)
+	} else if len(ats) != 1 || ats[0].ID != *attrs.GroupID || ats[0].ThresholdValue != 50.0 {
+		t.Errorf("Received: %+v", ats)
+	}
+	attrs.ThresholdValue = utils.Float64Pointer(55) // Change the threshold
+	if err := apierRPC.Call("ApierV2.SetAccountActionTriggers", attrs, &reply); err != nil {
+		t.Error(err)
+	}
+	if err := apierRPC.Call("ApierV2.GetAccountActionTriggers", v1.AttrAcntAction{Tenant: "cgrates.org", Account: "dan"}, &ats); err != nil {
+		t.Error(err)
+	} else if len(ats) != 1 || ats[0].ID != *attrs.GroupID || ats[0].ThresholdValue != 55.0 {
+		t.Errorf("Received: %+v", ats)
+	}
+}
+
+func TestApierV2itFraudMitigation(t *testing.T) {
+	if !*testIT {
+		return
+	}
+	attrs := &utils.AttrSetBalance{
+		Tenant:      "cgrates.org",
+		Account:     "dan",
+		BalanceType: utils.MONETARY,
+		BalanceID:   utils.StringPointer(utils.META_DEFAULT),
+		Value:       utils.Float64Pointer(60.0),
+		Weight:      utils.Float64Pointer(10.0),
+	}
+	var reply string
+	if err := apierRPC.Call("ApierV2.SetBalance", attrs, &reply); err != nil {
+		t.Fatal(err)
+	}
+	var acnt engine.Account
+	if err := apierRPC.Call("ApierV2.GetAccount", &utils.AttrGetAccount{Tenant: "cgrates.org", Account: "dan"}, &acnt); err != nil {
+		t.Error(err)
+	} else if len(acnt.BalanceMap) != 1 || acnt.BalanceMap[utils.MONETARY][0].Value != 60.0 {
+		t.Errorf("Unexpected balance received: %+v", acnt.BalanceMap[utils.MONETARY][0])
+		//} else if !acnt.Disabled {
+		//	t.Fatalf("Received account: %+v", acnt)
+	}
+	attrSetAcnt := AttrSetAccount{
+		Tenant:   "cgrates.org",
+		Account:  "dan",
+		Disabled: utils.BoolPointer(false),
+	}
+	if err := apierRPC.Call("ApierV2.SetAccount", attrSetAcnt, &reply); err != nil {
+		t.Fatal(err)
+	}
+	if err := apierRPC.Call("ApierV2.GetAccount", &utils.AttrGetAccount{Tenant: "cgrates.org", Account: "dan"}, &acnt); err != nil {
+		t.Error(err)
+	} else if len(acnt.BalanceMap) != 1 || acnt.BalanceMap[utils.MONETARY][0].Value != 60.0 {
+		t.Errorf("Unexpected balance received: %+v", acnt.BalanceMap[utils.MONETARY][0])
+	} else if acnt.Disabled {
+		t.Fatalf("Received account: %+v", acnt)
+	}
+}
+
+func TestApierV2itKillEngine(t *testing.T) {
+	if !*testIT {
+		return
+	}
+	if err := engine.KillEngine(delay); err != nil {
+		t.Error(err)
+	}
+}
---
+++ apier/v2/triggers.go
@@ -0,0 +1,196 @@
+/*
+Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
+Copyright (C) ITsysCOM GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+package v2
+
+import (
+	"errors"
+
+	"github.com/cgrates/cgrates/engine"
+	"github.com/cgrates/cgrates/utils"
+)
+
+type AttrSetAccountActionTriggers struct {
+	Tenant                string
+	Account               string
+	GroupID               *string
+	UniqueID              *string
+	ThresholdType         *string
+	ThresholdValue        *float64
+	Recurrent             *bool
+	Executed              *bool
+	MinSleep              *string
+	ExpirationDate        *string
+	ActivationDate        *string
+	BalanceID             *string
+	BalanceType           *string
+	BalanceDirections     *[]string
+	BalanceDestinationIds *[]string
+	BalanceWeight         *float64
+	BalanceExpirationDate *string
+	BalanceTimingTags     *[]string
+	BalanceRatingSubject  *string
+	BalanceCategories     *[]string
+	BalanceSharedGroups   *[]string
+	BalanceBlocker        *bool
+	BalanceDisabled       *bool
+	MinQueuedItems        *int
+	ActionsID             *string
+}
+
+func (attr *AttrSetAccountActionTriggers) UpdateActionTrigger(at *engine.ActionTrigger, timezone string) (updated bool, err error) {
+	if at == nil {
+		return false, errors.New("Empty ActionTrigger")
+	}
+	if at.ID == "" { // New AT, update it's data
+		if missing := utils.MissingStructFields(attr, []string{"GroupID", "ThresholdType", "ThresholdValue"}); len(missing) != 0 {
+			return false, utils.NewErrMandatoryIeMissing(missing...)
+		}
+		at.ID = *attr.GroupID
+		if attr.UniqueID != nil {
+			at.UniqueID = *attr.UniqueID
+		}
+	}
+	if attr.GroupID != nil && *attr.GroupID != at.ID {
+		return
+	}
+	if attr.UniqueID != nil && *attr.UniqueID != at.UniqueID {
+		return
+	}
+	// at matches
+	updated = true
+	if attr.ThresholdType != nil {
+		at.ThresholdType = *attr.ThresholdType
+	}
+	if attr.ThresholdValue != nil {
+		at.ThresholdValue = *attr.ThresholdValue
+	}
+	if attr.Recurrent != nil {
+		at.Recurrent = *attr.Recurrent
+	}
+	if attr.Executed != nil {
+		at.Executed = *attr.Executed
+	}
+	if attr.MinSleep != nil {
+		if at.MinSleep, err = utils.ParseDurationWithSecs(*attr.MinSleep); err != nil {
+			return
+		}
+	}
+	if attr.ExpirationDate != nil {
+		if at.ExpirationDate, err = utils.ParseTimeDetectLayout(*attr.ExpirationDate, timezone); err != nil {
+			return
+		}
+	}
+	if attr.ActivationDate != nil {
+		if at.ActivationDate, err = utils.ParseTimeDetectLayout(*attr.ActivationDate, timezone); err != nil {
+			return
+		}
+	}
+	if at.Balance == nil {
+		at.Balance = &engine.BalanceFilter{}
+	}
+	if attr.BalanceID != nil {
+		at.Balance.ID = attr.BalanceID
+	}
+	if attr.BalanceType != nil {
+		at.Balance.Type = attr.BalanceType
+	}
+	if attr.BalanceDirections != nil {
+		at.Balance.Directions = utils.StringMapPointer(utils.NewStringMap(*attr.BalanceDirections...))
+	}
+	if attr.BalanceDestinationIds != nil {
+		at.Balance.DestinationIDs = utils.StringMapPointer(utils.NewStringMap(*attr.BalanceDestinationIds...))
+	}
+	if attr.BalanceWeight != nil {
+		at.Balance.Weight = attr.BalanceWeight
+	}
+	if attr.BalanceExpirationDate != nil {
+		balanceExpTime, err := utils.ParseDate(*attr.BalanceExpirationDate)
+		if err != nil {
+			return false, err
+		}
+		at.Balance.ExpirationDate = &balanceExpTime
+	}
+	if attr.BalanceTimingTags != nil {
+		at.Balance.TimingIDs = utils.StringMapPointer(utils.NewStringMap(*attr.BalanceTimingTags...))
+	}
+	if attr.BalanceRatingSubject != nil {
+		at.Balance.RatingSubject = attr.BalanceRatingSubject
+	}
+	if attr.BalanceCategories != nil {
+		at.Balance.Categories = utils.StringMapPointer(utils.NewStringMap(*attr.BalanceCategories...))
+	}
+	if attr.BalanceSharedGroups != nil {
+		at.Balance.SharedGroups = utils.StringMapPointer(utils.NewStringMap(*attr.BalanceSharedGroups...))
+	}
+	if attr.BalanceBlocker != nil {
+		at.Balance.Blocker = attr.BalanceBlocker
+	}
+	if attr.BalanceDisabled != nil {
+		at.Balance.Disabled = attr.BalanceDisabled
+	}
+	if attr.MinQueuedItems != nil {
+		at.MinQueuedItems = *attr.MinQueuedItems
+	}
+	if attr.ActionsID != nil {
+		at.ActionsID = *attr.ActionsID
+	}
+	return
+}
+
+// SetAccountActionTriggers Updates or Creates ActionTriggers for an Account
+func (self *ApierV2) SetAccountActionTriggers(attr AttrSetAccountActionTriggers, reply *string) error {
+	if missing := utils.MissingStructFields(&attr, []string{"Tenant", "Account"}); len(missing) != 0 {
+		return utils.NewErrMandatoryIeMissing(missing...)
+	}
+	accID := utils.AccountKey(attr.Tenant, attr.Account)
+	var account *engine.Account
+	_, err := engine.Guardian.Guard(func() (interface{}, error) {
+		if acc, err := self.AccountDb.GetAccount(accID); err == nil {
+			account = acc
+		} else {
+			return 0, err
+		}
+		var foundOne bool
+		for _, at := range account.ActionTriggers {
+			if updated, err := attr.UpdateActionTrigger(at, self.Config.DefaultTimezone); err != nil {
+				return 0, err
+			} else if updated && !foundOne {
+				foundOne = true
+			}
+		}
+		if !foundOne { // Did not find one to update, create a new AT
+			at := new(engine.ActionTrigger)
+			if updated, err := attr.UpdateActionTrigger(at, self.Config.DefaultTimezone); err != nil {
+				return 0, err
+			} else if updated { // Adding a new AT
+				account.ActionTriggers = append(account.ActionTriggers, at)
+			}
+		}
+		account.ExecuteActionTriggers(nil)
+		if err := self.AccountDb.SetAccount(account); err != nil {
+			return 0, err
+		}
+		return 0, nil
+	}, 0, accID)
+	if err != nil {
+		*reply = err.Error()
+		return err
+	}
+	*reply = utils.OK
+	return nil
+}
