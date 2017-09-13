d9b4bbb60454bb0cb13b90e4c3bae0cb377eee4d
edwardro22 <edwardro22@gmail.com>
2017-09-13 14:27:42 +0000

    Small fixes

--- data/storage/migrator/mysql_cdr_migration.sql
+++ data/storage/migrator/mysql_cdr_migration.sql
@@ -56,8 +56,3 @@ END //
 DELIMITER ;
 
 CALL cgrates.migration();
-
-ALTER TABLE cdrs CHANGE COLUMN `usage` `usage_old` DECIMAL(30,9);
-ALTER TABLE cdrs ADD `usage` DECIMAL(30);
-UPDATE cdrs SET `usage` = `usage_old` * 1000000000 WHERE usage_old IS NOT NULL;
-ALTER TABLEX cdrs DROP COLUMN usage_old;
--- data/storage/migrator/mysql_tables_update.sql
+++ data/storage/migrator/mysql_tables_update.sql
@@ -72,3 +72,8 @@ CREATE TABLE versions (
 
 
 
+
+ALTER TABLE cdrs CHANGE COLUMN `usage` `usage_old` DECIMAL(30,9);
+ALTER TABLE cdrs ADD `usage` DECIMAL(30);
+UPDATE cdrs SET `usage` = `usage_old` * 1000000000 WHERE usage_old IS NOT NULL;
+ALTER TABLE cdrs DROP COLUMN usage_old;
