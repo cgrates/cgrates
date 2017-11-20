8bca0a2d64aaf92cd3158aa145ec88293adf81bc
DanB <danb@cgrates.org>
2017-11-20 09:55:23 +0100

    Missing import

---
+++ engine/lcrs.go
@@ -0,0 +1,37 @@
+/*
+Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
+Copyright (C) ITsysCOM GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+package engine
+
+import (
+	"github.com/cgrates/cgrates/utils"
+)
+
+// LCRProfile represents the configuration of a  LCR profile
+type LCRProfile struct {
+	Tenant             string
+	ID                 string // LCR Profile ID
+	FilterIDs          []string
+	ActivationInterval *utils.ActivationInterval // Activation interval
+	Strategy           string                    // LCR Strategy used when computing
+	StrategyParams     []string
+	SupplierID         string
+	RatingPlanIDs      []string // RatingPlans used when computing price
+	StatIDs            []string // StatProfiles queried in case of QoS based strategies
+	Weight             float64
+}
