b6a1f0a1db6efde8e020f86a71d4c3e14b4773a0
DanB <danb@cgrates.org>
2018-02-09 20:11:22 +0100

    KamailioAgent - Usage value mangling instead of header

--- agents/kamevent.go
+++ agents/kamevent.go
@@ -116,7 +116,7 @@ func (kev KamEvent) AsMapStringInterface() (mp map[string]interface{}) {
 	mp = make(map[string]interface{})
 	for k, v := range kev {
 		if k == utils.Usage {
-			k += "s" // mark the Usage as seconds
+			v += "s" // mark the Usage as seconds
 		}
 		if !utils.IsSliceMember(kamReservedFields, k) { // reserved attributes not getting into event
 			mp[k] = v
