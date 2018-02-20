3b9d283a1e546b3bd7b62848ed7c04a5c4ca18ad
TeoV <teofil.voivozeanu@itsyscom.com>
2018-02-20 10:49:25 -0500

    Update migrator for sessions costs

--- migrator/sessions_costs.go
+++ migrator/sessions_costs.go
@@ -19,8 +19,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 package migrator
 
 import (
-	"errors"
+	"database/sql"
 	"fmt"
+	"log"
 
 	"github.com/cgrates/cgrates/engine"
 	"github.com/cgrates/cgrates/utils"
@@ -41,7 +42,37 @@ func (m *Migrator) migrateSessionsCosts() (err error) {
 			"version number is not defined for SessionsCosts model")
 	}
 	if vrs[utils.SessionsCosts] < 2 {
-		return errors.New("Wrong version. Please use <cgr-migrator -migrate=*set_versions>")
+		var isPostGres bool
+		var storSQL *sql.DB
+		switch m.storDBType {
+		case utils.MYSQL:
+			isPostGres = false
+			storSQL = m.storDB.(*engine.SQLStorage).Db
+		case utils.POSTGRES:
+			isPostGres = true
+			storSQL = m.storDB.(*engine.SQLStorage).Db
+		default:
+			return utils.NewCGRError(utils.Migrator,
+				utils.MandatoryIEMissingCaps,
+				utils.UnsupportedDB,
+				fmt.Sprintf("unsupported database type: <%s>", m.storDBType))
+		}
+		qry := "RENAME TABLE sm_costs TO sessions_costs;"
+		if isPostGres {
+			qry = "ALTER TABLE sm_costs RENAME TO sessions_costs"
+		}
+		if _, err := storSQL.Exec(qry); err != nil {
+			log.Print(err)
+			return err
+		}
+		m.stats[utils.SessionsCosts] = 2
+		vrs = engine.Versions{utils.SessionsCosts: engine.CurrentStorDBVersions()[utils.SessionsCosts]}
+		if err := m.storDB.SetVersions(vrs, false); err != nil {
+			return utils.NewCGRError(utils.Migrator,
+				utils.ServerErrorCaps,
+				err.Error(),
+				fmt.Sprintf("error: <%s> when updating SessionsCosts version into StorDB", err.Error()))
+		}
 	}
 	return nil
 }
