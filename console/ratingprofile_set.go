759d82d36aecb67ea20645a1c76a0545089cf7b8
Carlos Cruz <carlos@irontec.com>
2018-04-24 18:46:56 +0200

    Fix Overwrite for ratingprofile_set console command
    This commit fixes rpcParams type so that Overwrite attribute gets
    usable through ratingprofile_set command.

--- console/ratingprofile_set.go
+++ console/ratingprofile_set.go
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 package console
 
-import "github.com/cgrates/cgrates/utils"
+import "github.com/cgrates/cgrates/apier/v1"
 
 func init() {
 	c := &CmdSetRatingProfile{
@@ -33,7 +33,7 @@ func init() {
 type CmdSetRatingProfile struct {
 	name      string
 	rpcMethod string
-	rpcParams *utils.TPRatingProfile
+	rpcParams *v1.AttrSetRatingProfile
 	rpcResult string
 	*CommandExecuter
 }
@@ -48,7 +48,7 @@ func (self *CmdSetRatingProfile) RpcMethod() string {
 
 func (self *CmdSetRatingProfile) RpcParams(reset bool) interface{} {
 	if reset || self.rpcParams == nil {
-		self.rpcParams = &utils.TPRatingProfile{}
+		self.rpcParams = &v1.AttrSetRatingProfile{}
 	}
 	return self.rpcParams
 }
