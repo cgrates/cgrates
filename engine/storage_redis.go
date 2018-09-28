fc5be77eb6eb93b40044ad0c659f5971bb2c8f41
TeoV <teofil.voivozeanu@itsyscom.com>
2018-09-28 10:13:52 -0400

    Refactor filterIndexes in mongo

--- engine/storage_redis.go
+++ engine/storage_redis.go
@@ -1370,6 +1370,7 @@ func (rs *RedisStorage) RemoveTimingDrv(id string) (err error) {
 }
 
 //GetFilterIndexesDrv retrieves Indexes from dataDB
+//filterType is used togheter with fieldName:Val
 func (rs *RedisStorage) GetFilterIndexesDrv(cacheID, itemIDPrefix, filterType string,
 	fldNameVal map[string]string) (indexes map[string]utils.StringMap, err error) {
 	mp := make(map[string]string)
