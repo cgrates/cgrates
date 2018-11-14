5b94c21f732a42187e453a02190383d8fe34234d
TeoV <teofil.voivozeanu@itsyscom.com>
2018-11-14 07:05:17 -0500

    Update stats from tutorial with ttl -1

--- general_tests/tutorial_calls_test.go
+++ general_tests/tutorial_calls_test.go
@@ -39,7 +39,7 @@ import (
 var tutorialCallsCfg *config.CGRConfig
 var tutorialCallsRpc *rpc.Client
 var tutorialCallsPjSuaListener *os.File
-var waitRater = flag.Int("wait_rater", 100, "Number of miliseconds to wait for rater to start and cache")
+var waitRater = flag.Int("wait_rater", 1000, "Number of miliseconds to wait for rater to start and cache")
 var dataDir = flag.String("data_dir", "/usr/share/cgrates", "CGR data dir path here")
 var fsConfig = flag.String("fsConfig", "/usr/share/cgrates/tutorials/fs_evsock", "FreeSwitch tutorial folder")
 var kamConfig = flag.String("kamConfig", "/usr/share/cgrates/tutorials/kamevapi", "Kamailio tutorial folder")
@@ -72,6 +72,7 @@ var sTestsCalls = []func(t *testing.T){
 	testCallAccount1001,
 	testCall1001Cdrs,
 	testCall1002Cdrs,
+	testCall1003Cdrs,
 	testCallStatMetrics,
 	testCallCheckResourceRelease,
 	testCallCheckThreshold1001After,
@@ -245,7 +246,6 @@ func testCallRpcConn(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	time.Sleep(time.Duration(*waitRater) * time.Millisecond) // Give time for scheduler to execute topups
 }
 
 // Load the tariff plan, creating accounts and their balances
@@ -364,6 +364,7 @@ func testCallStartPjsuaListener(t *testing.T) {
 		acnts, 5070, time.Duration(*waitRater)*time.Millisecond); err != nil {
 		t.Fatal(err)
 	}
+	time.Sleep(1 * time.Second)
 }
 
 // Call from 1001 (prepaid) to 1002
@@ -373,7 +374,8 @@ func testCallCall1001To1002(t *testing.T) {
 		"sip:1002@127.0.0.1", "sip:127.0.0.1:5080", time.Duration(67)*time.Second, 5071); err != nil {
 		t.Fatal(err)
 	}
-	time.Sleep(1 * time.Second)
+	// give time to session to start so we can check it
+	time.Sleep(time.Second)
 }
 
 // GetActiveSessions
@@ -430,6 +432,7 @@ func testCallCall1003To1001(t *testing.T) {
 		t.Fatal(err)
 	}
 	time.Sleep(22 * time.Second)
+	// after this call from 1001 to 1003 and call from 1003 to 1001 should be done
 }
 
 // Call from 1003 (prepaid) to 1001 for 15 seconds
@@ -439,6 +442,7 @@ func testCallCall1003To1001SecondTime(t *testing.T) {
 		"sip:1001@127.0.0.1", "sip:127.0.0.1:5080", time.Duration(15)*time.Second, 5075); err != nil {
 		t.Fatal(err)
 	}
+	time.Sleep(time.Second)
 }
 
 // Check if the resource was Allocated
@@ -463,11 +467,12 @@ func testCallCheckResourceAllocation(t *testing.T) {
 			t.Errorf("Unexpected resource: %+v", utils.ToJSON(r))
 		}
 	}
+	// Allow calls to finish before start querying the results
+	time.Sleep(time.Duration(45) * time.Second)
 }
 
 // Make sure account was debited properly
 func testCallAccount1001(t *testing.T) {
-	time.Sleep(time.Duration(60) * time.Second) // Allow calls to finish before start querying the results
 	var reply *engine.Account
 	attrs := &utils.AttrGetAccount{Tenant: "cgrates.org", Account: "1001"}
 	if err := tutorialCallsRpc.Call("ApierV2.GetAccount", attrs, &reply); err != nil {
@@ -514,7 +519,8 @@ func testCall1001Cdrs(t *testing.T) {
 // Make sure account was debited properly
 func testCall1002Cdrs(t *testing.T) {
 	var reply []*engine.ExternalCDR
-	req := utils.RPCCDRsFilter{RunIDs: []string{utils.META_DEFAULT}, Accounts: []string{"1002"}, DestinationPrefixes: []string{"1001"}}
+	req := utils.RPCCDRsFilter{RunIDs: []string{utils.META_DEFAULT},
+		Accounts: []string{"1002"}, DestinationPrefixes: []string{"1001"}}
 	if err := tutorialCallsRpc.Call("ApierV2.GetCdrs", req, &reply); err != nil {
 		t.Error("Unexpected error: ", err.Error())
 	} else if len(reply) != 1 {
@@ -532,11 +538,36 @@ func testCall1002Cdrs(t *testing.T) {
 	}
 }
 
+// Make sure account was debited properly
+func testCall1003Cdrs(t *testing.T) {
+	var reply []*engine.ExternalCDR
+	req := utils.RPCCDRsFilter{RunIDs: []string{utils.META_DEFAULT},
+		Accounts: []string{"1003"}, DestinationPrefixes: []string{"1001"}}
+	if err := tutorialCallsRpc.Call("ApierV2.GetCdrs", req, &reply); err != nil {
+		t.Error("Unexpected error: ", err.Error())
+	} else if len(reply) != 2 {
+		t.Error("Unexpected number of CDRs returned: ", len(reply))
+	} else {
+		for _, cdr := range reply {
+			if cdr.RequestType != utils.META_PREPAID {
+				t.Errorf("Unexpected RequestType for CDR: %+v", cdr.RequestType)
+			}
+			if cdr.Usage != "15s" && cdr.Usage != "20s" { // Usage as seconds
+				t.Errorf("Unexpected Usage for CDR: %+v", cdr.Usage)
+			}
+			if cdr.CostSource != utils.MetaSessionS {
+				t.Errorf("Unexpected CostSource for CDR: %+v", cdr.CostSource)
+			}
+
+		}
+	}
+}
+
 func testCallStatMetrics(t *testing.T) {
 	var metrics map[string]string
 	firstStatMetrics1 := map[string]string{
-		utils.MetaTCC: "1.35009",
-		utils.MetaTCD: "2m25s",
+		utils.MetaTCC: "1.22009",
+		utils.MetaTCD: "2m12s",
 	}
 	firstStatMetrics2 := map[string]string{
 		utils.MetaTCC: "1.34009",
@@ -614,19 +645,19 @@ func testCallSyncSessions(t *testing.T) {
 	var reply *[]*sessions.ActiveSession
 	// activeSessions shouldn't be active
 	if err := tutorialCallsRpc.Call(utils.SessionSv1GetActiveSessions,
-		&map[string]string{}, &reply); err.Error() != utils.ErrNotFound.Error() {
-		t.Error("Got error on SessionSv1.GetActiveSessions: ", err.Error())
+		&map[string]string{}, &reply); err == nil || err.Error() != utils.ErrNotFound.Error() {
+		t.Error("Got error on SessionSv1.GetActiveSessions: ", err)
 	}
 	// 1001 call 1002 stop the call after 12 seconds
 	if err := engine.PjsuaCallUri(
 		&engine.PjsuaAccount{Id: "sip:1001@127.0.0.1", Username: "1001", Password: "CGRateS.org", Realm: "*"},
-		"sip:1002@127.0.0.1", "sip:127.0.0.1:5080", time.Duration(120)*time.Second, 5071); err != nil {
+		"sip:1002@127.0.0.1", "sip:127.0.0.1:5080", time.Duration(120)*time.Second, 5076); err != nil {
 		t.Fatal(err)
 	}
 	// 1001 call 1003 stop the call after 11 seconds
 	if err := engine.PjsuaCallUri(
 		&engine.PjsuaAccount{Id: "sip:1001@127.0.0.1", Username: "1001", Password: "CGRateS.org", Realm: "*"},
-		"sip:1003@127.0.0.1", "sip:127.0.0.1:5080", time.Duration(120)*time.Second, 5073); err != nil {
+		"sip:1003@127.0.0.1", "sip:127.0.0.1:5080", time.Duration(120)*time.Second, 5077); err != nil {
 		t.Fatal(err)
 	}
 	time.Sleep(1 * time.Second)
@@ -635,21 +666,19 @@ func testCallSyncSessions(t *testing.T) {
 		&map[string]string{}, &reply); err != nil {
 		t.Error("Got error on SessionSv1.GetActiveSessions: ", err.Error())
 	} else if len(*reply) != 2 {
-		t.Errorf("expecting 2, received reply: %+v", utils.ToJSON(reply))
+		t.Errorf("expecting 2 active sessions, received: %+v", utils.ToJSON(reply))
 	}
 	//check if resource was allocated for 2 calls(1001->1002;1001->1003)
 	var rs *engine.Resources
 	args := &utils.ArgRSv1ResourceUsage{
 		CGREvent: utils.CGREvent{
 			Tenant: "cgrates.org",
-			ID:     "ResourceAllocation",
+			ID:     "AllocateResource",
 			Event: map[string]interface{}{
 				utils.Account:     "1001",
 				utils.Subject:     "1001",
 				utils.Destination: "1002"},
-		},
-		Units: 1,
-	}
+		}}
 	if err := tutorialCallsRpc.Call(utils.ResourceSv1GetResourcesForEvent, args, &rs); err != nil {
 		t.Error(err)
 	} else if len(*rs) != 1 {
@@ -684,8 +713,8 @@ func testCallSyncSessions(t *testing.T) {
 
 	// activeSessions shouldn't be active
 	if err := tutorialCallsRpc.Call(utils.SessionSv1GetActiveSessions,
-		&map[string]string{}, &reply); err.Error() != utils.ErrNotFound.Error() {
-		t.Error("Got error on SessionSv1.GetActiveSessions: ", err.Error())
+		&map[string]string{}, &reply); err == nil || err.Error() != utils.ErrNotFound.Error() {
+		t.Error("Got error on SessionSv1.GetActiveSessions: ", err)
 	}
 
 	var sourceForCDR string
@@ -696,6 +725,9 @@ func testCallSyncSessions(t *testing.T) {
 		numberOfCDR = 2
 	case utils.Kamailio:
 		sourceForCDR = utils.KamailioAgent
+		numberOfCDR = 3
+	case utils.Asterisk:
+		sourceForCDR = ""
 		numberOfCDR = 3
 	}
 	// verify cdr
