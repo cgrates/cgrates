8c281a16dddd20189a46a5d3c6f0485284e837ee
TeoV <teofil.voivozeanu@itsyscom.com>
2019-01-11 06:31:56 -0500

    Add dispatcher and tp dispatcher in migrator

---
+++ migrator/dispatchers.go
@@ -0,0 +1,81 @@
+/*
+Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
+Copyright (C) ITsysCOM GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+package migrator
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/cgrates/cgrates/config"
+	"github.com/cgrates/cgrates/engine"
+	"github.com/cgrates/cgrates/utils"
+)
+
+func (m *Migrator) migrateCurrentDispatcher() (err error) {
+	var ids []string
+	tenant := config.CgrConfig().GeneralCfg().DefaultTenant
+	ids, err = m.dmIN.DataManager().DataDB().GetKeysForPrefix(utils.DispatcherProfilePrefix)
+	if err != nil {
+		return err
+	}
+	for _, id := range ids {
+		idg := strings.TrimPrefix(id, utils.DispatcherProfilePrefix+tenant+":")
+		dpp, err := m.dmIN.DataManager().GetDispatcherProfile(tenant, idg, false, false, utils.NonTransactional)
+		if err != nil {
+			return err
+		}
+		if dpp != nil {
+			if m.dryRun != true {
+				if err := m.dmOut.DataManager().SetDispatcherProfile(dpp, true); err != nil {
+					return err
+				}
+				m.stats[utils.Dispatchers] += 1
+			}
+		}
+	}
+	return
+}
+
+func (m *Migrator) migrateDispatchers() (err error) {
+	var vrs engine.Versions
+	current := engine.CurrentDataDBVersions()
+	vrs, err = m.dmOut.DataManager().DataDB().GetVersions("")
+	if err != nil {
+		return utils.NewCGRError(utils.Migrator,
+			utils.ServerErrorCaps,
+			err.Error(),
+			fmt.Sprintf("error: <%s> when querying oldDataDB for versions", err.Error()))
+	} else if len(vrs) == 0 {
+		return utils.NewCGRError(utils.Migrator,
+			utils.MandatoryIEMissingCaps,
+			utils.UndefinedVersion,
+			"version number is not defined for DispatcherProfile model")
+	}
+	switch vrs[utils.Dispatchers] {
+	case current[utils.Dispatchers]:
+		if m.sameDataDB {
+			return
+		}
+		if err := m.migrateCurrentDispatcher(); err != nil {
+			return err
+		}
+		return
+	}
+	return
+}
---
+++ migrator/tp_dispatchers.go
@@ -0,0 +1,90 @@
+/*
+Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
+Copyright (C) ITsysCOM GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+package migrator
+
+import (
+	"fmt"
+
+	"github.com/cgrates/cgrates/engine"
+	"github.com/cgrates/cgrates/utils"
+)
+
+func (m *Migrator) migrateCurrentTPDispatchers() (err error) {
+	tpids, err := m.storDBIn.StorDB().GetTpIds(utils.TBLTPDispatchers)
+	if err != nil {
+		return err
+	}
+
+	for _, tpid := range tpids {
+		ids, err := m.storDBIn.StorDB().GetTpTableIds(tpid, utils.TBLTPDispatchers,
+			utils.TPDistinctIds{"id"}, map[string]string{}, nil)
+		if err != nil {
+			return err
+		}
+		for _, id := range ids {
+			dispatchers, err := m.storDBIn.StorDB().GetTPDispatchers(tpid, "", id)
+			if err != nil {
+				return err
+			}
+			if dispatchers != nil {
+				if m.dryRun != true {
+					if err := m.storDBOut.StorDB().SetTPDispatchers(dispatchers); err != nil {
+						return err
+					}
+					for _, dispatcher := range dispatchers {
+						if err := m.storDBIn.StorDB().RemTpData(utils.TBLTPDispatchers, dispatcher.TPid,
+							map[string]string{"id": dispatcher.ID}); err != nil {
+							return err
+						}
+					}
+					m.stats[utils.TpDispatchers] += 1
+				}
+			}
+		}
+	}
+	return
+}
+
+func (m *Migrator) migrateTPDispatchers() (err error) {
+	var vrs engine.Versions
+	current := engine.CurrentStorDBVersions()
+	vrs, err = m.storDBOut.StorDB().GetVersions("")
+	if err != nil {
+		return utils.NewCGRError(utils.Migrator,
+			utils.ServerErrorCaps,
+			err.Error(),
+			fmt.Sprintf("error: <%s> when querying oldDataDB for versions", err.Error()))
+	} else if len(vrs) == 0 {
+		return utils.NewCGRError(utils.Migrator,
+			utils.MandatoryIEMissingCaps,
+			utils.UndefinedVersion,
+			"version number is not defined for TPDispatcher model")
+	}
+	switch vrs[utils.TpDispatchers] {
+	case current[utils.TpDispatchers]:
+		if m.sameStorDB {
+			return
+		}
+		if err := m.migrateCurrentTPDispatchers(); err != nil {
+			return err
+		}
+		return
+	}
+	return
+}
