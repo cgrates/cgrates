464957b897684a3b62825e41f1be877f32f882f7
Gavin Henry <ghenry@surevoip.co.uk>
2019-03-25 22:07:47 +0000

    Update Dockerfile for FreeSWITCH 1.8, install from deb and latest cgr-engine syntax.

--- data/docker/prod/README.md
+++ data/docker/prod/README.md
@@ -8,4 +8,6 @@ From the project root:
 docker build -t cgrates data/docker/prod
 # create the container
 docker run --rm -itv `pwd`:/root/code --name cgr cgrates
+# If running on local machine, do this to expose agent for /pprof:
+docker run --rm -itv `pwd`:/root/code -p 127.0.0.1:2080:2080/tcp --name cgr cgrates
 ```
--- data/docker/prod/start.sh
+++ data/docker/prod/start.sh
@@ -6,6 +6,16 @@ cd /usr/share/cgrates/storage/mysql && ./setup_cgr_db.sh root CGRateS.org
 cd /usr/share/cgrates/storage/postgres && ./setup_cgr_db.sh
 
 /usr/share/cgrates/tutorials/fs_evsock/freeswitch/etc/init.d/freeswitch start
+ 
+# Docker doesn't have syslog. Let others modify this to send out logs if needed
+sed -i 's/config_dir/config_path/g' /usr/share/cgrates/tutorials/fs_evsock/cgrates/etc/init.d/cgrates
+sed -i 's/\/etc\/cgrates/\/etc\/cgrates -httprof_path=\/pprof -logger=*stdout/g' /usr/share/cgrates/tutorials/fs_evsock/cgrates/etc/init.d/cgrates
+
+# Get our data ready
+/usr/bin/cgr-migrator -migrate=*set_versions -config_path=/usr/share/cgrates/tutorials/fs_evsock/cgrates/etc/cgrates/
+
+# Let FreeSWITCH start up
+sleep 5
 /usr/share/cgrates/tutorials/fs_evsock/cgrates/etc/init.d/cgrates start
 
 bash --rcfile /root/.bashrc
