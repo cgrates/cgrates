56607c268044cdd15b4bfd1d3be143aab5b7d6b4
Tripon Alexandru-Ionut <alexandru.tripon@itsyscom.com>
2019-04-09 15:57:23 +0300

    Updated ping command

--- console/ping.go
+++ console/ping.go
@@ -36,7 +36,8 @@ func init() {
 type CmdApierPing struct {
 	name      string
 	rpcMethod string
-	rpcParams *StringWrapper
+	rpcParams interface{}
+	item      string
 	*CommandExecuter
 }
 
@@ -49,7 +50,7 @@ func (self *CmdApierPing) Name() string {
 }
 
 func (self *CmdApierPing) RpcMethod() string {
-	switch strings.ToLower(self.rpcParams.Item) {
+	switch strings.ToLower(self.item) {
 	case utils.SuppliersLow:
 		return utils.SupplierSv1Ping
 	case utils.AttributesLow:
@@ -81,11 +82,14 @@ func (self *CmdApierPing) RpcParams(reset bool) interface{} {
 	if reset || self.rpcParams == nil {
 		self.rpcParams = &StringWrapper{}
 	}
-
-	return utils.CGREventWithArgDispatcher{}
+	return self.rpcParams
 }
 
 func (self *CmdApierPing) PostprocessRpcParams() error {
+	if val, can := self.rpcParams.(*StringWrapper); can {
+		self.item = val.Item
+	}
+	self.rpcParams = &utils.CGREventWithArgDispatcher{}
 	return nil
 }
 
