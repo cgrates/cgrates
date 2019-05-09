23d117a3210063486706103711e29d934aabeb30
TeoV <teofil.voivozeanu@itsyscom.com>
2019-05-09 10:08:05 +0300

    Update CacheSv1 API's with ArgDispatcher

--- apier/v1/caches.go
+++ apier/v1/caches.go
@@ -21,6 +21,7 @@ package v1
 import (
 	"time"
 
+	"github.com/cgrates/cgrates/dispatchers"
 	"github.com/cgrates/cgrates/engine"
 	"github.com/cgrates/cgrates/utils"
 	"github.com/cgrates/ltcache"
@@ -36,77 +37,77 @@ type CacheSv1 struct {
 }
 
 // GetItemIDs returns the IDs for cacheID with given prefix
-func (chSv1 *CacheSv1) GetItemIDs(args *engine.ArgsGetCacheItemIDs,
+func (chSv1 *CacheSv1) GetItemIDs(args *dispatchers.ArgsGetCacheItemIDsWithApiKey,
 	reply *[]string) error {
-	return chSv1.cacheS.V1GetItemIDs(args, reply)
+	return chSv1.cacheS.V1GetItemIDs(&args.ArgsGetCacheItemIDs, reply)
 }
 
 // HasItem verifies the existence of an Item in cache
-func (chSv1 *CacheSv1) HasItem(args *engine.ArgsGetCacheItem,
+func (chSv1 *CacheSv1) HasItem(args *dispatchers.ArgsGetCacheItemWithApiKey,
 	reply *bool) error {
-	return chSv1.cacheS.V1HasItem(args, reply)
+	return chSv1.cacheS.V1HasItem(&args.ArgsGetCacheItem, reply)
 }
 
 // GetItemExpiryTime returns the expiryTime for an item
-func (chSv1 *CacheSv1) GetItemExpiryTime(args *engine.ArgsGetCacheItem,
+func (chSv1 *CacheSv1) GetItemExpiryTime(args *dispatchers.ArgsGetCacheItemWithApiKey,
 	reply *time.Time) error {
-	return chSv1.cacheS.V1GetItemExpiryTime(args, reply)
+	return chSv1.cacheS.V1GetItemExpiryTime(&args.ArgsGetCacheItem, reply)
 }
 
 // RemoveItem removes the Item with ID from cache
-func (chSv1 *CacheSv1) RemoveItem(args *engine.ArgsGetCacheItem,
+func (chSv1 *CacheSv1) RemoveItem(args *dispatchers.ArgsGetCacheItemWithApiKey,
 	reply *string) error {
-	return chSv1.cacheS.V1RemoveItem(args, reply)
+	return chSv1.cacheS.V1RemoveItem(&args.ArgsGetCacheItem, reply)
 }
 
 // Clear will clear partitions in the cache (nil fol all, empty slice for none)
-func (chSv1 *CacheSv1) Clear(cacheIDs []string,
+func (chSv1 *CacheSv1) Clear(args *dispatchers.AttrCacheIDsWithApiKey,
 	reply *string) error {
-	return chSv1.cacheS.V1Clear(cacheIDs, reply)
+	return chSv1.cacheS.V1Clear(args.CacheIDs, reply)
 }
 
 // FlushCache wipes out cache for a prefix or completely
-func (chSv1 *CacheSv1) FlushCache(args utils.AttrReloadCache, reply *string) (err error) {
-	return chSv1.cacheS.V1FlushCache(args, reply)
+func (chSv1 *CacheSv1) FlushCache(args dispatchers.AttrReloadCacheWithApiKey, reply *string) (err error) {
+	return chSv1.cacheS.V1FlushCache(args.AttrReloadCache, reply)
 }
 
 // GetCacheStats returns CacheStats filtered by cacheIDs
-func (chSv1 *CacheSv1) GetCacheStats(cacheIDs []string,
+func (chSv1 *CacheSv1) GetCacheStats(args *dispatchers.AttrCacheIDsWithApiKey,
 	rply *map[string]*ltcache.CacheStats) error {
-	return chSv1.cacheS.V1GetCacheStats(cacheIDs, rply)
+	return chSv1.cacheS.V1GetCacheStats(args.CacheIDs, rply)
 }
 
 // PrecacheStatus checks status of active precache processes
-func (chSv1 *CacheSv1) PrecacheStatus(cacheIDs []string, rply *map[string]string) error {
-	return chSv1.cacheS.V1PrecacheStatus(cacheIDs, rply)
+func (chSv1 *CacheSv1) PrecacheStatus(args *dispatchers.AttrCacheIDsWithApiKey, rply *map[string]string) error {
+	return chSv1.cacheS.V1PrecacheStatus(args.CacheIDs, rply)
 }
 
 // HasGroup checks existence of a group in cache
-func (chSv1 *CacheSv1) HasGroup(args *engine.ArgsGetGroup,
+func (chSv1 *CacheSv1) HasGroup(args *dispatchers.ArgsGetGroupWithApiKey,
 	rply *bool) (err error) {
-	return chSv1.cacheS.V1HasGroup(args, rply)
+	return chSv1.cacheS.V1HasGroup(&args.ArgsGetGroup, rply)
 }
 
 // GetGroupItemIDs returns a list of itemIDs in a cache group
-func (chSv1 *CacheSv1) GetGroupItemIDs(args *engine.ArgsGetGroup,
+func (chSv1 *CacheSv1) GetGroupItemIDs(args *dispatchers.ArgsGetGroupWithApiKey,
 	rply *[]string) (err error) {
-	return chSv1.cacheS.V1GetGroupItemIDs(args, rply)
+	return chSv1.cacheS.V1GetGroupItemIDs(&args.ArgsGetGroup, rply)
 }
 
 // RemoveGroup will remove a group and all items belonging to it from cache
-func (chSv1 *CacheSv1) RemoveGroup(args *engine.ArgsGetGroup,
+func (chSv1 *CacheSv1) RemoveGroup(args *dispatchers.ArgsGetGroupWithApiKey,
 	rply *string) (err error) {
-	return chSv1.cacheS.V1RemoveGroup(args, rply)
+	return chSv1.cacheS.V1RemoveGroup(&args.ArgsGetGroup, rply)
 }
 
 // ReloadCache reloads cache from DB for a prefix or completely
-func (chSv1 *CacheSv1) ReloadCache(attrs utils.AttrReloadCache, reply *string) (err error) {
-	return chSv1.cacheS.V1ReloadCache(attrs, reply)
+func (chSv1 *CacheSv1) ReloadCache(args dispatchers.AttrReloadCacheWithApiKey, reply *string) (err error) {
+	return chSv1.cacheS.V1ReloadCache(args.AttrReloadCache, reply)
 }
 
 // LoadCache loads cache from DB for a prefix or completely
-func (chSv1 *CacheSv1) LoadCache(args utils.AttrReloadCache, reply *string) (err error) {
-	return chSv1.cacheS.V1LoadCache(args, reply)
+func (chSv1 *CacheSv1) LoadCache(args dispatchers.AttrReloadCacheWithApiKey, reply *string) (err error) {
+	return chSv1.cacheS.V1LoadCache(args.AttrReloadCache, reply)
 }
 
 // Ping used to detreminate if component is active
