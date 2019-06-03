7fe94729b56396b722b69abb3f54560ba1b36f58
Tripon Alexandru-Ionut <alexandru.tripon@itsyscom.com>
2019-06-03 10:36:15 +0300

    Updated MapEvent.AsCDR

--- engine/mapevent.go
+++ engine/mapevent.go
@@ -267,8 +267,14 @@ func (me MapEvent) AsCDR(cfg *config.CGRConfig, tnt, tmz string) (cdr *CDR, err 
 			if cdr.CostDetails, err = IfaceAsEventCost(v); err != nil {
 				return nil, err
 			}
-		case utils.ExtraInfo, utils.OrderID:
-
+		case utils.ExtraInfo:
+			if cdr.ExtraInfo, err = utils.IfaceAsString(v); err != nil {
+				return nil, err
+			}
+		case utils.OrderID:
+			if cdr.OrderID, err = utils.IfaceAsInt64(v); err != nil {
+				return nil, err
+			}
 		}
 	}
 	if cfg != nil {
