1c0f94cef0d191a9e0ca79365984d24a5740ef96
TeoV <teofil.voivozeanu@itsyscom.com>
2019-07-21 13:52:58 +0300

    Add an integration test for LoaderS

--- loaders/loader_it_test.go
+++ loaders/loader_it_test.go
@@ -18,3 +18,176 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 package loaders
+
+import (
+	"io/ioutil"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+	"os"
+	"path"
+	"reflect"
+	"sort"
+	"testing"
+	"time"
+
+	"github.com/cgrates/cgrates/config"
+	"github.com/cgrates/cgrates/engine"
+	"github.com/cgrates/cgrates/utils"
+)
+
+var (
+	loaderCfgPath               string
+	loaderCfg                   *config.CGRConfig
+	loaderRPC                   *rpc.Client
+	loaderDataDir               = "/usr/share/cgrates"
+	loaderConfigDIR             string //run tests for specific configuration
+	loaderPathIn, loaderPathOut string
+)
+
+var sTestsLoader = []func(t *testing.T){
+	testLoaderInitCfg,
+	testLoaderResetDataDB,
+	testLoaderStartEngine,
+	testLoaderRPCConn,
+	testLoaderMakeFolders,
+	testLoaderPopulateData,
+	testLoaderLoadAttributes,
+	testLoaderVerifyOutDir,
+	testLoaderCheckAttributes,
+	testLoaderKillEngine,
+}
+
+//Test start here
+func TestLoaderITMySql(t *testing.T) {
+	loaderConfigDIR = "tutmysql"
+	for _, stest := range sTestsLoader {
+		t.Run(loaderConfigDIR, stest)
+	}
+}
+
+func TestLoaderITMongo(t *testing.T) {
+	loaderConfigDIR = "tutmongo"
+	for _, stest := range sTestsLoader {
+		t.Run(loaderConfigDIR, stest)
+	}
+}
+
+func testLoaderInitCfg(t *testing.T) {
+	var err error
+	loaderCfgPath = path.Join(loaderDataDir, "conf", "samples", loaderConfigDIR)
+	loaderCfg, err = config.NewCGRConfigFromPath(loaderCfgPath)
+	if err != nil {
+		t.Error(err)
+	}
+	loaderCfg.DataFolderPath = loaderDataDir // Share DataFolderPath through config towards StoreDb for Flush()
+	config.SetCgrConfig(loaderCfg)
+}
+
+// Wipe out the cdr database
+func testLoaderResetDataDB(t *testing.T) {
+	if err := engine.InitDataDb(loaderCfg); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Start CGR Engine
+func testLoaderStartEngine(t *testing.T) {
+	if _, err := engine.StopStartEngine(loaderCfgPath, 100); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Connect rpc client to rater
+func testLoaderRPCConn(t *testing.T) {
+	var err error
+	loaderRPC, err = jsonrpc.Dial("tcp", loaderCfg.ListenCfg().RPCJSONListen) // We connect over JSON so we can also troubleshoot if needed
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func testLoaderMakeFolders(t *testing.T) {
+	for _, dir := range []string{loaderCfg.LoaderCfg()[1].TpInDir, loaderCfg.LoaderCfg()[1].TpOutDir} {
+		if err := os.RemoveAll(dir); err != nil {
+			t.Fatal("Error removing folder: ", dir, err)
+		}
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			t.Fatal("Error creating folder: ", dir, err)
+		}
+	}
+	loaderPathIn = loaderCfg.LoaderCfg()[1].TpInDir
+	loaderPathOut = loaderCfg.LoaderCfg()[1].TpOutDir
+}
+
+func testLoaderPopulateData(t *testing.T) {
+	fileName := utils.AttributesCsv
+	tmpFilePath := path.Join("/tmp", fileName)
+	if err := ioutil.WriteFile(tmpFilePath, []byte(engine.AttributesCSVContent), 0777); err != nil {
+		t.Fatal(err.Error())
+	}
+	if err := os.Rename(tmpFilePath, path.Join(loaderPathIn, fileName)); err != nil {
+		t.Fatal("Error moving file to processing directory: ", err)
+	}
+}
+
+func testLoaderLoadAttributes(t *testing.T) {
+	var reply string
+	if err := loaderRPC.Call(utils.LoaderSv1Load,
+		&ArgsProcessFolder{LoaderID: "CustomLoader"}, &reply); err != nil {
+		t.Error(err)
+	}
+}
+
+func testLoaderVerifyOutDir(t *testing.T) {
+	time.Sleep(100 * time.Millisecond)
+	if outContent1, err := ioutil.ReadFile(path.Join(loaderPathOut, utils.AttributesCsv)); err != nil {
+		t.Error(err)
+	} else if engine.AttributesCSVContent != string(outContent1) {
+		t.Errorf("Expecting: %q, received: %q", engine.AttributesCSVContent, string(outContent1))
+	}
+}
+
+func testLoaderCheckAttributes(t *testing.T) {
+	eAttrPrf := &engine.AttributeProfile{
+		Tenant:    "cgrates.org",
+		ID:        "ALS1",
+		Contexts:  []string{"con1", "con2", "con3"},
+		FilterIDs: []string{"*string:~Account:1001"},
+		ActivationInterval: &utils.ActivationInterval{
+			ActivationTime: time.Date(2014, 7, 29, 15, 0, 0, 0, time.UTC)},
+		Attributes: []*engine.Attribute{
+			&engine.Attribute{
+				FilterIDs: []string{"*string:~Field1:Initial"},
+				FieldName: "Field1",
+				Type:      utils.MetaVariable,
+				Value:     config.NewRSRParsersMustCompile("Sub1", true, utils.INFIELD_SEP),
+			},
+			&engine.Attribute{
+				FilterIDs: []string{},
+				FieldName: "Field2",
+				Type:      utils.MetaVariable,
+				Value:     config.NewRSRParsersMustCompile("Sub2", true, utils.INFIELD_SEP),
+			}},
+		Blocker: true,
+		Weight:  20,
+	}
+
+	var reply *engine.AttributeProfile
+	if err := loaderRPC.Call("ApierV1.GetAttributeProfile",
+		&utils.TenantID{Tenant: "cgrates.org", ID: "ALS1"}, &reply); err != nil {
+		t.Fatal(err)
+	}
+	eAttrPrf.Compile()
+	reply.Compile()
+	sort.Strings(eAttrPrf.Contexts)
+	sort.Strings(reply.Contexts)
+	if !reflect.DeepEqual(eAttrPrf, reply) {
+		t.Errorf("Expecting : %+v, received: %+v", eAttrPrf, reply)
+	}
+}
+
+func testLoaderKillEngine(t *testing.T) {
+	if err := engine.KillEngine(100); err != nil {
+		t.Error(err)
+	}
+}
