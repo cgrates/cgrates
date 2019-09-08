8a9ac3853699ea573279beef35566307dfdf8ad6
TeoV <teofil.voivozeanu@itsyscom.com>
2019-09-08 16:34:55 +0300

    Move csvProvider in config (SliceDP) and add ers in integration test script

--- config/dataprovider.go
+++ config/dataprovider.go
@@ -21,6 +21,7 @@ package config
 import (
 	"fmt"
 	"net"
+	"strconv"
 	"strings"
 
 	"github.com/cgrates/cgrates/utils"
@@ -154,5 +155,63 @@ func (objDP *ObjectDP) AsNavigableMap([]*FCTemplate) (
 
 // RemoteHost is part of engine.DataProvider interface
 func (objDP *ObjectDP) RemoteHost() net.Addr {
+	return utils.LocalAddr()
+}
+
+// NewSliceDP constructs a DataProvider
+func NewSliceDP(record []string) (dP DataProvider) {
+	dP = &SliceDP{req: record, cache: NewNavigableMap(nil)}
+	return
+}
+
+// SliceDP implements engine.DataProvider so we can pass it to filters
+type SliceDP struct {
+	req   []string
+	cache *NavigableMap
+}
+
+// String is part of engine.DataProvider interface
+// when called, it will display the already parsed values out of cache
+func (cP *SliceDP) String() string {
+	return utils.ToJSON(cP)
+}
+
+// FieldAsInterface is part of engine.DataProvider interface
+func (cP *SliceDP) FieldAsInterface(fldPath []string) (data interface{}, err error) {
+	if len(fldPath) != 1 {
+		return nil, utils.ErrNotFound
+	}
+	if data, err = cP.cache.FieldAsInterface(fldPath); err == nil ||
+		err != utils.ErrNotFound { // item found in cache
+		return
+	}
+	err = nil // cancel previous err
+	if cfgFieldIdx, err := strconv.Atoi(fldPath[0]); err != nil || len(cP.req) <= cfgFieldIdx {
+		return nil, fmt.Errorf("Ignoring record: %v with error : %+v", cP.req, err)
+	} else {
+		data = cP.req[cfgFieldIdx]
+	}
+	cP.cache.Set(fldPath, data, false, false)
+	return
+}
+
+// FieldAsString is part of engine.DataProvider interface
+func (cP *SliceDP) FieldAsString(fldPath []string) (data string, err error) {
+	var valIface interface{}
+	valIface, err = cP.FieldAsInterface(fldPath)
+	if err != nil {
+		return
+	}
+	return utils.IfaceAsString(valIface), nil
+}
+
+// AsNavigableMap is part of engine.DataProvider interface
+func (cP *SliceDP) AsNavigableMap([]*FCTemplate) (
+	nm *NavigableMap, err error) {
+	return nil, utils.ErrNotImplemented
+}
+
+// RemoteHost is part of engine.DataProvider interface
+func (cP *SliceDP) RemoteHost() net.Addr {
 	return utils.LocalAddr()
 }
