4fae6f8e878409f7f59b5f7bf49eab9255e0c35d
Trial97 <alexandru.tripon@itsyscom.com>
2019-09-26 13:42:46 +0300

    Added test for DNSAgent reload

---
+++ services/dnsagent_it_test.go
@@ -0,0 +1,85 @@
+// +build integration
+
+/*
+Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
+Copyright (C) ITsysCOM GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+package services
+
+import (
+	"path"
+	"testing"
+	"time"
+
+	"github.com/cgrates/cgrates/config"
+	"github.com/cgrates/cgrates/engine"
+	"github.com/cgrates/cgrates/servmanager"
+	"github.com/cgrates/cgrates/utils"
+	"github.com/cgrates/rpcclient"
+)
+
+func TestDNSAgentReload(t *testing.T) {
+	cfg, err := config.NewDefaultCGRConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+	cfg.SessionSCfg().Enabled = true
+	utils.Newlogger(utils.MetaSysLog, cfg.GeneralCfg().NodeID)
+	utils.Logger.SetLogLevel(7)
+	filterSChan := make(chan *engine.FilterS, 1)
+	filterSChan <- nil
+	engineShutdown := make(chan bool, 1)
+	chS := engine.NewCacheS(cfg, nil)
+
+	cacheSChan := make(chan rpcclient.RpcClientConnection, 1)
+	cacheSChan <- chS
+
+	server := utils.NewServer()
+	srvMngr := servmanager.NewServiceManager(cfg /*dm*/, nil,
+		/*cdrStorage*/ nil,
+		/*loadStorage*/ nil, filterSChan,
+		server, nil, engineShutdown)
+	srvMngr.SetCacheS(chS)
+	srv := NewDNSAgent()
+	srvMngr.AddService(srv, NewSessionService(), &CacheService{connChan: cacheSChan})
+	if err = srvMngr.StartServices(); err != nil {
+		t.Error(err)
+	}
+	if srv.IsRunning() {
+		t.Errorf("Expected service to be down")
+	}
+	var reply string
+	if err := cfg.V1ReloadConfig(&config.ConfigReloadWithArgDispatcher{
+		Path:    path.Join("/usr", "share", "cgrates", "conf", "samples", "dnsagent_reload"),
+		Section: config.DNSAgentJson,
+	}, &reply); err != nil {
+		t.Error(err)
+	} else if reply != utils.OK {
+		t.Errorf("Expecting OK ,received %s", reply)
+	}
+	time.Sleep(10 * time.Millisecond) //need to switch to gorutine
+	if !srv.IsRunning() {
+		t.Errorf("Expected service to be running")
+	}
+	cfg.DNSAgentCfg().Enabled = false
+	cfg.GetReloadChan(config.DNSAgentJson) <- struct{}{}
+	time.Sleep(10 * time.Millisecond)
+	if srv.IsRunning() {
+		t.Errorf("Expected service to be down")
+	}
+	engineShutdown <- true
+	time.Sleep(10 * time.Millisecond) // wait to stop session bijsonrpc server
+}
