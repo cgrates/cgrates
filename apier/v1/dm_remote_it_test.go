f9ac84202937de86ee9ce3ff784db06c26581190
TeoV <teofil.voivozeanu@itsyscom.com>
2019-11-22 11:28:16 +0200

    Add a small delay between account operations and get account

--- apier/v1/dm_remote_it_test.go
+++ apier/v1/dm_remote_it_test.go
@@ -674,6 +674,11 @@ func testInternalReplicationSetThreshold(t *testing.T) {
 	} else if !reflect.DeepEqual(tPrfl.ThresholdProfile, reply) {
 		t.Errorf("Expecting: %+v, received: %+v", tPrfl.ThresholdProfile, reply)
 	}
+	expectedIDX2 := []string{
+		"*string:~Account:1001:THD_ACNT_1001",
+		"*string:~Account:1001:THD_Replication",
+		"*string:~CustomField:CustomValue:THD_Replication",
+	}
 	// verify indexes on engine1 (should be the same as internal)
 	if err := engineOneRPC.Call("ApierV1.GetFilterIndexes", &AttrGetFilterIndexes{
 		ItemType: utils.MetaThresholds, Tenant: "cgrates.org", FilterType: utils.MetaString},
@@ -681,9 +686,9 @@ func testInternalReplicationSetThreshold(t *testing.T) {
 		t.Error(err)
 	}
 	sort.Strings(indexes)
-	if !reflect.DeepEqual(expectedIDX, indexes) {
+	if !reflect.DeepEqual(expectedIDX2, indexes) {
 		t.Errorf("Expecting: %+v, received: %+v",
-			expectedIDX, utils.ToJSON(indexes))
+			expectedIDX2, utils.ToJSON(indexes))
 	}
 	// verify data on engine2
 	if err := engineTwoRPC.Call("ApierV1.GetThresholdProfile",
@@ -764,7 +769,7 @@ func testInternalAccountBalanceOperations(t *testing.T) {
 	if err := internalRPC.Call(utils.ApierV1SetBalance, attrs, &reply); err != nil {
 		t.Error(err)
 	}
-
+	time.Sleep(50 * time.Millisecond)
 	var acnt *engine.Account
 	attrAcc := &utils.AttrGetAccount{
 		Tenant:  "cgrates.org",
@@ -803,7 +808,7 @@ func testInternalAccountBalanceOperations(t *testing.T) {
 	} else if reply != utils.OK {
 		t.Errorf("Received: %s", reply)
 	}
-
+	time.Sleep(50 * time.Millisecond)
 	// verify debited account on engineOne
 	if err := engineOneRPC.Call(utils.ApierV2GetAccount, attrAcc, &acnt); err != nil {
 		t.Error(err)
@@ -836,7 +841,7 @@ func testInternalAccountBalanceOperations(t *testing.T) {
 	if err := internalRPC.Call(utils.ApierV1AddBalance, addBal, &reply); err != nil {
 		t.Error(err)
 	}
-
+	time.Sleep(50 * time.Millisecond)
 	// verify account on engineOne
 	if err := engineOneRPC.Call(utils.ApierV2GetAccount, attrAcc, &acnt); err != nil {
 		t.Error(err)
