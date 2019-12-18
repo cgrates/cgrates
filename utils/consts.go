18b4771178abbac229cfebca2e1298761e4d1375
DanB <damb@cgrates.org>
2019-12-18 12:24:01 +0100

    Remove small comment

--- utils/consts.go
+++ utils/consts.go
@@ -369,7 +369,7 @@ const (
 	MetaUnixTimestamp           = "*unix_timestamp"
 	MetaPostCDR                 = "*post_cdr"
 	MetaDumpToFile              = "*dump_to_file"
-	NonTransactional            = "" // used in transactional cache mechanism
+	NonTransactional            = ""
 	DataDB                      = "data_db"
 	StorDB                      = "stor_db"
 	NotFoundCaps                = "NOT_FOUND"
