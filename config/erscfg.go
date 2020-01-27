0d7021218b715e172cfb47ed3275cc91bf0dfde2
Michal Škuta <michal.skuta@gmail.com>
2020-01-27 15:43:47 +0100

    Update erscfg.go

--- config/erscfg.go
+++ config/erscfg.go
@@ -20,6 +20,7 @@ package config
 
 import (
 	"time"
+	"strconv"
 
 	"github.com/cgrates/cgrates/utils"
 )
@@ -134,7 +135,7 @@ func (er *EventReaderCfg) loadFromJsonCfg(jsnCfg *EventReaderJsonCfg, sep string
 		er.FieldSep = *jsnCfg.Field_separator
 	}
 	if jsnCfg.Run_delay != nil {
-		er.RunDelay = utils.ParseDurationWithSecs(*jsnCfg.Run_delay)
+		er.RunDelay = utils.ParseDurationWithSecs(strconv.Itoa(*jsnCfg.Run_delay))
 	}
 	if jsnCfg.Concurrent_requests != nil {
 		er.ConcurrentReqs = *jsnCfg.Concurrent_requests
