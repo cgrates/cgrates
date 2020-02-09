cc5661915aec8949026da96551a78dfdcbdf6658
TeoV <teofil.voivozeanu@itsyscom.com>
2020-02-09 12:35:29 +0200

    Update scripts to work on centOS when building packages

--- data/scripts/migrator/dbsmerge_redis.py
+++ data/scripts/migrator/dbsmerge_redis.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # depends:
 #   ^ redis # install via easy_install redis
--- data/scripts/migrator/usage_postgres.py
+++ data/scripts/migrator/usage_postgres.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # depends:
 #   ^ psycopg2 (debian: python-psycopg2)
