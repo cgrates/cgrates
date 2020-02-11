77e4ca7be774b58e483083fa1f586aae963635e4
Trial97 <alexandru.tripon@itsyscom.com>
2020-02-11 10:49:53 +0200

    Updated build script to check git tag

--- build.sh
+++ build.sh
@@ -3,6 +3,13 @@ echo "Building CGRateS ..."
 
 GIT_LAST_LOG=$(git log -1 | tr -d "'")
 
+GIT_TAG_LOG=$(git tag -l --points-at HEAD)
+
+if [ ! -z "$GIT_TAG_LOG" ]
+then
+    GIT_LAST_LOG=""
+fi
+
 go install -ldflags "-X 'github.com/cgrates/cgrates/utils.GitLastLog=$GIT_LAST_LOG'" github.com/cgrates/cgrates/cmd/cgr-engine
 cr=$?
 go install -ldflags "-X 'github.com/cgrates/cgrates/utils.GitLastLog=$GIT_LAST_LOG'" github.com/cgrates/cgrates/cmd/cgr-loader
