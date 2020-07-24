680756b6cae849d5ce26958c98f29ddc6cf66206
Trial97 <alexandru.tripon@itsyscom.com>
2020-07-24 16:26:59 +0300

    Added tests for chargers with *opts filter

--- engine/filters.go
+++ engine/filters.go
@@ -603,7 +603,7 @@ func (dDP *dynamicDP) RemoteHost() net.Addr {
 
 var initialDPPrefixes = utils.NewStringSet([]string{utils.MetaReq, utils.MetaVars,
 	utils.MetaCgreq, utils.MetaCgrep, utils.MetaRep, utils.MetaCGRAReq,
-	utils.MetaAct, utils.MetaEC, utils.MetaUCH})
+	utils.MetaAct, utils.MetaEC, utils.MetaUCH, utils.MetaOpts})
 
 func (dDP *dynamicDP) FieldAsInterface(fldPath []string) (val interface{}, err error) {
 	if len(fldPath) == 0 {
