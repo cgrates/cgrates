ca3555cf035d8d71cb3f7bf958a27ec1a019851f
DanB <danb@cgrates.org>
2020-10-08 13:28:01 +0200

    Changing the version to v0.10.2

--- utils/consts.go
+++ utils/consts.go
@@ -136,7 +136,7 @@ var (
 
 const (
 	CGRateS                      = "CGRateS"
-	VERSION                      = "v0.10.2~dev"
+	VERSION                      = "v0.10.2"
 	DIAMETER_FIRMWARE_REVISION   = 918
 	REDIS_MAX_CONNS              = 10
 	CGRATES                      = "cgrates"
