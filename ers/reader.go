49e423d21ab7aaeded4a0b72ba0f9ec2fca72bf8
Trial97 <alexandru.tripon@itsyscom.com>
2020-10-19 10:33:27 +0300

    Added S3 Event Reader

--- ers/reader.go
+++ ers/reader.go
@@ -57,6 +57,8 @@ func NewEventReader(cfg *config.CGRConfig, cfgIdx int,
 		return NewJSONFileER(cfg, cfgIdx, rdrEvents, rdrErr, fltrS, rdrExit)
 	case utils.MetaAMQPjsonMap:
 		return NewAMQPER(cfg, cfgIdx, rdrEvents, rdrErr, fltrS, rdrExit)
+	case utils.MetaS3jsonMap:
+		return NewS3ER(cfg, cfgIdx, rdrEvents, rdrErr, fltrS, rdrExit)
 	}
 	return
 }
