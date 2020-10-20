1f9f052226afcffa7f2be99f0801dac7520ea3e9
TeoV <teofil.voivozeanu@itsyscom.com>
2020-10-20 13:28:19 +0300

    Correctly use RALs conns in case of SessionS refund

--- sessions/sessions.go
+++ sessions/sessions.go
@@ -648,7 +648,7 @@ func (sS *SessionS) roundCost(s *Session, sRunIdx int) (err error) {
 		cd.RunID = runID
 		cd.Increments = roundIncrements
 		var response float64
-		if err = sS.connMgr.Call(sS.cgrCfg.SessionSCfg().ResSConns, nil,
+		if err = sS.connMgr.Call(sS.cgrCfg.SessionSCfg().RALsConns, nil,
 			utils.ResponderRefundRounding,
 			&engine.CallDescriptorWithArgDispatcher{CallDescriptor: cd},
 			&response); err != nil {
