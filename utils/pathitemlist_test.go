13b142a1feb67f4406af0866df59281e1389a759
andronache <andrei.andronache@itsyscom.com>
2020-11-06 17:36:26 +0200

    Added tests to /utils

--- utils/pathitemlist_test.go
+++ utils/pathitemlist_test.go
@@ -19,53 +19,147 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 package utils
 
 import (
+	"reflect"
 	"testing"
 )
+
+func TestPathItemListNext(t *testing.T) {
+	list := NewPathItemList()
+	node1 := NewPathItems([]string{"path1"})
+	node2 := NewPathItems([]string{"path2"})
+	node3 := NewPathItems([]string{"path3"})
+	list.PushFront(node3)
+	list.PushFront(node2)
+	list.PushFront(node1)
+	if !reflect.DeepEqual("path2", list.Front().Next().Value.String()) {
+		t.Errorf("Expecting: <path1>, received: <%+v>", list.Front().Value.String())
+	}
+
+}
 
 func TestPathItemListNextNil(t *testing.T) {
 	var e PathItemElement
 	received := e.Next()
 	if received != nil {
-		t.Errorf("Expecting: nil, received: %+v", received)
+		t.Errorf("Expecting: <nil>, received: <%+v>", received)
 	}
+}
+
+func TestPathItemListPrev(t *testing.T) {
+	list := NewPathItemList()
+	node1 := NewPathItems([]string{"path1"})
+	node2 := NewPathItems([]string{"path2"})
+	node3 := NewPathItems([]string{"path3"})
+	list.PushFront(node3)
+	list.PushFront(node2)
+	list.PushFront(node1)
+	if !reflect.DeepEqual("path2", list.Back().Prev().Value.String()) {
+		t.Errorf("Expecting: <path1>, received: <%+v>", list.Front().Value.String())
+	}
+
 }
 
 func TestPathItemListPrevNil(t *testing.T) {
 	var e PathItemElement
 	received := e.Prev()
 	if received != nil {
-		t.Errorf("Expecting: nil, received: %+v", received)
+		t.Errorf("Expecting: <nil>, received: <%+v>", received)
 	}
+}
+
+func TestPathItemListInit(t *testing.T) {
+	list := NewPathItemList()
+	list2 := NewPathItemList()
+	node1 := NewPathItems([]string{"path1"})
+	node2 := NewPathItems([]string{"path2"})
+	node3 := NewPathItems([]string{"path3"})
+	list.PushFront(node3)
+	list.PushFront(node2)
+	list.PushFront(node1)
+	if !reflect.DeepEqual(list2, list.Init()) {
+		t.Errorf("Expecting: <%+v>, received: <%+v>", list2, list.Init())
+	}
+}
+
+func TestPathItemListNewPathItemList(t *testing.T) {
+	list := NewPathItemList()
+	list2 := new(PathItemList).Init()
+	if !reflect.DeepEqual(list2, list) {
+		t.Errorf("Expecting: <%+v>, received: <%+v>", list2, list)
+	}
+}
+
+func TestPathItemListLen(t *testing.T) {
+	list := NewPathItemList()
+	node1 := NewPathItems([]string{"path1"})
+	node2 := NewPathItems([]string{"path2"})
+	node3 := NewPathItems([]string{"path3"})
+	list.PushFront(node3)
+	list.PushFront(node2)
+	list.PushFront(node1)
+	if !reflect.DeepEqual(3, list.Len()) {
+		t.Errorf("Expecting: <3>, received: <%+v>", list.Init())
+	}
+}
+
+func TestPathItemListFront(t *testing.T) {
+	list := NewPathItemList()
+	node1 := NewPathItems([]string{"path1"})
+	node2 := NewPathItems([]string{"path2"})
+	list.PushBack(node1)
+	list.PushBack(node2)
+	if !reflect.DeepEqual("path1", list.Front().Value.String()) {
+		t.Errorf("Expecting: <path1>, received: <%+v>", list.Front().Value.String())
+	}
+
 }
 
 func TestPathItemListFrontNil(t *testing.T) {
 	var e PathItemList
 	received := e.Front()
 	if received != nil {
-		t.Errorf("Expecting: nil, received: %+v", received)
+		t.Errorf("Expecting: <nil>, received: <%+v>", received)
 	}
+}
+
+func TestPathItemListBack(t *testing.T) {
+	list := NewPathItemList()
+	node1 := NewPathItems([]string{"path1"})
+	node2 := NewPathItems([]string{"path2"})
+	list.PushBack(node1)
+	list.PushBack(node2)
+	if !reflect.DeepEqual("path2", list.Back().Value.String()) {
+		t.Errorf("Expecting: <path2>, received: <%+v>", list.Back().Value.String())
+	}
+
 }
 
 func TestPathItemListBackNil(t *testing.T) {
 	var e PathItemList
 	received := e.Back()
 	if received != nil {
-		t.Errorf("Expecting: nil, received: %+v", received)
+		t.Errorf("Expecting: <nil>, received: <%+v>", received)
 	}
 }
 
-/*
-func TestPathItemListNext(t *testing.T) {
+func TestPathItemListLazyInit(t *testing.T) {
+	list := NewPathItemList()
+	list2 := NewPathItemList()
+	list.root.next = nil
+	list.lazyInit()
+	if !reflect.DeepEqual(list2, list) {
+		t.Errorf("Expecting: <%+v>, received: <%+v>", list2, list)
+	}
+}
+
+func TestPathItemListInsert(t *testing.T) {
 	list := NewPathItemList()
 	node1 := NewPathItems([]string{"path1"})
 	node2 := NewPathItems([]string{"path2"})
-	node3 := NewPathItems([]string{"path3"})
-	list.PushFront(node1)
-	list.PushFront(node2)
-	list.PushFront(node3)
-	fmt.Println(list.Back().Value.String())
-	fmt.Println(list.Back().Prev().Value.String())
-
+	list.PushBack(node1)
+	list.PushBack(node2)
+	list.insert(list.Front(), list.Back())
+	if !reflect.DeepEqual("path1", list.Back().Value.String()) {
+		t.Errorf("Expecting: <path1>, received: <%+v>", list.Back().Value.String())
+	}
 }
-
-*/
