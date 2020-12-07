00fdb5f5807a5c2857e7d2d3d8c30ca2e0cb8324
porosnicuadrian <adrian.porosnicu@itsyscom.com>
2020-12-07 12:55:40 +0200

    Covered utils.go to 100%

--- utils/file_it_test.go
+++ utils/file_it_test.go
@@ -1,3 +1,5 @@
+// +build integration
+
 /*
 Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
 Copyright (C) ITsysCOM GmbH
@@ -19,14 +21,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 package utils
 
 import (
+	"fmt"
 	"os"
+	"path"
 	"testing"
+
+	"gopkg.in/fsnotify.v1"
 )
 
 var (
 	testsWatchDir = []func(t *testing.T){
-		testWatchDir,
-		testWatchDirNoError,
+		testWatchWatcherError,
+		testWatchWatcherEvents,
+		testWatchDirValidPath,
+		testWatchDirInvalidPath,
+		testWatchNewWatcherError,
 	}
 )
 
@@ -36,27 +45,89 @@ func TestFileIT(t *testing.T) {
 	}
 }
 
-func testWatchDir(t *testing.T) {
-	stopWatching := make(chan struct{}, 1)
-	close(stopWatching)
-	flPath := "/tmp/testWatchDir"
-	if err := os.MkdirAll(flPath, 0777); err != nil {
-		t.Error(err)
-	}
-	if err := WatchDir(flPath, nil, "randomID", stopWatching); err != nil {
-		t.Error(err)
-	}
-
-	if err := os.RemoveAll(flPath); err != nil {
+func testWatchWatcherError(t *testing.T) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
 		t.Fatal(err)
+	}
+	watcher.Events = make(chan fsnotify.Event, 1)
+	watcher.Errors = make(chan error, 1)
+
+	chanErr := fmt.Errorf("")
+	watcher.Errors <- chanErr
+	stopWatching := make(chan struct{}, 1)
+	if err := watch(EmptyString, EmptyString, nil, watcher, stopWatching); err != chanErr {
+		t.Error(err)
 	}
 }
 
-func testWatchDirNoError(t *testing.T) {
+func testWatchWatcherEvents(t *testing.T) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		t.Fatal(err)
+	}
+	watcher.Events = make(chan fsnotify.Event, 1)
+	watcher.Errors = make(chan error, 1)
+
+	watcher.Events <- fsnotify.Event{
+		Name: "/tmp/file.txt",
+		Op:   fsnotify.Create,
+	}
 	stopWatching := make(chan struct{}, 1)
-	flPath := "/tmp/inexistentDir"
-	expectedErr := "no such file or directory"
-	if err := WatchDir(flPath, nil, "randomID", stopWatching); err == nil || err.Error() != expectedErr {
-		t.Errorf("Expected %+v, received %+v", expectedErr, err)
+	f := func(itmPath, itmID string) error {
+		close(stopWatching)
+		if itmPath != "/tmp" || itmID != "file.txt" {
+			t.Errorf("Invalid directory or file")
+		}
+		return fmt.Errorf("Can't match path")
+	}
+	expected := "Can't match path"
+	if err := watch(EmptyString, EmptyString, f, watcher, stopWatching); err == nil || err.Error() != expected {
+		t.Errorf("Expected %+v, received %+v", expected, err)
+	}
+}
+
+func testWatchDirValidPath(t *testing.T) {
+	flPath := "/tmp/testWatchDirValidPath/"
+	if err := os.MkdirAll(flPath, 0777); err != nil {
+		t.Error(err)
+	}
+
+	newFile, err := os.Create(path.Join(flPath, "file.txt"))
+	if err != nil {
+		t.Error(err)
+	}
+	newFile.Close()
+
+	stopWatching := make(chan struct{}, 1)
+	if err := WatchDir(path.Join(flPath, "file.txt"), nil, EmptyString, stopWatching); err != nil {
+		t.Error(err)
+	}
+
+	if err := os.Remove(path.Join(flPath, "file.txt")); err != nil {
+		t.Error(err)
+	}
+	if err := os.Remove(flPath); err != nil {
+		t.Error(err)
+	}
+}
+
+func testWatchDirInvalidPath(t *testing.T) {
+	flPath := "tmp/testWatchDirInvalidPath"
+	stopWatching := make(chan struct{}, 1)
+	expected := "no such file or directory"
+	if err := WatchDir(flPath, nil, EmptyString, stopWatching); err == nil || err.Error() != expected {
+		t.Errorf("Expected %+v, received %+v", expected, err)
+	}
+}
+
+func testWatchNewWatcherError(t *testing.T) {
+	newWatcher := func() (*fsnotify.Watcher, error) {
+		return nil, fmt.Errorf("Invalid watcher")
+	}
+	stopWatching := make(chan struct{}, 1)
+	expected := "Invalid watcher"
+	if err := watchDir(EmptyString, nil, EmptyString, stopWatching, newWatcher); err == nil || err.Error() != expected {
+		t.Errorf("Expected %+v, received %+v", expected, err)
 	}
 }
