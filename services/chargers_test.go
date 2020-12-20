881ca920595bd301b00a074eaaee22004610126b
andronache <andrei.andronache@itsyscom.com>
2020-12-20 14:58:01 +0200

    Coverage tests in services

--- services/chargers_test.go
+++ services/chargers_test.go
@@ -1,5 +1,3 @@
-// +build integration
-
 /*
 Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
 Copyright (C) ITsysCOM GmbH
@@ -20,70 +18,54 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 package services
 
 import (
-	"reflect"
 	"sync"
 	"testing"
+
+	"github.com/cgrates/rpcclient"
 
 	"github.com/cgrates/cgrates/config"
 	"github.com/cgrates/cgrates/cores"
 	"github.com/cgrates/cgrates/engine"
 	"github.com/cgrates/cgrates/utils"
-	"github.com/cgrates/rpcclient"
 )
 
-//TestCoreSCoverage for cover testing
-func TestCoreSCoverage(t *testing.T) {
+//TestChargerSCoverage for cover testing
+func TestChargerSCoverage(t *testing.T) {
 	cfg := config.NewDefaultCGRConfig()
-	caps := engine.NewCaps(1, "test_caps")
-	server := cores.NewServer(nil)
-	internalCoreSChan := make(chan rpcclient.ClientConnector, 1)
+	cfg.AttributeSCfg().Enabled = true
+	shdChan := utils.NewSyncedChan()
+	chS := engine.NewCacheS(cfg, nil, nil)
 	filterSChan := make(chan *engine.FilterS, 1)
 	filterSChan <- nil
-	shdChan := utils.NewSyncedChan()
 	srvDep := map[string]*sync.WaitGroup{utils.DataDB: new(sync.WaitGroup)}
+	server := cores.NewServer(nil)
+	db := NewDataDBService(cfg, nil, srvDep)
 	anz := NewAnalyzerService(cfg, server, filterSChan, shdChan, make(chan rpcclient.ClientConnector, 1), srvDep)
-	srv := NewCoreService(cfg, caps, server,
-		internalCoreSChan, anz, srvDep)
-	if srv == nil {
-		t.Errorf("\nExpecting <nil>,\n Received <%+v>", utils.ToJSON(srv))
+	chrS := &ChargerService{
+		connChan:    make(chan rpcclient.ClientConnector, 1),
+		cfg:         cfg,
+		dm:          db,
+		cacheS:      chS,
+		filterSChan: filterSChan,
+		server:      server,
+		connMgr:     nil,
+		anz:         anz,
+		srvDep:      srvDep,
 	}
-	if srv.IsRunning() {
+	if chrS.IsRunning() {
 		t.Errorf("Expected service to be down")
 	}
-	srv.cS = &cores.CoreService{}
-	if !srv.IsRunning() {
+
+	chrS.chrS = &engine.ChargerService{}
+	if !chrS.IsRunning() {
 		t.Errorf("Expected service to be running")
 	}
-	err := srv.Start()
+	err := chrS.Start()
 	if err == nil || err != utils.ErrServiceAlreadyRunning {
 		t.Errorf("\nExpecting <%+v>,\n Received <%+v>", utils.ErrServiceAlreadyRunning, err)
 	}
-	serviceName := srv.ServiceName()
-	if !reflect.DeepEqual(serviceName, utils.CoreS) {
-		t.Errorf("\nExpecting <%+v>,\n Received <%+v>", utils.CoreS, serviceName)
+	err = chrS.Reload()
+	if err != nil {
+		t.Errorf("\nExpecting <nil>,\n Received <%+v>", err)
 	}
-	shouldRun := srv.ShouldRun()
-	if !reflect.DeepEqual(shouldRun, true) {
-		t.Errorf("\nExpecting <true>,\n Received <%+v>", shouldRun)
-	}
-	rld := srv.Reload()
-	if rld != nil {
-		t.Errorf("\nExpecting <nil>,\n Received <%+v>", rld)
-	}
-	getCoreS := srv.GetCoreS()
-	if getCoreS == nil {
-		t.Errorf("\nExpecting not <nil>,\n Received <%+v>", getCoreS)
-	}
-	//populates connChan with something in order to call the shutdown function
-	chS := engine.NewCacheS(cfg, nil, nil)
-	srv.connChan <- chS
-	srv.stopChan = make(chan struct{})
-	getShut := srv.Shutdown()
-	if getShut != nil {
-		t.Errorf("\nExpecting not <nil>,\n Received <%+v>", getShut)
-	}
-	if srv.IsRunning() {
-		t.Errorf("Expected service to be down")
-	}
-
 }
