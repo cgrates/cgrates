ac6442f9baecf8fe50e5a7d14587a14bf31f3a7f
andronache <andrei.andronache@itsyscom.com>
2020-12-20 14:58:01 +0200

    Coverage tests in services

--- services/cdrs_test.go
+++ services/cdrs_test.go
@@ -1,5 +1,3 @@
-// +build integration
-
 /*
 Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
 Copyright (C) ITsysCOM GmbH
@@ -25,78 +23,110 @@ import (
 	"testing"
 	"time"
 
-	"github.com/cgrates/rpcclient"
-
 	"github.com/cgrates/cgrates/config"
 	"github.com/cgrates/cgrates/cores"
 	"github.com/cgrates/cgrates/engine"
 	"github.com/cgrates/cgrates/servmanager"
 	"github.com/cgrates/cgrates/utils"
+	"github.com/cgrates/rpcclient"
 )
 
-func TestChargerSReload(t *testing.T) {
+//TestCdrsCoverage for cover testing
+func TestCdrsCoverage(t *testing.T) {
 	cfg := config.NewDefaultCGRConfig()
 
 	utils.Logger, _ = utils.Newlogger(utils.MetaSysLog, cfg.GeneralCfg().NodeID)
 	utils.Logger.SetLogLevel(7)
-	cfg.AttributeSCfg().Enabled = true
+	filterSChan := make(chan *engine.FilterS, 1)
+	filterSChan <- nil
 	shdChan := utils.NewSyncedChan()
 	shdWg := new(sync.WaitGroup)
 	chS := engine.NewCacheS(cfg, nil, nil)
-	close(chS.GetPrecacheChannel(utils.CacheAttributeProfiles))
-	close(chS.GetPrecacheChannel(utils.CacheAttributeFilterIndexes))
+
 	close(chS.GetPrecacheChannel(utils.CacheChargerProfiles))
 	close(chS.GetPrecacheChannel(utils.CacheChargerFilterIndexes))
-	filterSChan := make(chan *engine.FilterS, 1)
-	filterSChan <- nil
-	srvDep := map[string]*sync.WaitGroup{utils.DataDB: new(sync.WaitGroup)}
+
+	close(chS.GetPrecacheChannel(utils.CacheDestinations))
+	close(chS.GetPrecacheChannel(utils.CacheReverseDestinations))
+	close(chS.GetPrecacheChannel(utils.CacheRatingPlans))
+	close(chS.GetPrecacheChannel(utils.CacheRatingProfiles))
+	close(chS.GetPrecacheChannel(utils.CacheActions))
+	close(chS.GetPrecacheChannel(utils.CacheActionPlans))
+	close(chS.GetPrecacheChannel(utils.CacheAccountActionPlans))
+	close(chS.GetPrecacheChannel(utils.CacheActionTriggers))
+	close(chS.GetPrecacheChannel(utils.CacheSharedGroups))
+	close(chS.GetPrecacheChannel(utils.CacheTimings))
+
+	cfg.ChargerSCfg().Enabled = true
 	server := cores.NewServer(nil)
 	srvMngr := servmanager.NewServiceManager(cfg, shdChan, shdWg)
+	srvDep := map[string]*sync.WaitGroup{utils.DataDB: new(sync.WaitGroup)}
 	db := NewDataDBService(cfg, nil, srvDep)
+	cfg.StorDbCfg().Type = utils.INTERNAL
+	stordb := NewStorDBService(cfg, srvDep)
 	anz := NewAnalyzerService(cfg, server, filterSChan, shdChan, make(chan rpcclient.ClientConnector, 1), srvDep)
-	attrS := NewAttributeService(cfg, db, chS, filterSChan, server, make(chan rpcclient.ClientConnector, 1), anz, srvDep)
 	chrS := NewChargerService(cfg, db, chS, filterSChan, server, make(chan rpcclient.ClientConnector, 1), nil, anz, srvDep)
-	engine.NewConnManager(cfg, nil)
-	srvMngr.AddServices(attrS, chrS,
+	schS := NewSchedulerService(cfg, db, chS, filterSChan, server, make(chan rpcclient.ClientConnector, 1), nil, anz, srvDep)
+	ralS := NewRalService(cfg, chS, server,
+		make(chan rpcclient.ClientConnector, 1),
+		make(chan rpcclient.ClientConnector, 1),
+		shdChan, nil, anz, srvDep)
+	cdrsRPC := make(chan rpcclient.ClientConnector, 1)
+	cdrS := NewCDRServer(cfg, db, stordb, filterSChan, server,
+		cdrsRPC, nil, anz, srvDep)
+	srvMngr.AddServices(cdrS, ralS, schS, chrS,
 		NewLoaderService(cfg, db, filterSChan, server,
-			make(chan rpcclient.ClientConnector, 1), nil, anz, srvDep), db)
+			make(chan rpcclient.ClientConnector, 1), nil, anz, srvDep), db, stordb)
 	if err := srvMngr.StartServices(); err != nil {
 		t.Error(err)
 	}
-	if chrS.IsRunning() {
+	if cdrS.IsRunning() {
 		t.Errorf("Expected service to be down")
 	}
 	if db.IsRunning() {
 		t.Errorf("Expected service to be down")
 	}
+	if stordb.IsRunning() {
+		t.Errorf("Expected service to be down")
+	}
+
+	cfg.RalsCfg().Enabled = true
 	var reply string
 	if err := cfg.V1ReloadConfig(&config.ReloadArgs{
 		Path:    path.Join("/usr", "share", "cgrates", "conf", "samples", "tutmongo"),
-		Section: config.ChargerSCfgJson,
+		Section: config.CDRS_JSN,
 	}, &reply); err != nil {
 		t.Error(err)
 	} else if reply != utils.OK {
 		t.Errorf("Expecting OK ,received %s", reply)
 	}
-	time.Sleep(10 * time.Millisecond) //need to switch to gorutine
-	if !chrS.IsRunning() {
+	select {
+	case d := <-cdrsRPC:
+		cdrsRPC <- d
+	case <-time.After(time.Second):
+		t.Fatal("It took to long to reload the cache")
+	}
+	if !cdrS.IsRunning() {
 		t.Errorf("Expected service to be running")
 	}
 	if !db.IsRunning() {
 		t.Errorf("Expected service to be running")
 	}
-	err := chrS.Start()
+	if !stordb.IsRunning() {
+		t.Errorf("Expected service to be running")
+	}
+	err := cdrS.Start()
 	if err == nil || err != utils.ErrServiceAlreadyRunning {
 		t.Errorf("\nExpecting <%+v>,\n Received <%+v>", utils.ErrServiceAlreadyRunning, err)
 	}
-	err = chrS.Reload()
+	err = cdrS.Reload()
 	if err != nil {
 		t.Errorf("\nExpecting <nil>,\n Received <%+v>", err)
 	}
-	cfg.ChargerSCfg().Enabled = false
-	cfg.GetReloadChan(config.ChargerSCfgJson) <- struct{}{}
+	cfg.CdrsCfg().Enabled = false
+	cfg.GetReloadChan(config.CDRS_JSN) <- struct{}{}
 	time.Sleep(10 * time.Millisecond)
-	if chrS.IsRunning() {
+	if cdrS.IsRunning() {
 		t.Errorf("Expected service to be down")
 	}
 	shdChan.CloseOnce()
---
+++ services/cores_test.go
@@ -0,0 +1,87 @@
+/*
+Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
+Copyright (C) ITsysCOM GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+package services
+
+import (
+	"reflect"
+	"sync"
+	"testing"
+
+	"github.com/cgrates/cgrates/config"
+	"github.com/cgrates/cgrates/cores"
+	"github.com/cgrates/cgrates/engine"
+	"github.com/cgrates/cgrates/utils"
+	"github.com/cgrates/rpcclient"
+)
+
+//TestCoreSCoverage for cover testing
+func TestCoreSCoverage(t *testing.T) {
+	cfg := config.NewDefaultCGRConfig()
+	caps := engine.NewCaps(1, "test_caps")
+	server := cores.NewServer(nil)
+	internalCoreSChan := make(chan rpcclient.ClientConnector, 1)
+	filterSChan := make(chan *engine.FilterS, 1)
+	filterSChan <- nil
+	shdChan := utils.NewSyncedChan()
+	srvDep := map[string]*sync.WaitGroup{utils.DataDB: new(sync.WaitGroup)}
+	anz := NewAnalyzerService(cfg, server, filterSChan, shdChan, make(chan rpcclient.ClientConnector, 1), srvDep)
+	srv := NewCoreService(cfg, caps, server,
+		internalCoreSChan, anz, srvDep)
+	if srv == nil {
+		t.Errorf("\nExpecting <nil>,\n Received <%+v>", utils.ToJSON(srv))
+	}
+	if srv.IsRunning() {
+		t.Errorf("Expected service to be down")
+	}
+	srv.cS = &cores.CoreService{}
+	if !srv.IsRunning() {
+		t.Errorf("Expected service to be running")
+	}
+	err := srv.Start()
+	if err == nil || err != utils.ErrServiceAlreadyRunning {
+		t.Errorf("\nExpecting <%+v>,\n Received <%+v>", utils.ErrServiceAlreadyRunning, err)
+	}
+	serviceName := srv.ServiceName()
+	if !reflect.DeepEqual(serviceName, utils.CoreS) {
+		t.Errorf("\nExpecting <%+v>,\n Received <%+v>", utils.CoreS, serviceName)
+	}
+	shouldRun := srv.ShouldRun()
+	if !reflect.DeepEqual(shouldRun, true) {
+		t.Errorf("\nExpecting <true>,\n Received <%+v>", shouldRun)
+	}
+	rld := srv.Reload()
+	if rld != nil {
+		t.Errorf("\nExpecting <nil>,\n Received <%+v>", rld)
+	}
+	getCoreS := srv.GetCoreS()
+	if getCoreS == nil {
+		t.Errorf("\nExpecting not <nil>,\n Received <%+v>", getCoreS)
+	}
+	//populates connChan with something in order to call the shutdown function
+	chS := engine.NewCacheS(cfg, nil, nil)
+	srv.connChan <- chS
+	srv.stopChan = make(chan struct{})
+	getShut := srv.Shutdown()
+	if getShut != nil {
+		t.Errorf("\nExpecting not <nil>,\n Received <%+v>", getShut)
+	}
+	if srv.IsRunning() {
+		t.Errorf("Expected service to be down")
+	}
+
+}
