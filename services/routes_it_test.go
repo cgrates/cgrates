e236958dbf82c0c074d53cf12483ce3aa09b34d4
andronache <andrei.andronache@itsyscom.com>
2021-02-03 11:36:46 +0200

    Integration tests in services

--- services/routes_it_test.go
+++ services/routes_it_test.go
@@ -19,22 +19,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 package services
 
-/*
+import (
+	"path"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/cgrates/cgrates/config"
+	"github.com/cgrates/cgrates/cores"
+	"github.com/cgrates/cgrates/engine"
+	"github.com/cgrates/cgrates/servmanager"
+	"github.com/cgrates/cgrates/utils"
+	"github.com/cgrates/rpcclient"
+)
+
 func TestRouteSReload(t *testing.T) {
 	// utils.Logger.SetLogLevel(7)
 	cfg := config.NewDefaultCGRConfig()
 
 	utils.Logger, _ = utils.Newlogger(utils.MetaSysLog, cfg.GeneralCfg().NodeID)
 	utils.Logger.SetLogLevel(7)
-	cfg.ThresholdSCfg().Enabled = true
 	filterSChan := make(chan *engine.FilterS, 1)
 	filterSChan <- nil
 	shdChan := utils.NewSyncedChan()
 	shdWg := new(sync.WaitGroup)
 	chS := engine.NewCacheS(cfg, nil, nil)
-	close(chS.GetPrecacheChannel(utils.CacheThresholdProfiles))
-	close(chS.GetPrecacheChannel(utils.CacheThresholds))
-	close(chS.GetPrecacheChannel(utils.CacheThresholdFilterIndexes))
 	close(chS.GetPrecacheChannel(utils.CacheRouteProfiles))
 	close(chS.GetPrecacheChannel(utils.CacheRouteFilterIndexes))
 	server := cores.NewServer(nil)
@@ -42,15 +51,14 @@ func TestRouteSReload(t *testing.T) {
 	srvDep := map[string]*sync.WaitGroup{utils.DataDB: new(sync.WaitGroup)}
 	anz := NewAnalyzerService(cfg, server, filterSChan, shdChan, make(chan rpcclient.ClientConnector, 1), srvDep)
 	db := NewDataDBService(cfg, nil, srvDep)
-	tS := NewThresholdService(cfg, db, chS, filterSChan, server, make(chan rpcclient.ClientConnector, 1), anz, srvDep)
-	reS := NewRouteService(cfg, db, chS, filterSChan, server, make(chan rpcclient.ClientConnector, 1), nil, anz, srvDep)
+	routeS := NewRouteService(cfg, db, chS, filterSChan, server, make(chan rpcclient.ClientConnector, 1), nil, anz, srvDep)
 	engine.NewConnManager(cfg, nil)
-	srvMngr.AddServices(tS, reS,
+	srvMngr.AddServices(routeS,
 		NewLoaderService(cfg, db, filterSChan, server, make(chan rpcclient.ClientConnector, 1), nil, anz, srvDep), db)
 	if err := srvMngr.StartServices(); err != nil {
 		t.Error(err)
 	}
-	if reS.IsRunning() {
+	if routeS.IsRunning() {
 		t.Errorf("Expected service to be down")
 	}
 	if db.IsRunning() {
@@ -58,7 +66,7 @@ func TestRouteSReload(t *testing.T) {
 	}
 	var reply string
 	if err := cfg.V1ReloadConfig(&config.ReloadArgs{
-		Path:    path.Join("/usr", "share", "cgrates", "conf", "samples", "tutmongo"),
+		Path:    path.Join("/usr", "share", "cgrates", "conf", "samples", "cluelrn"),
 		Section: config.RouteSJson,
 	}, &reply); err != nil {
 		t.Error(err)
@@ -66,27 +74,27 @@ func TestRouteSReload(t *testing.T) {
 		t.Errorf("Expecting OK ,received %s", reply)
 	}
 	time.Sleep(10 * time.Millisecond) //need to switch to gorutine
-	if !reS.IsRunning() {
-		t.Errorf("Expected service to be running")
+	if !routeS.IsRunning() {
+		t.Fatalf("Expected service to be running")
 	}
 	if !db.IsRunning() {
 		t.Errorf("Expected service to be running")
 	}
-	err := reS.Start()
+
+	err := routeS.Start()
 	if err == nil || err != utils.ErrServiceAlreadyRunning {
 		t.Errorf("\nExpecting <%+v>,\n Received <%+v>", utils.ErrServiceAlreadyRunning, err)
 	}
-	err = reS.Reload()
+	err = routeS.Reload()
 	if err != nil {
 		t.Errorf("\nExpecting <nil>,\n Received <%+v>", err)
 	}
 	cfg.RouteSCfg().Enabled = false
 	cfg.GetReloadChan(config.RouteSJson) <- struct{}{}
 	time.Sleep(10 * time.Millisecond)
-	if reS.IsRunning() {
+	if routeS.IsRunning() {
 		t.Errorf("Expected service to be down")
 	}
 	shdChan.CloseOnce()
 	time.Sleep(10 * time.Millisecond)
 }
-*/
