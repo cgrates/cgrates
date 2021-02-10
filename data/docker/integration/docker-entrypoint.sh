d27404ab72ec8834b66b970b38d69e6e49a3a53a
Trial97 <alexandru.tripon@itsyscom.com>
2021-02-10 17:56:44 +0200

    Updated drone ansible script

--- data/docker/integration/docker-entrypoint.sh
+++ data/docker/integration/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 set -ev
 
 # start basic subsystems
+export KAFKA_HEAP_OPTS="-Xmx100M -Xms100M"
 /kafka/bin/zookeeper-server-start.sh -daemon /kafka/config/zookeeper.properties
 /kafka/bin/kafka-server-start.sh -daemon /kafka/config/server.properties
 
@@ -12,6 +13,26 @@ redis-server /etc/redis/redis.conf &
 MYSQL_ROOT_PASSWORD="CGRateS.org" /scripts/mariadb-ep.sh mysqld > /logs/mariadb_script.log 2>&1
 rabbitmq-server > /logs/rabbitmq.log  2>&1 &
 
+
+START_TIMEOUT=600
+
+start_timeout_exceeded=false
+count=0
+step=10
+while netstat -lnt | awk '$4 ~ /:9092$/ {exit 1}'; do
+    echo "waiting for kafka to be ready"
+    sleep $step;
+    count=$((count + step))
+    if [ $count -gt $START_TIMEOUT ]; then
+        start_timeout_exceeded=true
+        break
+    fi
+done
+
+if $start_timeout_exceeded; then
+    echo "Not able to auto-create topic (waited for $START_TIMEOUT sec)"
+    exit 1
+fi
 
 /kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic cgrates
 /kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic cgrates_cdrs
