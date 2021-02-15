106fbcc443871183f6b6b1c39d1b747f6127ac55
porosnicuadrian <adrian.porosnicu@itsyscom.com>
2021-02-15 11:02:54 +0200

    Removed BalanceBlocker from db tables and csv

--- data/storage/mysql/create_tariffplan_tables.sql
+++ data/storage/mysql/create_tariffplan_tables.sql
@@ -549,7 +549,6 @@ CREATE TABLE tp_account_profiles (
   `balance_id` varchar(64) NOT NULL,
   `balance_filter_ids` varchar(64) NOT NULL,
   `balance_weights` decimal(8,2) NOT NULL,
-  `balance_blocker` BOOLEAN NOT NULL,
   `balance_type` varchar(64) NOT NULL,
   `balance_units` decimal(16,4) NOT NULL,
   `balance_unit_factors` varchar(64) NOT NULL,
