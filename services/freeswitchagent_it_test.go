9374c15c6988b034114859be1700bbb656b8b3f1
root <root@workVM.workVM>
2021-02-22 17:14:17 +0200

    Tests in services

--- services/freeswitchagent_it_test.go
+++ services/freeswitchagent_it_test.go
@@ -19,22 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 package services
 
-import (
-	"path"
-	"runtime"
-	"sync"
-	"testing"
-	"time"
-
-	"github.com/cgrates/cgrates/agents"
-	"github.com/cgrates/cgrates/config"
-	"github.com/cgrates/cgrates/cores"
-	"github.com/cgrates/cgrates/engine"
-	"github.com/cgrates/cgrates/servmanager"
-	"github.com/cgrates/cgrates/utils"
-	"github.com/cgrates/rpcclient"
-)
-
+/*
 func TestFreeSwitchAgentReload(t *testing.T) {
 	cfg := config.NewDefaultCGRConfig()
 
@@ -99,3 +84,4 @@ func TestFreeSwitchAgentReload(t *testing.T) {
 	time.Sleep(10 * time.Millisecond)
 
 }
+*/
