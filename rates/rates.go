1642df4b718d9e3cdaa9b8d2d49217580ec6c175
andronache <andrei.andronache@itsyscom.com>
2021-02-25 17:03:44 +0200

    Fixed cover test

--- rates/rates.go
+++ rates/rates.go
@@ -58,8 +58,9 @@ func (rS *RateS) ListenAndServe(stopChan, cfgRld chan struct{}) {
 }
 
 // Shutdown is called to shutdown the service
-func (rS *RateS) Shutdown() {
+func (rS *RateS) Shutdown() (err error) {
 	utils.Logger.Info(fmt.Sprintf("<%s> shutdown <%s>", utils.CoreS, utils.RateS))
+	return
 }
 
 // Call implements rpcclient.ClientConnector interface for internal RPC
