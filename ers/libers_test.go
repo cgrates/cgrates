43cb84591e83e1e5e6e37ea15ce4062d5d161542
andronache <andrei.andronache@itsyscom.com>
2021-03-09 15:19:04 +0200

    Finished tests for libers.go

---
+++ ers/libers_test.go
@@ -0,0 +1,37 @@
+/*
+Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
+Copyright (C) ITsysCOM GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+package ers
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetProcessOptions(t *testing.T) {
+	opts := map[string]interface{}{
+		"testKeyProcessed": "testValue",
+	}
+	result := getProcessOptions(opts)
+	expected := map[string]interface{}{
+		"testKey": "testValue",
+	}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("\nExpected <%+v>, \nReceived <%+v>", expected, result)
+	}
+}
