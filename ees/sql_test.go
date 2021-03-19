69ef84d9784934eb4f520524a09ae66ace73a603
nickolasdaniel <nickolas.filip@itsyscom.com>
2021-03-19 17:09:38 +0200

    Coverage tests for ees

--- ees/sql_test.go
+++ ees/sql_test.go
@@ -19,10 +19,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 package ees
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
+	"github.com/cgrates/cgrates/config"
+	"github.com/cgrates/cgrates/engine"
 	"github.com/cgrates/cgrates/utils"
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 )
 
 func TestSqlID(t *testing.T) {
@@ -48,5 +53,104 @@ func TestSqlGetMetrics(t *testing.T) {
 
 	if rcv := sqlEe.GetMetrics(); !reflect.DeepEqual(rcv, sqlEe.dc) {
 		t.Errorf("Expected %+v but got %+v", utils.ToJSON(rcv), utils.ToJSON(sqlEe.dc))
+	}
+}
+
+func TestNewSQLeUrl(t *testing.T) {
+	cgrCfg := config.NewDefaultCGRConfig()
+	cgrCfg.EEsCfg().Exporters[0].Opts[utils.SQLTableName] = "expTable"
+	cgrCfg.EEsCfg().Exporters[0].Opts[utils.SQLDBName] = "postgres"
+	cgrCfg.EEsCfg().Exporters[0].Opts[utils.SQLSSLMode] = "test"
+	newIDb := engine.NewInternalDB(nil, nil, true)
+	newDM := engine.NewDataManager(newIDb, cgrCfg.CacheCfg(), nil)
+	filterS := engine.NewFilterS(cgrCfg, nil, newDM)
+	dc, err := newEEMetrics(utils.FirstNonEmpty(
+		"Local",
+		utils.EmptyString,
+	))
+	if err != nil {
+		t.Error(err)
+	}
+	sqlEe := &SQLEe{id: cgrCfg.EEsCfg().Exporters[0].ID,
+		cgrCfg: cgrCfg, cfgIdx: 0, filterS: filterS, dc: dc}
+	_, err = sqlEe.NewSQLEeUrl(cgrCfg)
+	errExpect := "db type <> not supported"
+	if err == nil || err.Error() != errExpect {
+		t.Errorf("Expected %v but received %v", errExpect, err)
+	}
+}
+
+func TestNewSQLeUrlSQL(t *testing.T) {
+	cgrCfg := config.NewDefaultCGRConfig()
+	cgrCfg.EEsCfg().Exporters[0].Opts[utils.SQLTableName] = "expTable"
+	cgrCfg.EEsCfg().Exporters[0].Opts[utils.SQLDBName] = "mysql"
+	cgrCfg.EEsCfg().Exporters[0].ExportPath = `mysql://cgrates:CGRateS.org@127.0.0.1:3306`
+	newIDb := engine.NewInternalDB(nil, nil, true)
+	newDM := engine.NewDataManager(newIDb, cgrCfg.CacheCfg(), nil)
+	filterS := engine.NewFilterS(cgrCfg, nil, newDM)
+	dc, err := newEEMetrics(utils.FirstNonEmpty(
+		"Local",
+		utils.EmptyString,
+	))
+	if err != nil {
+		t.Error(err)
+	}
+	sqlEe := &SQLEe{id: cgrCfg.EEsCfg().Exporters[0].ID,
+		cgrCfg: cgrCfg, cfgIdx: 0, filterS: filterS, dc: dc}
+	dialectExpect := mysql.Open(fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&loc=Local&parseTime=true&sql_mode='ALLOW_INVALID_DATES'",
+		"cgrates", "CGRateS.org", "127.0.0.1", "3306", "mysql"))
+	if dialect, err := sqlEe.NewSQLEeUrl(cgrCfg); err != nil {
+		t.Error(err)
+	} else if !reflect.DeepEqual(dialect, dialectExpect) {
+		t.Errorf("Expected %v but received %v", utils.ToJSON(dialectExpect), utils.ToJSON(dialect))
+	}
+}
+
+func TestNewSQLeUrlPostgres(t *testing.T) {
+	cgrCfg := config.NewDefaultCGRConfig()
+	cgrCfg.EEsCfg().Exporters[0].Opts[utils.SQLTableName] = "expTable"
+	cgrCfg.EEsCfg().Exporters[0].Opts[utils.SQLDBName] = "postgres"
+	cgrCfg.EEsCfg().Exporters[0].ExportPath = `postgres://cgrates:CGRateS.org@127.0.0.1:3306`
+	newIDb := engine.NewInternalDB(nil, nil, true)
+	newDM := engine.NewDataManager(newIDb, cgrCfg.CacheCfg(), nil)
+	filterS := engine.NewFilterS(cgrCfg, nil, newDM)
+	dc, err := newEEMetrics(utils.FirstNonEmpty(
+		"Local",
+		utils.EmptyString,
+	))
+	if err != nil {
+		t.Error(err)
+	}
+	sqlEe := &SQLEe{id: cgrCfg.EEsCfg().Exporters[0].ID,
+		cgrCfg: cgrCfg, cfgIdx: 0, filterS: filterS, dc: dc}
+	dialectExpect := postgres.Open(fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=%s",
+		"127.0.0.1", "3306", "postgres", "cgrates", "CGRateS.org", utils.SQLDefaultSSLMode))
+	if dialect, err := sqlEe.NewSQLEeUrl(cgrCfg); err != nil {
+		t.Error(err)
+	} else if !reflect.DeepEqual(dialect, dialectExpect) {
+		t.Errorf("Expected %v but received %v", utils.ToJSON(dialectExpect), utils.ToJSON(dialect))
+	}
+}
+
+func TestNewSQLeExportPathError(t *testing.T) {
+	cgrCfg := config.NewDefaultCGRConfig()
+	cgrCfg.EEsCfg().Exporters[0].Opts[utils.SQLTableName] = "expTable"
+	cgrCfg.EEsCfg().Exporters[0].Opts[utils.SQLDBName] = "postgres"
+	cgrCfg.EEsCfg().Exporters[0].ExportPath = ":foo"
+	newIDb := engine.NewInternalDB(nil, nil, true)
+	newDM := engine.NewDataManager(newIDb, cgrCfg.CacheCfg(), nil)
+	filterS := engine.NewFilterS(cgrCfg, nil, newDM)
+	dc, err := newEEMetrics(utils.FirstNonEmpty(
+		"Local",
+		utils.EmptyString,
+	))
+	if err != nil {
+		t.Error(err)
+	}
+	sqlEe := &SQLEe{id: cgrCfg.EEsCfg().Exporters[0].ID,
+		cgrCfg: cgrCfg, cfgIdx: 0, filterS: filterS, dc: dc}
+	errExpect := `parse ":foo": missing protocol scheme`
+	if _, err := sqlEe.NewSQLEeUrl(cgrCfg); err == nil || err.Error() != errExpect {
+		t.Errorf("Expected %v but received %v", errExpect, err)
 	}
 }
