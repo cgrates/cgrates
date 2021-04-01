cc39a99a2c50a7c2097f7a50e0e6cba7b0772811
andronache <andrei.andronache@itsyscom.com>
2021-04-01 09:47:45 +0300

    Fixed integration test

--- engine/datamanager.go
+++ engine/datamanager.go
@@ -288,6 +288,13 @@ func (dm *DataManager) CacheDataFromDB(prfx string, ids []string, mustBeCached b
 				return
 			}
 			_, err = dm.GetIndexes(utils.CacheActionProfilesFilterIndexes, tntCtx, idxKey, false, true)
+		case utils.FilterIndexPrfx:
+			idx := strings.LastIndexByte(dataID, utils.InInFieldSep[0])
+			if idx < 0 {
+				err = fmt.Errorf("WRONG_IDX_KEY_FORMAT<%s>", dataID)
+				return
+			}
+			_, err = dm.GetIndexes(utils.CacheReverseFilterIndexes, dataID[:idx], dataID[idx+1:], false, true)
 		case utils.LoadIDPrefix:
 			_, err = dm.GetItemLoadIDs(utils.EmptyString, true)
 		case utils.MetaAPIBan:
