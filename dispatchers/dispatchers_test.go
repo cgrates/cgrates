7339fd0e82c706590e46da925f9bf70835b85c7c
andronache <andrei.andronache@itsyscom.com>
2021-04-21 13:03:29 +0300

    Test for authorize in dispatchers

--- dispatchers/dispatchers_test.go
+++ dispatchers/dispatchers_test.go
@@ -19,7 +19,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 package dispatchers
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/cgrates/cgrates/config"
 	"github.com/cgrates/cgrates/engine"
@@ -270,6 +272,95 @@ func TestDispatcherServiceAuthorizeEventError3(t *testing.T) {
 	rply := &engine.AttrSProcessEventReply{}
 	err := dsp.authorizeEvent(ev, rply)
 	if err != nil {
+		t.Errorf("\nExpected <%+v>, \nReceived <%+v>", nil, err)
+	}
+	engine.Cache = cacheInit
+}
+
+type mockTypeCon3 struct{}
+
+func (*mockTypeCon3) Call(serviceMethod string, args, reply interface{}) error {
+	fmt.Println("yayCall")
+	eVreply := &engine.AttrSProcessEventReply{
+		CGREvent: &utils.CGREvent{
+			Tenant: "testTenant",
+			ID:     "testID",
+			Time:   nil,
+			Event: map[string]interface{}{
+				utils.APIMethods: "yes",
+			},
+			APIOpts: nil,
+		},
+	}
+	*reply.(*engine.AttrSProcessEventReply) = *eVreply
+	return nil
+}
+
+func TestDispatcherServiceAuthorizeError(t *testing.T) {
+	cacheInit := engine.Cache
+	cfg := config.NewDefaultCGRConfig()
+	cfg.DispatcherSCfg().AttributeSConns = []string{utils.ConcatenatedKey(utils.MetaInternal, utils.MetaAttributes)}
+	dm := engine.NewDataManager(nil, nil, nil)
+	chanRPC := make(chan rpcclient.ClientConnector, 1)
+	chanRPC <- new(mockTypeCon3)
+	rpcInt := map[string]chan rpcclient.ClientConnector{
+		utils.ConcatenatedKey(utils.MetaInternal, utils.MetaAttributes): chanRPC,
+	}
+	connMgr := engine.NewConnManager(cfg, rpcInt)
+
+	dsp := NewDispatcherService(dm, cfg, nil, connMgr)
+	value := &engine.DispatcherHost{
+		Tenant: "testTenant",
+		RemoteHost: &config.RemoteHost{
+			ID:          "testID",
+			Address:     rpcclient.InternalRPC,
+			Transport:   utils.MetaInternal,
+			Synchronous: false,
+			TLS:         false,
+		},
+	}
+	newCache := engine.NewCacheS(cfg, dm, nil)
+	engine.Cache = newCache
+	engine.Cache.SetWithoutReplicate(utils.CacheRPCConnections, "testID",
+		value, nil, true, utils.NonTransactional)
+	err := dsp.authorize(utils.APIMethods, "testTenant", "apikey", &time.Time{})
+	expected := "UNAUTHORIZED_API"
+	if err == nil || err.Error() != expected {
+		t.Errorf("\nExpected <%+v>, \nReceived <%+v>", nil, err)
+	}
+	engine.Cache = cacheInit
+}
+
+func TestDispatcherServiceAuthorizeError2(t *testing.T) {
+	cacheInit := engine.Cache
+	cfg := config.NewDefaultCGRConfig()
+	cfg.DispatcherSCfg().AttributeSConns = []string{utils.ConcatenatedKey(utils.MetaInternal, utils.MetaAttributes)}
+	dm := engine.NewDataManager(nil, nil, nil)
+	chanRPC := make(chan rpcclient.ClientConnector, 1)
+	chanRPC <- new(mockTypeCon3)
+	rpcInt := map[string]chan rpcclient.ClientConnector{
+		utils.ConcatenatedKey(utils.MetaInternal, utils.MetaAttributes): chanRPC,
+	}
+	connMgr := engine.NewConnManager(cfg, rpcInt)
+
+	dsp := NewDispatcherService(dm, cfg, nil, connMgr)
+	value := &engine.DispatcherHost{
+		Tenant: "testTenant",
+		RemoteHost: &config.RemoteHost{
+			ID:          "testID",
+			Address:     rpcclient.InternalRPC,
+			Transport:   utils.MetaInternal,
+			Synchronous: false,
+			TLS:         false,
+		},
+	}
+	newCache := engine.NewCacheS(cfg, dm, nil)
+	engine.Cache = newCache
+	engine.Cache.SetWithoutReplicate(utils.CacheRPCConnections, "testID",
+		value, nil, true, utils.NonTransactional)
+	err := dsp.authorize(utils.APIMethods, "testTenant", "apikey", &time.Time{})
+	expected := "UNAUTHORIZED_API"
+	if err == nil || err.Error() != expected {
 		t.Errorf("\nExpected <%+v>, \nReceived <%+v>", nil, err)
 	}
 	engine.Cache = cacheInit
