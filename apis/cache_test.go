96824bb87f51df264aeae8481f8f037c2c55c096
porosnicuadrian <adrian.porosnicu@itsyscom.com>
2021-04-28 18:03:48 +0300

    Finished cache unit tests with context

--- apis/cache_test.go
+++ apis/cache_test.go
@@ -22,6 +22,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/cgrates/birpc/context"
 	"github.com/cgrates/cgrates/config"
 	"github.com/cgrates/cgrates/engine"
 	"github.com/cgrates/cgrates/utils"
@@ -63,20 +64,20 @@ func TestCacheHasItemAndGetItem(t *testing.T) {
 	}
 }
 
-func TestCacheSetAndRemoveItems(t *testing.T) {
+func TestCacheSetAndRemoveItem(t *testing.T) {
 	cfg := config.NewDefaultCGRConfig()
 	data := engine.NewInternalDB(nil, nil, true)
 	cfg.AdminSCfg().CachesConns = []string{utils.ConcatenatedKey(utils.MetaInternal, utils.MetaCaches)}
 	dm := engine.NewDataManager(data, cfg.CacheCfg(), nil)
 	ch := engine.NewCacheS(cfg, dm, nil)
-	ch.SetWithoutReplicate(utils.CacheAttributeProfiles, "cgrates.org:TestCacheSetAndRemoveItems", nil, nil, true, utils.NonTransactional)
+	ch.SetWithoutReplicate(utils.CacheAttributeProfiles, "cgrates.org:TestCacheSetAndRemoveItem", nil, nil, true, utils.NonTransactional)
 	cache := NewCacheSv1(ch)
 
 	var replyBool bool
 	argsHasItem := &utils.ArgsGetCacheItemWithAPIOpts{
 		ArgsGetCacheItem: utils.ArgsGetCacheItem{
 			CacheID: utils.CacheAttributeProfiles,
-			ItemID:  "cgrates.org:TestCacheSetAndRemoveItems",
+			ItemID:  "cgrates.org:TestCacheSetAndRemoveItem",
 		},
 	}
 	if err := cache.HasItem(nil, argsHasItem, &replyBool); err != nil {
@@ -88,7 +89,7 @@ func TestCacheSetAndRemoveItems(t *testing.T) {
 	argsRemItm := &utils.ArgsGetCacheItemWithAPIOpts{
 		ArgsGetCacheItem: utils.ArgsGetCacheItem{
 			CacheID: utils.CacheAttributeProfiles,
-			ItemID:  "cgrates.org:TestCacheSetAndRemoveItems",
+			ItemID:  "cgrates.org:TestCacheSetAndRemoveItem",
 		},
 	}
 	var reply string
@@ -101,7 +102,7 @@ func TestCacheSetAndRemoveItems(t *testing.T) {
 	argsHasItem = &utils.ArgsGetCacheItemWithAPIOpts{
 		ArgsGetCacheItem: utils.ArgsGetCacheItem{
 			CacheID: utils.CacheAttributeProfiles,
-			ItemID:  "cgrates.org:TestCacheSetAndRemoveItems",
+			ItemID:  "cgrates.org:TestCacheSetAndRemoveItem",
 		},
 	}
 	if err := cache.HasItem(nil, argsHasItem, &replyBool); err != nil {
@@ -117,6 +118,134 @@ func TestCacheSetAndRemoveItems(t *testing.T) {
 	}
 	var replyStr []string
 	if err := cache.GetItemIDs(nil, argsGetItem, &replyStr); err == nil || err != utils.ErrNotFound {
+		t.Errorf("Expected %+v, received %+v", utils.ErrNotFound, err)
+	}
+}
+
+func TestCacheSetAndRemoveItems(t *testing.T) {
+	cfg := config.NewDefaultCGRConfig()
+	data := engine.NewInternalDB(nil, nil, true)
+	cfg.AdminSCfg().CachesConns = []string{utils.ConcatenatedKey(utils.MetaInternal, utils.MetaCaches)}
+	dm := engine.NewDataManager(data, cfg.CacheCfg(), nil)
+	ch := engine.NewCacheS(cfg, dm, nil)
+	ch.SetWithoutReplicate(utils.CacheAttributeProfiles, "cgrates.org:TestCacheSetAndRemoveItems1", nil, nil, true, utils.NonTransactional)
+	ch.SetWithoutReplicate(utils.CacheAttributeProfiles, "cgrates.org:TestCacheSetAndRemoveItems2", nil, nil, true, utils.NonTransactional)
+	ch.SetWithoutReplicate(utils.CacheAttributeProfiles, "cgrates.org:TestCacheSetAndRemoveItems3", nil, nil, true, utils.NonTransactional)
+	cache := NewCacheSv1(ch)
+
+	argsRemItm := utils.AttrReloadCacheWithAPIOpts{
+		ArgsCache: map[string][]string{
+			utils.AttributeProfileIDs: []string{"cgrates.org:TestCacheSetAndRemoveItems1",
+				"cgrates.org:TestCacheSetAndRemoveItems2", "cgrates.org:TestCacheSetAndRemoveItems3"},
+		},
+	}
+	var reply string
+	if err := cache.RemoveItems(nil, argsRemItm, &reply); err != nil {
+		t.Error(err)
+	} else if reply != utils.OK {
+		t.Errorf("Unexpected replyBool returned")
+	}
+
+	argsHasItem := &utils.ArgsGetCacheItemWithAPIOpts{
+		ArgsGetCacheItem: utils.ArgsGetCacheItem{
+			CacheID: utils.CacheAttributeProfiles,
+			ItemID:  "cgrates.org:TestCacheSetAndRemoveItems1",
+		},
+	}
+	var replyBool bool
+	if err := cache.HasItem(nil, argsHasItem, &replyBool); err != nil {
+		t.Error(err)
+	} else if replyBool {
+		t.Errorf("Unexpected replyBool returned")
+	}
+	argsHasItem.ArgsGetCacheItem.ItemID = "cgrates.org:TestCacheSetAndRemoveItems2"
+	if err := cache.HasItem(nil, argsHasItem, &replyBool); err != nil {
+		t.Error(err)
+	} else if replyBool {
+		t.Errorf("Unexpected replyBool returned")
+	}
+	argsHasItem.ArgsGetCacheItem.ItemID = "cgrates.org:TestCacheSetAndRemoveItems3"
+	if err := cache.HasItem(nil, argsHasItem, &replyBool); err != nil {
+		t.Error(err)
+	} else if replyBool {
+		t.Errorf("Unexpected replyBool returned")
+	}
+
+	argsGetItem := &utils.ArgsGetCacheItemIDsWithAPIOpts{
+		ArgsGetCacheItemIDs: utils.ArgsGetCacheItemIDs{
+			CacheID: utils.CacheAttributeProfiles,
+		},
+	}
+	var replyStr []string
+	if err := cache.GetItemIDs(nil, argsGetItem, &replyStr); err == nil || err != utils.ErrNotFound {
+		t.Errorf("Expected %+v, received %+v", utils.ErrNotFound, err)
+	}
+}
+
+func TestCacheClear(t *testing.T) {
+	cfg := config.NewDefaultCGRConfig()
+	data := engine.NewInternalDB(nil, nil, true)
+	cfg.AdminSCfg().CachesConns = []string{utils.ConcatenatedKey(utils.MetaInternal, utils.MetaCaches)}
+	dm := engine.NewDataManager(data, cfg.CacheCfg(), nil)
+	ch := engine.NewCacheS(cfg, dm, nil)
+	ch.SetWithoutReplicate(utils.CacheAttributeProfiles, "cgrates.org:TestCacheClearAttributes", nil, nil, true, utils.NonTransactional)
+	ch.SetWithoutReplicate(utils.CacheRateProfiles, "cgrates.org:TestCacheClearRates", nil, nil, true, utils.NonTransactional)
+	cache := NewCacheSv1(ch)
+
+	argsHasItem := &utils.AttrCacheIDsWithAPIOpts{
+		CacheIDs: nil,
+	}
+	var replyString string
+	if err := cache.Clear(nil, argsHasItem, &replyString); err != nil {
+		t.Error(err)
+	} else if replyString != utils.OK {
+		t.Errorf("Unexpected replyString returned")
+	}
+
+	argsGetItem := &utils.ArgsGetCacheItemIDsWithAPIOpts{
+		ArgsGetCacheItemIDs: utils.ArgsGetCacheItemIDs{
+			CacheID: utils.CacheAttributeProfiles,
+		},
+	}
+	var replyStr []string
+	if err := cache.GetItemIDs(nil, argsGetItem, &replyStr); err == nil || err != utils.ErrNotFound {
+		t.Errorf("Expected %+v, received %+v", utils.ErrNotFound, err)
+	}
+
+	argsGetItem = &utils.ArgsGetCacheItemIDsWithAPIOpts{
+		ArgsGetCacheItemIDs: utils.ArgsGetCacheItemIDs{
+			CacheID: utils.CacheRateProfiles,
+		},
+	}
+	if err := cache.GetItemIDs(nil, argsGetItem, &replyStr); err == nil || err != utils.ErrNotFound {
+		t.Errorf("Expected %+v, received %+v", utils.ErrNotFound, err)
+	}
+}
+
+func TestCacheLoadCache(t *testing.T) {
+	cfg := config.NewDefaultCGRConfig()
+	data := engine.NewInternalDB(nil, nil, true)
+	cfg.AdminSCfg().CachesConns = []string{utils.ConcatenatedKey(utils.MetaInternal, utils.MetaCaches)}
+	dm := engine.NewDataManager(data, cfg.CacheCfg(), nil)
+	ch := engine.NewCacheS(cfg, dm, nil)
+	cache := NewCacheSv1(ch)
+
+	var reply string
+	if err := cache.LoadCache(context.Background(), utils.NewAttrReloadCacheWithOpts(),
+		&reply); err != nil {
+		t.Error(err)
+	} else if reply != utils.OK {
+		t.Errorf("Unexcpected rep[ly returned")
+	}
+
+	argsGetItem := &utils.ArgsGetCacheItemIDsWithAPIOpts{
+		ArgsGetCacheItemIDs: utils.ArgsGetCacheItemIDs{
+			CacheID: utils.CacheAttributeProfiles,
+		},
+	}
+	var replyStr []string
+	if err := cache.GetItemIDs(nil, argsGetItem,
+		&replyStr); err == nil || err != utils.ErrNotFound {
 		t.Errorf("Expected %+v, received %+v", utils.ErrNotFound, err)
 	}
 }
