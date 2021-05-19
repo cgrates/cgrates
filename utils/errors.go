6a95eff821dfcfa84611bb3d1e24115fa03b84ed
Trial97 <alexandru.tripon@itsyscom.com>
2021-05-19 11:48:09 +0300

    Updated *acc and *tcc metrics

--- utils/errors.go
+++ utils/errors.go
@@ -75,6 +75,7 @@ var (
 	ErrMaxConcurentRPCExceededNoCaps = errors.New("max concurent rpc exceeded") // on internal we return this error for concureq
 	ErrMaxConcurentRPCExceeded       = errors.New("MAX_CONCURENT_RPC_EXCEEDED") // but the codec will rewrite it with this one to be sure that we corectly dealocate the request
 	ErrMaxIterationsReached          = errors.New("maximum iterations reached")
+	ErrNegative                      = errors.New("NEGATIVE")
 
 	ErrMap = map[string]error{
 		ErrNoMoreData.Error():              ErrNoMoreData,
