aab094f6f12d33b720a5a86a00b027ef8049d011
ionutboangiu <ionut.boangiu@itsyscom.com>
2021-05-20 16:50:20 +0300

    Test new *any profile matching implementation for dispatchers and attributes

--- dispatchers/dispatchers_test.go
+++ dispatchers/dispatchers_test.go
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 package dispatchers
 
 import (
+	"reflect"
 	"testing"
 	"time"
 
@@ -1269,5 +1270,82 @@ func TestDispatcherServiceCall4(t *testing.T) {
 	expected := "SERVER_ERROR"
 	if err == nil || err.Error() != expected {
 		t.Errorf("\nExpected <%+v>, \nReceived <%+v>", expected, err)
+	}
+}
+
+func TestDispatchersdispatcherProfileForEventAnySSfalse(t *testing.T) {
+	tmp := engine.Cache
+	defer func() {
+		engine.Cache = tmp
+	}()
+
+	cfg := config.NewDefaultCGRConfig()
+	dataDB := engine.NewInternalDB(nil, nil, true)
+	dm := engine.NewDataManager(dataDB, cfg.CacheCfg(), nil)
+	engine.Cache = engine.NewCacheS(cfg, dm, nil)
+
+	dS := &DispatcherService{
+		cfg:   cfg,
+		dm:    dm,
+		fltrS: engine.NewFilterS(cfg, nil, dm),
+	}
+	dS.cfg.DispatcherSCfg().AnySubsystem = false
+
+	dsp1 := &engine.DispatcherProfile{
+		Tenant:     "cgrates.org",
+		FilterIDs:  []string{"*string:~*req.Account:1002"},
+		Subsystems: []string{utils.MetaSessionS},
+		ID:         "DSP_1",
+		Strategy:   "*weight",
+		Weight:     10,
+	}
+	err := dS.dm.SetDispatcherProfile(dsp1, true)
+	if err != nil {
+		t.Error(err)
+	}
+
+	dsp2 := &engine.DispatcherProfile{
+		Tenant:     "cgrates.org",
+		FilterIDs:  []string{"*string:~*req.Account:1001"},
+		Subsystems: []string{utils.MetaAny},
+		ID:         "DSP_2",
+		Strategy:   "*weight",
+		Weight:     20,
+	}
+	err = dS.dm.SetDispatcherProfile(dsp2, true)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tnt := "cgrates.org"
+	ev := &utils.CGREvent{
+		Tenant: tnt,
+		Event: map[string]interface{}{
+			utils.AccountField: "1001",
+		},
+	}
+	subsys := utils.MetaSessionS
+
+	if rcv, err := dS.dispatcherProfileForEvent(tnt, ev, subsys); err != nil {
+		t.Errorf("\nexpected: <%+v>, \nreceived: <%+v>", nil, err)
+	} else if !reflect.DeepEqual(rcv, dsp2) {
+		t.Errorf("\nexpected: <%+v>, \nreceived: <%+v>", dsp2, rcv)
+	}
+
+	engine.Cache.Clear(nil)
+	dsp1.FilterIDs = []string{"*string:~*req.Account:1001"}
+	err = dS.dm.SetDispatcherProfile(dsp1, true)
+	if err != nil {
+		t.Error(err)
+	}
+	err = dS.dm.SetDispatcherProfile(dsp2, true)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if rcv, err := dS.dispatcherProfileForEvent(tnt, ev, subsys); err != nil {
+		t.Errorf("\nexpected: <%+v>, \nreceived: <%+v>", nil, err)
+	} else if !reflect.DeepEqual(rcv, dsp1) {
+		t.Errorf("\nexpected: <%+v>, \nreceived: <%+v>", dsp1, rcv)
 	}
 }
