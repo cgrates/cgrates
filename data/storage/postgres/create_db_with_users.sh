2de9da2349b78d7b203383a956fc31581a3c29a9
Trial97 <alexandru.tripon@itsyscom.com>
2021-06-02 08:16:56 +0300

    Moved ers database creation in a separate script

--- data/storage/postgres/create_db_with_users.sh
+++ data/storage/postgres/create_db_with_users.sh
@@ -7,7 +7,3 @@ sudo -u postgres dropdb -e cgrates
 sudo -u postgres dropuser -e cgrates
 sudo -u postgres psql  -c "CREATE USER cgrates password 'CGRateS.org';"
 sudo -u postgres createdb -e -O cgrates cgrates
-
-# extra DB for ees and ers
-sudo -u postgres dropdb -e cgrates2
-sudo -u postgres createdb -e -O cgrates cgrates2
