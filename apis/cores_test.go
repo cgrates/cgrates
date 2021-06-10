58976efab432976279c9b5addca9e39ea81d2c5c
nickolasdaniel <nickolas.filip@itsyscom.com>
2021-06-10 15:01:07 +0300

    Added CoreSv1 APIs back

---
+++ apis/cores_test.go
@@ -0,0 +1,59 @@
+/*
+Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
+Copyright (C) ITsysCOM GmbH
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+package apis
+
+import (
+	"testing"
+	"time"
+
+	"github.com/cgrates/birpc/context"
+	"github.com/cgrates/cgrates/config"
+	"github.com/cgrates/cgrates/cores"
+	"github.com/cgrates/cgrates/engine"
+	"github.com/cgrates/cgrates/utils"
+)
+
+func TestCoreSStatus(t *testing.T) {
+	cfg := config.NewDefaultCGRConfig()
+	caps := engine.NewCaps(2, utils.MetaTopUp)
+	coreService := cores.NewCoreService(cfg, caps, make(chan struct{}))
+	cS := NewCoreSv1(coreService)
+	arg := &utils.TenantWithAPIOpts{
+		Tenant:  "cgrates.org",
+		APIOpts: map[string]interface{}{},
+	}
+	var reply map[string]interface{}
+	if err := cS.Status(context.Background(), arg, &reply); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestCoreSSleep(t *testing.T) {
+	cfg := config.NewDefaultCGRConfig()
+	caps := engine.NewCaps(2, utils.MetaTopUp)
+	coreService := cores.NewCoreService(cfg, caps, make(chan struct{}))
+	cS := NewCoreSv1(coreService)
+	arg := &utils.DurationArgs{
+		Duration: 3 * time.Second,
+	}
+	var reply string
+	if err := cS.Sleep(context.Background(), arg, &reply); err != nil {
+		t.Error(err)
+	} else if reply != "OK" {
+		t.Errorf("Expected OK, reveived %+v", reply)
+	}
+
+}
