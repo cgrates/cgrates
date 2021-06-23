dcee7d31c4208641e17485dacd33cd40e46f8c2d
andronache <andrei.andronache@itsyscom.com>
2021-06-23 16:59:51 +0300

    Modified Ansible script for asterisk

--- data/storage/mysql/create_ers_db.sql
+++ data/storage/mysql/create_ers_db.sql
@@ -1,13 +1,5 @@
 
 --
--- Sample db and users creation. Replace here with your own details
---
-
-DROP DATABASE IF EXISTS cgrates;
-CREATE DATABASE cgrates;
-
-GRANT ALL on cgrates.* TO 'cgrates'@'localhost' IDENTIFIED BY 'CGRateS.org';
-
 -- extra DB for ers
 DROP DATABASE IF EXISTS cgrates2;
 CREATE DATABASE cgrates2;
