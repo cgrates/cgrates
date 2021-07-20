4276bf195d485078ad09daa444d6048689e4d009
nickolasdaniel <nickolas.filip@itsyscom.com>
2021-07-20 17:07:22 +0300

    Added tenant flag for cgr-loader

--- cmd/cgr-loader/cgr-loader.go
+++ cmd/cgr-loader/cgr-loader.go
@@ -106,6 +106,7 @@ var (
 	remove         = cgrLoaderFlags.Bool(utils.RemoveCgr, false, "Will remove instead of adding data from DB")
 	apiKey         = cgrLoaderFlags.String(utils.APIKeyCfg, utils.EmptyString, "Api Key used to comosed ArgDispatcher")
 	routeID        = cgrLoaderFlags.String(utils.RouteIDCfg, utils.EmptyString, "RouteID used to comosed ArgDispatcher")
+	tenant         = cgrLoaderFlags.String(utils.TenantCfg, dfltCfg.GeneralCfg().DefaultTenant, "If set, will overwrite the default tenant")
 
 	fromStorDB    = cgrLoaderFlags.Bool(utils.FromStorDBCgr, false, "Load the tariff plan from storDb to dataDb")
 	toStorDB      = cgrLoaderFlags.Bool(utils.ToStorDBcgr, false, "Import the tariff plan from files to storDb")
@@ -406,7 +407,7 @@ func main() {
 	if err = tpReader.ReloadCache(context.Background(), ldrCfg.GeneralCfg().DefaultCaching, *verbose, map[string]interface{}{
 		utils.OptsAPIKey:  *apiKey,
 		utils.OptsRouteID: *routeID,
-	}); err != nil {
+	}, *tenant); err != nil {
 		log.Fatal("Could not reload cache: ", err)
 	}
 
