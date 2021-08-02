74d47c5aa17b09687333542621886969ef8194df
nickolasdaniel <nickolas.filip@itsyscom.com>
2021-08-02 16:28:48 +0300

    Reduced time.Sleep in ees tests

--- ees/httpjsonmap_test.go
+++ ees/httpjsonmap_test.go
@@ -393,7 +393,7 @@ func TestHttpJsonMapSync(t *testing.T) {
 
 	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 
-		time.Sleep(3 * time.Second)
+		time.Sleep(25 * time.Millisecond)
 		wg1.Done()
 	}))
 
@@ -413,7 +413,7 @@ func TestHttpJsonMapSync(t *testing.T) {
 	select {
 	case <-test:
 		return
-	case <-time.After(4 * time.Second):
+	case <-time.After(50 * time.Millisecond):
 		t.Error("Can't asynchronously export events")
 	}
 }
@@ -452,7 +452,7 @@ func TestHttpJsonMapSyncLimit(t *testing.T) {
 	}()
 
 	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		time.Sleep(3 * time.Second)
+		time.Sleep(25 * time.Millisecond)
 		wg1.Done()
 	}))
 
@@ -472,7 +472,7 @@ func TestHttpJsonMapSyncLimit(t *testing.T) {
 	select {
 	case <-test:
 		t.Error("Should not have been possible to asynchronously export events")
-	case <-time.After(4 * time.Second):
+	case <-time.After(50 * time.Millisecond):
 		return
 	}
 }
