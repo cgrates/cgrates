287b01a7e11f3e1d6229e0346ecd26ac69b96635
Trial97 <alexandru.tripon@itsyscom.com>
2021-10-12 15:32:53 +0300

    Added protection for Redis limit when setting indexes

--- engine/storage_redis.go
+++ engine/storage_redis.go
@@ -66,10 +66,11 @@ const (
 	redisHMSET    = "HMSET"
 
 	redisLoadError = "Redis is loading the dataset in memory"
+	RedisLimit     = 524287 // https://github.com/StackExchange/StackExchange.Redis/issues/201#issuecomment-98639005
 )
 
 func NewRedisStorage(address string, db int, user, pass, mrshlerStr string,
-	maxConns,attempts int, sentinelName string, isCluster bool, clusterSync,
+	maxConns, attempts int, sentinelName string, isCluster bool, clusterSync,
 	clusterOnDownDelay time.Duration, tlsConn bool,
 	tlsClientCert, tlsClientKey, tlsCACert string) (_ *RedisStorage, err error) {
 	var ms Marshaler
@@ -823,6 +824,12 @@ func (rs *RedisStorage) SetIndexesDrv(ctx *context.Context, idxItmType, tntCtx s
 	for key, strMp := range indexes {
 		if len(strMp) == 0 { // remove with no more elements inside
 			deleteArgs = append(deleteArgs, key)
+			if len(deleteArgs) == RedisLimit+1 { // minus dbkey
+				if err = rs.Cmd(nil, redisHDEL, deleteArgs...); err != nil {
+					return
+				}
+				deleteArgs = []string{dbKey} // the dbkey is necesary for the HDEL command
+			}
 			continue
 		}
 		var encodedMp []byte
@@ -830,6 +837,12 @@ func (rs *RedisStorage) SetIndexesDrv(ctx *context.Context, idxItmType, tntCtx s
 			return
 		}
 		mp[key] = string(encodedMp)
+		if len(mp) == RedisLimit {
+			if err = rs.FlatCmd(nil, redisHMSET, dbKey, mp); err != nil {
+				return
+			}
+			mp = make(map[string]string)
+		}
 	}
 	if len(deleteArgs) != 1 {
 		if err = rs.Cmd(nil, redisHDEL, deleteArgs...); err != nil {
