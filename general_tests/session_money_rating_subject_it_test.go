bcb564240ee6d913db34a39980fd613da622ed60
andronache <andrei.andronache@itsyscom.com>
2021-10-29 15:50:03 +0300

    Corrected header in multiple files

--- general_tests/session_money_rating_subject_it_test.go
+++ general_tests/session_money_rating_subject_it_test.go
@@ -16,7 +16,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licensesMRS/>
+along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 package general_tests
 
