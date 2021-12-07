b64663b192819f776d45252c699cc8eb07840a81
Trial97 <alexandru.tripon@itsyscom.com>
2021-12-07 17:00:29 +0200

    Updated tests

--- engine/libstats.go
+++ engine/libstats.go
@@ -557,6 +557,9 @@ func (sqp *StatQueueProfile) Set(path []string, val interface{}, newBranch bool,
 	default:
 		return utils.ErrWrongPath
 	case 1:
+		if val == utils.EmptyString {
+			return
+		}
 		switch path[0] {
 		default:
 			return utils.ErrWrongPath
