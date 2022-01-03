d8661feed9f1f36589c2ec139adb844b05528538
andronache98 <andrei.andronache@itsyscom.com>
2022-01-03 13:02:33 +0200

    Repaired test failling because of date

--- engine/actions_test.go
+++ engine/actions_test.go
@@ -87,9 +87,9 @@ func TestActionPlanOnlyHour(t *testing.T) {
 }
 
 func TestActionPlanHourYear(t *testing.T) {
-	at := &ActionTiming{Timing: &RateInterval{Timing: &RITiming{Years: utils.Years{2022}, StartTime: "10:01:00"}}}
+	at := &ActionTiming{Timing: &RateInterval{Timing: &RITiming{Years: utils.Years{2035}, StartTime: "10:01:00"}}}
 	st := at.GetNextStartTime(referenceDate)
-	expected := time.Date(2022, 1, 1, 10, 1, 0, 0, time.Local)
+	expected := time.Date(2035, 1, 1, 10, 1, 0, 0, time.Local)
 	if !st.Equal(expected) {
 		t.Errorf("Expected %v was %v", expected, st)
 	}
