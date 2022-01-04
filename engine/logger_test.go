3d3a01648d535f0e6e219cb41bedbf6971624aff
ionutboangiu <ionut.boangiu@itsyscom.com>
2022-01-04 18:08:19 +0200

    Add tests for exporter logger

---
+++ engine/logger_test.go
@@ -0,0 +1,427 @@
+/*
+Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
+Copyright (C) ITsysCOM GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+package engine
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/cgrates/birpc"
+	"github.com/cgrates/birpc/context"
+	"github.com/cgrates/cgrates/config"
+	"github.com/cgrates/cgrates/utils"
+)
+
+func TestLoggerNewLoggerExport(t *testing.T) {
+	eesConn := utils.ConcatenatedKey(utils.MetaInternal, utils.MetaEEs)
+	cfg := config.NewDefaultCGRConfig()
+	cfg.CoreSCfg().EEsConns = []string{eesConn}
+	cM := NewConnManager(cfg)
+	exp := &ExportLogger{
+		connMgr:  cM,
+		eesConns: []string{eesConn},
+		logLevel: 7,
+		nodeID:   "123",
+		tenant:   "cgrates.org",
+	}
+	if rcv, err := NewLogger(utils.MetaExportLog, "cgrates.org", "123", 7, cM, []string{eesConn}); err != nil {
+		t.Error(err)
+	} else if !reflect.DeepEqual(rcv.(*ExportLogger), exp) {
+		t.Errorf("expected: <%+v>, \nreceived: <%+v>", exp, rcv)
+	}
+}
+
+func TestLoggerNewLoggerDefault(t *testing.T) {
+	experr := `unsupported logger: <invalid>`
+	if _, err := NewLogger("invalid", "cgrates.org", "123", 7, nil, nil); err == nil ||
+		err.Error() != experr {
+		t.Errorf("expected: <%s>, \nreceived: <%s>", experr, err)
+	}
+}
+
+func TestLoggerNewExportLogger(t *testing.T) {
+	eesConn := utils.ConcatenatedKey(utils.MetaInternal, utils.MetaEEs)
+	cfg := config.NewDefaultCGRConfig()
+	cfg.CoreSCfg().EEsConns = []string{eesConn}
+	cM := NewConnManager(cfg)
+	exp := &ExportLogger{
+		connMgr:  cM,
+		eesConns: []string{eesConn},
+		logLevel: 7,
+		nodeID:   "123",
+		tenant:   "cgrates.org",
+	}
+	if rcv := NewExportLogger("123", "cgrates.org", 7, cM,
+		[]string{eesConn}); !reflect.DeepEqual(rcv, exp) {
+		t.Errorf("expected: <%+v>, \nreceived: <%+v>", exp, rcv)
+	}
+}
+
+func TestLoggerExportEmerg(t *testing.T) {
+	tmp := Cache
+	defer func() {
+		Cache = tmp
+	}()
+	eesConn := utils.ConcatenatedKey(utils.MetaInternal, utils.MetaEEs)
+	cfg := config.NewDefaultCGRConfig()
+	cfg.CoreSCfg().EEsConns = []string{eesConn}
+	Cache = NewCacheS(cfg, nil, nil)
+	cM := NewConnManager(cfg)
+	ccM := &ccMock{
+		calls: map[string]func(ctx *context.Context, args interface{}, reply interface{}) error{
+			utils.EeSv1ProcessEvent: func(ctx *context.Context, args, reply interface{}) error {
+				exp := &utils.CGREvent{
+					Tenant: "cgrates.org",
+					Event: map[string]interface{}{
+						utils.NodeID: "123",
+						"Message":    "Emergency message",
+						"Severity":   utils.LOGLEVEL_EMERGENCY,
+					},
+				}
+				if !reflect.DeepEqual(exp, args) {
+					return fmt.Errorf("\nexpected: <%+v>, \nreceived: <%+v>",
+						utils.ToJSON(exp), utils.ToJSON(args))
+				}
+				return nil
+			},
+		},
+	}
+	rpcInternal := make(chan birpc.ClientConnector, 1)
+	rpcInternal <- ccM
+	cM.AddInternalConn(eesConn, utils.EeSv1, rpcInternal)
+
+	el := NewExportLogger("123", "cgrates.org", 1, cM, []string{eesConn})
+
+	if err := el.Emerg("Emergency message"); err != nil {
+		t.Error(err)
+	}
+	el.logLevel = 0
+	if err := el.Emerg("Emergency message"); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestLoggerExportAlert(t *testing.T) {
+	tmp := Cache
+	defer func() {
+		Cache = tmp
+	}()
+	eesConn := utils.ConcatenatedKey(utils.MetaInternal, utils.MetaEEs)
+	cfg := config.NewDefaultCGRConfig()
+	cfg.CoreSCfg().EEsConns = []string{eesConn}
+	Cache = NewCacheS(cfg, nil, nil)
+	cM := NewConnManager(cfg)
+	ccM := &ccMock{
+		calls: map[string]func(ctx *context.Context, args interface{}, reply interface{}) error{
+			utils.EeSv1ProcessEvent: func(ctx *context.Context, args, reply interface{}) error {
+				exp := &utils.CGREvent{
+					Tenant: "cgrates.org",
+					Event: map[string]interface{}{
+						utils.NodeID: "123",
+						"Message":    "Alert message",
+						"Severity":   utils.LOGLEVEL_ALERT,
+					},
+				}
+				if !reflect.DeepEqual(exp, args) {
+					return fmt.Errorf("\nexpected: <%+v>, \nreceived: <%+v>",
+						utils.ToJSON(exp), utils.ToJSON(args))
+				}
+				return nil
+			},
+		},
+	}
+	rpcInternal := make(chan birpc.ClientConnector, 1)
+	rpcInternal <- ccM
+	cM.AddInternalConn(eesConn, utils.EeSv1, rpcInternal)
+
+	el := NewExportLogger("123", "cgrates.org", 2, cM, []string{eesConn})
+
+	if err := el.Alert("Alert message"); err != nil {
+		t.Error(err)
+	}
+	el.logLevel = 1
+	if err := el.Alert("Alert message"); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestLoggerExportCrit(t *testing.T) {
+	tmp := Cache
+	defer func() {
+		Cache = tmp
+	}()
+	eesConn := utils.ConcatenatedKey(utils.MetaInternal, utils.MetaEEs)
+	cfg := config.NewDefaultCGRConfig()
+	cfg.CoreSCfg().EEsConns = []string{eesConn}
+	Cache = NewCacheS(cfg, nil, nil)
+	cM := NewConnManager(cfg)
+	ccM := &ccMock{
+		calls: map[string]func(ctx *context.Context, args interface{}, reply interface{}) error{
+			utils.EeSv1ProcessEvent: func(ctx *context.Context, args, reply interface{}) error {
+				exp := &utils.CGREvent{
+					Tenant: "cgrates.org",
+					Event: map[string]interface{}{
+						utils.NodeID: "123",
+						"Message":    "Critical message",
+						"Severity":   utils.LOGLEVEL_CRITICAL,
+					},
+				}
+				if !reflect.DeepEqual(exp, args) {
+					return fmt.Errorf("\nexpected: <%+v>, \nreceived: <%+v>",
+						utils.ToJSON(exp), utils.ToJSON(args))
+				}
+				return nil
+			},
+		},
+	}
+	rpcInternal := make(chan birpc.ClientConnector, 1)
+	rpcInternal <- ccM
+	cM.AddInternalConn(eesConn, utils.EeSv1, rpcInternal)
+
+	el := NewExportLogger("123", "cgrates.org", 2, cM, []string{eesConn})
+
+	if err := el.Crit("Critical message"); err != nil {
+		t.Error(err)
+	}
+	el.logLevel = 1
+	if err := el.Crit("Critical message"); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestLoggerExportErr(t *testing.T) {
+	tmp := Cache
+	defer func() {
+		Cache = tmp
+	}()
+	eesConn := utils.ConcatenatedKey(utils.MetaInternal, utils.MetaEEs)
+	cfg := config.NewDefaultCGRConfig()
+	cfg.CoreSCfg().EEsConns = []string{eesConn}
+	Cache = NewCacheS(cfg, nil, nil)
+	cM := NewConnManager(cfg)
+	ccM := &ccMock{
+		calls: map[string]func(ctx *context.Context, args interface{}, reply interface{}) error{
+			utils.EeSv1ProcessEvent: func(ctx *context.Context, args, reply interface{}) error {
+				exp := &utils.CGREvent{
+					Tenant: "cgrates.org",
+					Event: map[string]interface{}{
+						utils.NodeID: "123",
+						"Message":    "Error message",
+						"Severity":   utils.LOGLEVEL_ERROR,
+					},
+				}
+				if !reflect.DeepEqual(exp, args) {
+					return fmt.Errorf("\nexpected: <%+v>, \nreceived: <%+v>",
+						utils.ToJSON(exp), utils.ToJSON(args))
+				}
+				return nil
+			},
+		},
+	}
+	rpcInternal := make(chan birpc.ClientConnector, 1)
+	rpcInternal <- ccM
+	cM.AddInternalConn(eesConn, utils.EeSv1, rpcInternal)
+
+	el := NewExportLogger("123", "cgrates.org", 3, cM, []string{eesConn})
+
+	if err := el.Err("Error message"); err != nil {
+		t.Error(err)
+	}
+	el.logLevel = 2
+	if err := el.Err("Error message"); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestLoggerExportWarning(t *testing.T) {
+	tmp := Cache
+	defer func() {
+		Cache = tmp
+	}()
+	eesConn := utils.ConcatenatedKey(utils.MetaInternal, utils.MetaEEs)
+	cfg := config.NewDefaultCGRConfig()
+	cfg.CoreSCfg().EEsConns = []string{eesConn}
+	Cache = NewCacheS(cfg, nil, nil)
+	cM := NewConnManager(cfg)
+	ccM := &ccMock{
+		calls: map[string]func(ctx *context.Context, args interface{}, reply interface{}) error{
+			utils.EeSv1ProcessEvent: func(ctx *context.Context, args, reply interface{}) error {
+				exp := &utils.CGREvent{
+					Tenant: "cgrates.org",
+					Event: map[string]interface{}{
+						utils.NodeID: "123",
+						"Message":    "Warning message",
+						"Severity":   utils.LOGLEVEL_WARNING,
+					},
+				}
+				if !reflect.DeepEqual(exp, args) {
+					return fmt.Errorf("\nexpected: <%+v>, \nreceived: <%+v>",
+						utils.ToJSON(exp), utils.ToJSON(args))
+				}
+				return nil
+			},
+		},
+	}
+	rpcInternal := make(chan birpc.ClientConnector, 1)
+	rpcInternal <- ccM
+	cM.AddInternalConn(eesConn, utils.EeSv1, rpcInternal)
+
+	el := NewExportLogger("123", "cgrates.org", 4, cM, []string{eesConn})
+
+	if err := el.Warning("Warning message"); err != nil {
+		t.Error(err)
+	}
+	el.logLevel = 3
+	if err := el.Warning("Warning message"); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestLoggerExportNotice(t *testing.T) {
+	tmp := Cache
+	defer func() {
+		Cache = tmp
+	}()
+	eesConn := utils.ConcatenatedKey(utils.MetaInternal, utils.MetaEEs)
+	cfg := config.NewDefaultCGRConfig()
+	cfg.CoreSCfg().EEsConns = []string{eesConn}
+	Cache = NewCacheS(cfg, nil, nil)
+	cM := NewConnManager(cfg)
+	ccM := &ccMock{
+		calls: map[string]func(ctx *context.Context, args interface{}, reply interface{}) error{
+			utils.EeSv1ProcessEvent: func(ctx *context.Context, args, reply interface{}) error {
+				exp := &utils.CGREvent{
+					Tenant: "cgrates.org",
+					Event: map[string]interface{}{
+						utils.NodeID: "123",
+						"Message":    "Notice message",
+						"Severity":   utils.LOGLEVEL_NOTICE,
+					},
+				}
+				if !reflect.DeepEqual(exp, args) {
+					return fmt.Errorf("\nexpected: <%+v>, \nreceived: <%+v>",
+						utils.ToJSON(exp), utils.ToJSON(args))
+				}
+				return nil
+			},
+		},
+	}
+	rpcInternal := make(chan birpc.ClientConnector, 1)
+	rpcInternal <- ccM
+	cM.AddInternalConn(eesConn, utils.EeSv1, rpcInternal)
+
+	el := NewExportLogger("123", "cgrates.org", 5, cM, []string{eesConn})
+
+	if err := el.Notice("Notice message"); err != nil {
+		t.Error(err)
+	}
+	el.logLevel = 4
+	if err := el.Notice("Notice message"); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestLoggerExportInfo(t *testing.T) {
+	tmp := Cache
+	defer func() {
+		Cache = tmp
+	}()
+	eesConn := utils.ConcatenatedKey(utils.MetaInternal, utils.MetaEEs)
+	cfg := config.NewDefaultCGRConfig()
+	cfg.CoreSCfg().EEsConns = []string{eesConn}
+	Cache = NewCacheS(cfg, nil, nil)
+	cM := NewConnManager(cfg)
+	ccM := &ccMock{
+		calls: map[string]func(ctx *context.Context, args interface{}, reply interface{}) error{
+			utils.EeSv1ProcessEvent: func(ctx *context.Context, args, reply interface{}) error {
+				exp := &utils.CGREvent{
+					Tenant: "cgrates.org",
+					Event: map[string]interface{}{
+						utils.NodeID: "123",
+						"Message":    "Info message",
+						"Severity":   utils.LOGLEVEL_INFO,
+					},
+				}
+				if !reflect.DeepEqual(exp, args) {
+					return fmt.Errorf("\nexpected: <%+v>, \nreceived: <%+v>",
+						utils.ToJSON(exp), utils.ToJSON(args))
+				}
+				return nil
+			},
+		},
+	}
+	rpcInternal := make(chan birpc.ClientConnector, 1)
+	rpcInternal <- ccM
+	cM.AddInternalConn(eesConn, utils.EeSv1, rpcInternal)
+
+	el := NewExportLogger("123", "cgrates.org", 6, cM, []string{eesConn})
+
+	if err := el.Info("Info message"); err != nil {
+		t.Error(err)
+	}
+	el.logLevel = 5
+	if err := el.Info("Info message"); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestLoggerExportDebug(t *testing.T) {
+	tmp := Cache
+	defer func() {
+		Cache = tmp
+	}()
+	eesConn := utils.ConcatenatedKey(utils.MetaInternal, utils.MetaEEs)
+	cfg := config.NewDefaultCGRConfig()
+	cfg.CoreSCfg().EEsConns = []string{eesConn}
+	Cache = NewCacheS(cfg, nil, nil)
+	cM := NewConnManager(cfg)
+	ccM := &ccMock{
+		calls: map[string]func(ctx *context.Context, args interface{}, reply interface{}) error{
+			utils.EeSv1ProcessEvent: func(ctx *context.Context, args, reply interface{}) error {
+				exp := &utils.CGREvent{
+					Tenant: "cgrates.org",
+					Event: map[string]interface{}{
+						utils.NodeID: "123",
+						"Message":    "Debug message",
+						"Severity":   utils.LOGLEVEL_DEBUG,
+					},
+				}
+				if !reflect.DeepEqual(exp, args) {
+					return fmt.Errorf("\nexpected: <%+v>, \nreceived: <%+v>",
+						utils.ToJSON(exp), utils.ToJSON(args))
+				}
+				return nil
+			},
+		},
+	}
+	rpcInternal := make(chan birpc.ClientConnector, 1)
+	rpcInternal <- ccM
+	cM.AddInternalConn(eesConn, utils.EeSv1, rpcInternal)
+
+	el := NewExportLogger("123", "cgrates.org", 7, cM, []string{eesConn})
+
+	if err := el.Debug("Debug message"); err != nil {
+		t.Error(err)
+	}
+	el.logLevel = 6
+	if err := el.Debug("Debug message"); err != nil {
+		t.Error(err)
+	}
+}
