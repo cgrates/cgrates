33d0672170ba0101129f14a9997ec2839d0ecf4c
nickolasdaniel <nickolas.filip@itsyscom.com>
2022-03-11 14:50:02 +0200

    Changes and additions for Drone

---
+++ data/docker/integration/scripts/postgres/create_db_with_users.sh
@@ -0,0 +1,9 @@
+
+#
+# Sample db and users creation. Replace here with your own details
+#
+
+sudo -u postgres dropdb -e cgrates
+sudo -u postgres dropuser -e cgrates
+sudo -u postgres psql  -c "CREATE USER cgrates password 'CGRateS.org';"
+sudo -u postgres createdb -e -O cgrates cgrates
