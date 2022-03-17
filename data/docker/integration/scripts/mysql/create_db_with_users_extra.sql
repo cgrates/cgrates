9e92552df5943ea0d507bba370fcfa0b2c987372
nickolasdaniel <nickolas.filip@itsyscom.com>
2022-03-17 14:47:12 +0200

    Changes and addittion for Drone CI

--- data/docker/integration/scripts/mysql/create_db_with_users_extra.sql
+++
@@ -1,9 +0,0 @@
-
---
--- Sample db and users creation. Replace here with your own details
---
-
-CREATE DATABASE cgrates2;
-
-GRANT ALL on cgrates.* TO 'cgrates'@'127.0.0.1' IDENTIFIED BY 'CGRateS.org';
-GRANT ALL on cgrates2.* TO 'cgrates'@'127.0.0.1' IDENTIFIED BY 'CGRateS.org';
