800a0ddcba4f997585bc6d80a391f9056c9a1e9a
ionutboangiu <ionut.boangiu@itsyscom.com>
2022-05-26 17:07:54 +0300

    Add IF NOT EXISTS condition when creating sql user

--- data/storage/mysql/create_db_with_users.sql
+++ data/storage/mysql/create_db_with_users.sql
@@ -5,6 +5,6 @@
 
 DROP DATABASE IF EXISTS cgrates;
 CREATE DATABASE cgrates;
-CREATE USER 'cgrates'@'localhost' IDENTIFIED BY 'CGRateS.org';
+CREATE USER IF NOT EXISTS 'cgrates'@'localhost' IDENTIFIED BY 'CGRateS.org';
 GRANT ALL PRIVILEGES ON cgrates.* TO 'cgrates'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
