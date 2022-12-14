505709b26f77243059382bc80095692ad9d6060c
gezimbll <gezim.blliku@itsyscom.com>
2022-12-14 10:55:02 -0500

    Improving coverage at engine

--- engine/exportrequest_test.go
+++ engine/exportrequest_test.go
@@ -551,79 +551,51 @@ func TestExportRequestCompose(t *testing.T) {
 
 }
 
-// func TestExportRequestSetFields(t *testing.T) {
-// 	Cache.Clear(nil)
-// 	onm := utils.NewOrderedNavigableMap()
-// 	fullPath := &utils.FullPath{
-// 		PathSlice: []string{utils.MetaReq, utils.MetaTenant},
-// 		Path:      utils.MetaTenant,
-// 	}
-// 	val := &utils.DataLeaf{
-// 		Data: "value1",
-// 	}
-// 	onm.Append(fullPath, val)
-// 	cfg := config.NewDefaultCGRConfig()
-// 	cfg.DataDbCfg().Items = map[string]*config.ItemOpt{
-// 		utils.CacheFilters: {
-// 			Limit:     4,
-// 			TTL:       5,
-// 			StaticTTL: true,
-// 		},
-// 		utils.CacheUCH: {
-// 			Limit:     4,
-// 			TTL:       5,
-// 			StaticTTL: true,
-// 		},
-// 	}
-// 	cfg.FilterSCfg().ResourceSConns = []string{utils.ConcatenatedKey(utils.MetaInternal, utils.ResourceSConnsCfg)}
-// 	cfg.FilterSCfg().StatSConns = []string{utils.ConcatenatedKey(utils.MetaInternal, utils.StatSConnsCfg)}
-// 	cfg.FilterSCfg().ApierSConns = []string{utils.ConcatenatedKey(utils.MetaInternal, utils.ApierSConnsCfg)}
-// 	db := NewInternalDB(nil, nil, true, cfg.DataDbCfg().Items)
+func TestExportRequestSetFields(t *testing.T) {
+	Cache.Clear(nil)
+	onm := utils.NewOrderedNavigableMap()
+	fullPath := &utils.FullPath{
+		PathSlice: []string{utils.MetaReq, utils.MetaAccount},
+		Path:      utils.MetaReq + utils.MetaAccount,
+	}
+	val := &utils.DataLeaf{
+		Data: "value1",
+	}
+	onm.Append(fullPath, val)
+	cfg := config.NewDefaultCGRConfig()
 
-// 	dm := NewDataManager(db, config.CgrConfig().CacheCfg(), nil)
-// 	eeR := &ExportRequest{
-// 		inData: map[string]utils.DataStorage{
-// 			utils.MetaReq: utils.MapStorage{
-// 				"Account": "1001",
-// 				"Usage":   "10m",
-// 			},
-// 			utils.MetaOpts: utils.MapStorage{
-// 				"*opts": "val",
-// 			},
-// 		},
-// 		filterS: NewFilterS(cfg, nil, dm),
-// 		tnt:     "cgrates.org",
-// 		ExpData: map[string]*utils.OrderedNavigableMap{
-// 			utils.MetaReq: onm,
-// 		},
-// 	}
-// 	fctTemp := []*config.FCTemplate{
-// 		{
-// 			Type:       utils.MetaFiller,
-// 			Value:      config.NewRSRParsersMustCompile("*month_endTest", utils.InfieldSep),
-// 			Layout:     "“Mon Jan _2 15:04:05 2006”",
-// 			Timezone:   "Local",
-// 			MaskLen:    3,
-// 			MaskDestID: "dest1",
-// 			Filters:    []string{"filter1"},
-// 			Path:       "<*uch;*opts>",
-// 		},
-// 	}
+	db := NewInternalDB(nil, nil, true, cfg.DataDbCfg().Items)
 
-// 	Cache.Set(utils.CacheFilters, utils.ConcatenatedKey(eeR.tnt, fctTemp[0].Filters[0]), &Filter{
-// 		Tenant: "cgrates.org",
-// 		ID:     "FLTR_2",
-// 		Rules:  []*FilterRule{
-// 			// {
-// 			// 	Type:    utils.MetaPrefix,
-// 			// 	Element: utils.DynamicDataPrefix + utils.MetaReq + utils.NestingSep + utils.AccountField,
-// 			// 	Values:  []string{"1001"},
-// 			// },
-// 		}}, []string{}, true, utils.NonTransactional)
-// 	if err = eeR.SetFields(fctTemp); err != nil {
-// 		t.Error(err)
-// 	}
-// }
+	dm := NewDataManager(db, config.CgrConfig().CacheCfg(), nil)
+	eeR := &ExportRequest{
+		inData: map[string]utils.DataStorage{
+			utils.MetaReq: utils.MapStorage{
+				"Account": "1001",
+				"Usage":   "10m",
+			},
+			utils.MetaOpts: utils.MapStorage{
+				"*opts": "val",
+			},
+		},
+		filterS: NewFilterS(cfg, nil, dm),
+		tnt:     "cgrates.org",
+		ExpData: map[string]*utils.OrderedNavigableMap{
+			utils.MetaReq: onm,
+		},
+	}
+	fctTemp := []*config.FCTemplate{
+		{
+			Type:     utils.MetaComposed,
+			Value:    config.NewRSRParsersMustCompile("1003", utils.InfieldSep),
+			Timezone: "Local",
+			Path:     "<*uch;*opts>",
+		},
+	}
+
+	if err = eeR.SetFields(fctTemp); err == nil || err.Error() != fmt.Sprint("unsupported field prefix: <*uch*opts> when set field") {
+		t.Error(err)
+	}
+}
 
 func TestExportRequestFieldAsString(t *testing.T) {
 	inData := map[string]utils.DataStorage{
