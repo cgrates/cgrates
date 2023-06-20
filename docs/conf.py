f3d0f2ef841b9fc763c4b0d254f94a01f9b8720a
ionutboangiu <ionut.boangiu@itsyscom.com>
2023-06-20 03:03:10 -0400

    Resolve issue with tabs not being displayed in docs

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,8 @@ import sys, os
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
   'sphinx.ext.todo', 
-  'sphinx_copybutton'
+  'sphinx_copybutton',
+  'sphinx_tabs.tabs'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -94,7 +95,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
