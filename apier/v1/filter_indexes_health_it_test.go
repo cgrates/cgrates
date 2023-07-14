f3428e298dd33ed905f137b5015d1695da59dd84
ionutboangiu <ionut.boangiu@itsyscom.com>
2023-07-14 17:19:07 -0400

    Fix failing test due to tariffplan changes

--- apier/v1/filter_indexes_health_it_test.go
+++ apier/v1/filter_indexes_health_it_test.go
@@ -23,6 +23,7 @@ package v1
 
 import (
 	"net/rpc"
+	"os"
 	"path"
 	"reflect"
 	"sort"
@@ -44,7 +45,7 @@ var (
 		testV1FIdxHStartEngine,
 		testV1FIdxHRpcConn,
 
-		testV1FIdxHLoadFromFolderTutorial2,
+		testV1FIdxHLoadTPs,
 		testV1FIdxHAccountActionPlansHealth,
 		testV1FIdxHReverseDestinationHealth,
 		testV1FIdxHdxInitDataDb,
@@ -122,20 +123,199 @@ func testV1FIdxHRpcConn(t *testing.T) {
 	}
 }
 
-func testV1FIdxHLoadFromFolderTutorial2(t *testing.T) {
-	var reply string
-	if err := tFIdxHRpc.Call(utils.CacheSv1Clear, &utils.AttrCacheIDsWithArgDispatcher{
-		CacheIDs: nil,
-	}, &reply); err != nil {
-		t.Error(err)
-	} else if reply != utils.OK {
-		t.Error("Reply: ", reply)
-	}
-	attrs := &utils.AttrLoadTpFromFolder{FolderPath: path.Join(*dataDir, "tariffplans", "tutorial2")}
-	if err := tFIdxHRpc.Call(utils.APIerSv1LoadTariffPlanFromFolder, attrs, &reply); err != nil {
+func testV1FIdxHLoadTPs(t *testing.T) {
+	if err := os.RemoveAll("/tmp/TestFIdxHealthIT"); err != nil {
 		t.Error(err)
 	}
-	time.Sleep(100 * time.Millisecond)
+	if err := os.MkdirAll("/tmp/TestFIdxHealthIT", 0755); err != nil {
+		t.Error(err)
+	}
+	defer os.RemoveAll("/tmp/TestFIdxHealthIT")
+	writeFile := func(fileName, data string) error {
+		csvFile, err := os.Create(path.Join("/tmp/TestFIdxHealthIT", fileName))
+		if err != nil {
+			return err
+		}
+		defer csvFile.Close()
+		_, err = csvFile.WriteString(data)
+		if err != nil {
+			return err
+
+		}
+		return csvFile.Sync()
+	}
+
+	// Create and populate AccountActions.csv
+	if err := writeFile(utils.AccountActionsCsv, `
+#Tenant,Account,ActionPlanID,ActionTriggersID,AllowNegative,Disabled
+cgrates.org,1001,STANDARD_PLAN,STANDARD_TRIGGERS,,
+cgrates.org,1002,STANDARD_PLAN,STANDARD_TRIGGERS,,
+cgrates.org,1003,STANDARD_PLAN,STANDARD_TRIGGERS,,
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and populate ActionPlans.csv
+	if err := writeFile(utils.ActionPlansCsv, `
+#Id,ActionsId,TimingId,Weight
+STANDARD_PLAN,TOPUP_RST_MONETARY_10,*asap,10
+STANDARD_PLAN,TOPUP_RST_5M_VOICE,*asap,10
+STANDARD_PLAN,TOPUP_RST_10M_VOICE,*asap,10
+STANDARD_PLAN,TOPUP_RST_100_SMS,*asap,10
+STANDARD_PLAN,TOPUP_RST_1024_DATA,*asap,10
+STANDARD_PLAN,TOPUP_RST_1024_DATA,TM_NOON,10
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and populate ActionTriggers.csv
+	if err := writeFile(utils.ActionTriggersCsv, `
+#ID[0],UniqueID[1],ThresholdType[2],ThresholdValue[3],Recurrent[4],MinSleep[5],ExpiryTime[6],ActivationTime[7],BalanceTag[8],BalanceType[9],BalanceCategories[10],BalanceDestinationIDs[11],BalanceRatingSubject[12],BalanceSharedGroup[13],BalanceExpiryTime[14],BalanceTimingIDs[15],BalanceWeight[16],BalanceBlocker[17],BalanceDisabled[18],ActionsID[19],Weight[20]
+STANDARD_TRIGGERS,,*min_balance,2,,,,,,*monetary,,,,,,,,,,TOPUP_BONUS_10SMS,10
+STANDARD_TRIGGERS,,*max_balance,100,,,,,,*monetary,,,,,,,,,,DISABLE_ACCOUNT,10
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and populate Actions.csv
+	if err := writeFile(utils.ActionsCsv, `
+#ActionsId[0],Action[1],ExtraParameters[2],Filter[3],BalanceId[4],BalanceType[5],Categories[6],DestinationIds[7],RatingSubject[8],SharedGroup[9],ExpiryTime[10],TimingIds[11],Units[12],BalanceWeight[13],BalanceBlocker[14],BalanceDisabled[15],Weight[16]
+TOPUP_RST_MONETARY_10,*topup_reset,,,*default,*monetary,,,,,,,10,10,,,10
+TOPUP_RST_10M_VOICE,*topup_reset,,,PER_CALL,*voice,,DST_10,RPF_SPECIAL_BLC,,,,10m,10,,,10
+TOPUP_RST_5M_VOICE,*topup_reset,,,FREE_MINS,*voice,,,*zero1m,,,,5m,20,,,10
+TOPUP_RST_100_SMS,*topup_reset,,,FREE_SMSes,*sms,,,,,,,100,20,,,10
+TOPUP_BONUS_10SMS,*topup,,,BONUS_SMSes,*sms,,DST_50,,,,,10,30,,,10
+TOPUP_RST_1024_DATA,*topup_reset,,,FREE_DATA,*data,,,,,,,1024,20,,,10
+LOG_WARNING,*log,,,,,,,,,,,,,,,10
+DISABLE_ACCOUNT,*disable_account,,,,,,,,,,,,,,,10
+WARN_HTTP_ASYNC,*http_post_async,http://$path/$to/$warn,,,,,,,,,,,,false,false,10
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and populate Attributes.csv
+	if err := writeFile(utils.AttributesCsv, `
+#Tenant,ID,Contexts,FilterIDs,ActivationInterval,AttributeFilterIDs,Path,Type,Value,Blocker,Weight
+cgrates.org,ATTR_CRG_SUPPLIER1,*sessions;*cdrs,,,,*req.Category,*constant,reseller1,false,0
+cgrates.org,ATTR_CRG_SUPPLIER1,,,,,*req.RequestType,*constant,*rated,,
+cgrates.org,ATTR_1001_AUTH,auth,*string:~*req.Account:1001,,,*req.Password,*constant,CGRateS.org,false,20
+cgrates.org,ATTR_1002_AUTH,auth,*string:~*req.Account:1002,,,*req.Password,*constant,CGRateS.org,false,20
+cgrates.org,ATTR_1003_AUTH,auth,*string:~*req.Account:1003,,,*req.Password,*constant,CGRateS.org,false,20
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and populate Chargers.csv
+	if err := writeFile(utils.ChargersCsv, `
+#Tenant,ID,FilterIDs,ActivationInterval,RunID,AttributeIDs,Weight
+cgrates.org,CGR_DEFAULT,,,*default,*none,0
+cgrates.org,CRG_RESELLER1,,,reseller1,ATTR_CRG_RESELLER1,1
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and populate DestinationRates.csv
+	if err := writeFile(utils.DestinationRatesCsv, `
+#ID,DestinationsID,RatesID,RoundingMethod,RoundingDecimals,MaxCost,MaxCostStrategy
+DR_10_120C,DST_10,RT_120C,*up,4,,
+DR_10_60C,DST_10,RT_60C,*up,4,,
+DR_2030_120C,DST_2030,RT_120C,*up,4,,
+DR_20_60C,DST_20,RT_60C,*up,4,,
+DR_VOICEMAIL_FREE,DST_VOICEMAIL,RT_0,*up,4,,
+DR_1002_60C,DST_1002,RT_60C,*up,4,,
+DR_ANY_10C_CN,*any,RT_10C_CN,*up,4,,
+DR_ANY_1024_1,*any,RT_1024_1,*up,4,,
+DR_1002_10C1,DST_1002,RT_10C1,*up,4,,
+DR_10_20C1,DST_10,RT_20C1,*up,4,,
+DR_1CNT,*any,RT_1CNT,*up,4,,
+DR_10CNT,*any,RT_10CNT,*up,4,,
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and populate Destinations.csv
+	if err := writeFile(utils.DestinationsCsv, `
+#ID,Prefix
+DST_10,10
+DST_20,20
+DST_2030,20
+DST_2030,30
+DST_VOICEMAIL,voicemail
+DST_1002,1002
+DST_50,50
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and populate Rates.csv
+	if err := writeFile(utils.RatesCsv, `
+#ID,ConnectFee,Rate,RateUnit,RateIncrement,GroupIntervalStart
+RT_120C,0.2,1.2,1m,1m,0
+RT_120C,,1.2,1m,1s,1m
+RT_60C,0.1,0.01,1s,1s,0
+RT_0,0,0,1m,1m,0
+RT_10C_CN,0.1,0,0,1s,0
+RT_1024_1,0,1,1024,1024,0
+RT_10C1,0,0.1,1,1,0
+RT_20C1,0,0.2,1,1,0
+RT_1CNT,0,0.01,1s,1s,0
+RT_10CNT,0.1,0,1s,1s,0
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and populate RatingPlans.csv
+	if err := writeFile(utils.RatingPlansCsv, `
+#ID,DestinationRatesID,TimingID,Weight
+RP_STANDARD,DR_10_120C,PEAK,10
+RP_STANDARD,DR_10_60C,OFFPEAK_MORNING,10
+RP_STANDARD,DR_10_60C,OFFPEAK_EVENING,10
+RP_STANDARD,DR_10_60C,OFFPEAK_WEEKEND,10
+RP_STANDARD,DR_2030_120C,*any,10
+RP_STANDARD,DR_20_60C,NEW_YEAR,20
+RP_STANDARD,DR_VOICEMAIL_FREE,*any,10
+RP_1001,DR_1002_60C,*any,10
+RP_SPECIAL_BLC,DR_ANY_10C_CN,*any,10
+RP_DATA,DR_ANY_1024_1,*any,10
+RP_SMS,DR_1002_10C1,*any,10
+RP_SMS,DR_10_20C1,*any,10
+RP_1CNT,DR_1CNT,*any,0
+RP_10CNT,DR_10CNT,*any,0
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and populate RatingProfiles.csv
+	if err := writeFile(utils.RatingProfilesCsv, `
+#Tenant,Category,Subject,ActivationTime,RatingPlanID,FallbackSubject
+cgrates.org,call,*any,2019-03-01T00:00:00Z,RP_STANDARD,
+cgrates.org,call,1001,2019-03-01T00:00:00Z,RP_1001,*any
+cgrates.org,call,RPF_SPECIAL_BLC,2019-03-01T00:00:00Z,RP_SPECIAL_BLC,
+cgrates.org,data,*any,2019-03-01T00:00:00Z,RP_DATA,
+cgrates.org,sms,*any,2019-03-01T00:00:00Z,RP_SMS,
+cgrates.org,reseller1,*any,2019-03-01T00:00:00Z,RP_10CNT,
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and populate Timings.csv
+	if err := writeFile(utils.TimingsCsv, `
+#ID,Years,Months,MonthDays,WeekDays,Time
+PEAK,*any,*any,*any,1;2;3;4;5,08:00:00
+OFFPEAK_MORNING,*any,*any,*any,1;2;3;4;5,00:00:00
+OFFPEAK_EVENING,*any,*any,*any,1;2;3;4;5,19:00:00
+OFFPEAK_WEEKEND,*any,*any,*any,6;7,00:00:00
+NEW_YEAR,*any,1,1,*any,00:00:00
+TM_NOON,*any,*any,*any,*any,12:00:00
+`); err != nil {
+		t.Fatal(err)
+	}
+
+	var loadInst string
+	if err := tFIdxHRpc.Call(utils.APIerSv1LoadTariffPlanFromFolder,
+		&utils.AttrLoadTpFromFolder{FolderPath: "/tmp/TestFIdxHealthIT"}, &loadInst); err != nil {
+		t.Error(err)
+	}
 }
 
 func testV1FIdxHAccountActionPlansHealth(t *testing.T) {
@@ -248,7 +428,7 @@ func testV1FIdxGetThresholdsIndexesHealth(t *testing.T) {
 	// all indexes are set and points to their objects correctly
 	args := &engine.IndexHealthArgsWith3Ch{}
 	expRPly := &engine.FilterIHReply{
-		// MissingObjects: []string{},
+		MissingObjects: []string{},
 		MissingIndexes: map[string][]string{},
 		BrokenIndexes:  map[string][]string{},
 		MissingFilters: map[string][]string{},
@@ -293,7 +473,7 @@ func testV1FIdxGetThresholdsIndexesHealth(t *testing.T) {
 	}
 	//as we removed the object, the index specified is removed too, so the health of the indexes is fine
 	expRPly = &engine.FilterIHReply{
-		// MissingObjects: []string{},
+		MissingObjects: []string{},
 		MissingIndexes: map[string][]string{},
 		BrokenIndexes:  map[string][]string{},
 		MissingFilters: map[string][]string{},
@@ -357,7 +537,7 @@ func testV1FIdxGetResourcesIndexesHealth(t *testing.T) {
 
 	// all indexes are set and points to their objects correctly
 	expRPly := &engine.FilterIHReply{
-		// MissingObjects: []string{},
+		MissingObjects: []string{},
 		MissingIndexes: map[string][]string{},
 		BrokenIndexes:  map[string][]string{},
 		MissingFilters: map[string][]string{},
@@ -451,7 +631,7 @@ func testV1FIdxGetStatsIndexesHealth(t *testing.T) {
 
 	// all indexes are set and points to their objects correctly
 	expRPly := &engine.FilterIHReply{
-		// MissingObjects: []string{},
+		MissingObjects: []string{},
 		MissingIndexes: map[string][]string{},
 		BrokenIndexes:  map[string][]string{},
 		MissingFilters: map[string][]string{},
@@ -534,7 +714,7 @@ func testV1FIdxGetSupplierIndexesHealth(t *testing.T) {
 
 	// all indexes are set and points to their objects correctly
 	expRPly := &engine.FilterIHReply{
-		// MissingObjects: []string{},
+		MissingObjects: []string{},
 		MissingIndexes: map[string][]string{},
 		BrokenIndexes:  map[string][]string{},
 		MissingFilters: map[string][]string{},
@@ -635,7 +815,7 @@ func testV1FIdxGetChargersIndexesHealth(t *testing.T) {
 
 	// all indexes are set and points to their objects correctly
 	expRPly := &engine.FilterIHReply{
-		// MissingObjects: []string{},
+		MissingObjects: []string{},
 		MissingIndexes: map[string][]string{},
 		BrokenIndexes:  map[string][]string{},
 		MissingFilters: map[string][]string{},
@@ -767,7 +947,7 @@ func testV1FIdxGetAttributesIndexesHealth(t *testing.T) {
 
 	//as we removed the object, the index specified is removed too, so the health of the indexes is fine
 	expRPly := &engine.FilterIHReply{
-		// MissingObjects: []string{},
+		MissingObjects: []string{},
 		MissingIndexes: map[string][]string{},
 		BrokenIndexes:  map[string][]string{},
 		MissingFilters: map[string][]string{},
@@ -821,7 +1001,7 @@ func testV1FIdxHGetDispatchersIndexesHealth(t *testing.T) {
 
 	// all indexes are set and points to their objects correctly
 	expRPly := &engine.FilterIHReply{
-		// MissingObjects: []string{},
+		MissingObjects: []string{},
 		MissingIndexes: map[string][]string{},
 		BrokenIndexes:  map[string][]string{},
 		MissingFilters: map[string][]string{},
