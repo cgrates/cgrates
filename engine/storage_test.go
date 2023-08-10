716ea4323465ba47a536a337aa5b9137a0c7fd5f
NikolasPetriti <nikolas.petriti@itsyscom.com>
2023-08-10 14:17:15 +0200

    Add tests for msgpack encoding/decoding for all basic go types that we are using in 1.0

--- engine/storage_test.go
+++ engine/storage_test.go
@@ -18,6 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 package engine
 
 import (
+	"bytes"
+	"reflect"
 	"testing"
 	"time"
 
@@ -76,4 +78,129 @@ func TestMsgpackTime(t *testing.T) {
 func TestStoreInterfaces(t *testing.T) {
 	rds := new(RedisStorage)
 	var _ DataDB = rds
+}
+
+func TestStorageDecodeCodecMsgpackMarshaler(t *testing.T) {
+	type stc struct {
+		Name string
+	}
+
+	var s stc
+	mp := make(map[string]any)
+	var slc []string
+	var slcB []byte
+	var arr *[1]int
+	var nm int
+	var fl float64
+	var str string
+	var bl bool
+	var td time.Duration
+
+	tests := []struct {
+		name     string
+		expBytes []byte
+		val      any
+		decode   any
+		rng      bool
+	}{
+		{
+			name:     "map",
+			expBytes: []byte{129, 164, 107, 101, 121, 49, 166, 118, 97, 108, 117, 101, 49},
+			val:      map[string]any{"key1": "value1"},
+			decode:   mp,
+			rng:      true,
+		},
+		{
+			name:     "int",
+			expBytes: []byte{1},
+			val:      1,
+			decode:   nm,
+			rng:      false,
+		},
+		{
+			name:     "string",
+			expBytes: []byte{164, 116, 101, 115, 116},
+			val:      "test",
+			decode:   str,
+			rng:      false,
+		},
+		{
+			name:     "float64",
+			expBytes: []byte{203, 63, 248, 0, 0, 0, 0, 0, 0},
+			val:      1.5,
+			decode:   fl,
+			rng:      false,
+		},
+		{
+			name:     "boolean",
+			expBytes: []byte{195},
+			val:      true,
+			decode:   bl,
+			rng:      false,
+		},
+		{
+			name:     "slice",
+			expBytes: []byte{145, 164, 118, 97, 108, 49},
+			val:      []string{"val1"},
+			decode:   slc,
+			rng:      true,
+		},
+		{
+			name:     "array",
+			expBytes: []byte{145, 1},
+			val:      &[1]int{1},
+			decode:   arr,
+			rng:      true,
+		},
+		{
+			name:     "struct",
+			expBytes: []byte{129, 164, 78, 97, 109, 101, 164, 116, 101, 115, 116},
+			val:      stc{"test"},
+			decode:   s,
+			rng:      true,
+		},
+		{
+			name:     "time duration",
+			expBytes: []byte{210, 59, 154, 202, 0},
+			val:      1 * time.Second,
+			decode:   td,
+			rng:      false,
+		},
+		{
+			name:     "slice of bytes",
+			expBytes: []byte{162, 5, 8},
+			val:      []byte{5, 8},
+			decode:   slcB,
+			rng:      true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ms := utils.NewCodecMsgpackMarshaler()
+
+			b, err := ms.Marshal(tt.val)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !bytes.Equal(b, tt.expBytes) {
+				t.Fatalf("expected: %+v,\nreceived: %+v", tt.expBytes, b)
+			}
+
+			err = ms.Unmarshal(b, &tt.decode)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if tt.rng {
+				if !reflect.DeepEqual(tt.decode, tt.val) {
+					t.Errorf("expected %v, received %v", tt.val, tt.decode)
+				}
+			} else {
+				if tt.decode != tt.val {
+					t.Errorf("expected %v, received %v", tt.val, tt.decode)
+				}
+			}
+		})
+	}
 }
