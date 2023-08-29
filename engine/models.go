61a781675c4bfdbfcd0fde6abafa35fd61e51db2
NikolasPetriti <nikolas.petriti@itsyscom.com>
2023-08-29 08:09:45 +0200

    Fix comment in models

--- engine/models.go
+++ engine/models.go
@@ -2,7 +2,7 @@
 Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
 Copyright (C) ITsysCOM GmbH
 
-This program is free software:".*"y
+This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
