46e39770a14c5e8df6e2e12d3d74ffbe817a42c4
ionutboangiu <ionut.boangiu@itsyscom.com>
2023-10-26 10:41:30 -0400

    Replace deprecated Publish method with PublishWithContext

--- ees/amqp.go
+++ ees/amqp.go
@@ -145,7 +145,8 @@ func (pstr *AMQPee) ExportEvent(_ *context.Context, content, _ any) (err error) 
 		pstr.reqs.done()
 		return utils.ErrDisconnected
 	}
-	err = pstr.postChan.Publish(
+	err = pstr.postChan.PublishWithContext(
+		context.TODO(),
 		pstr.exchange,   // exchange
 		pstr.routingKey, // routing key
 		false,           // mandatory
