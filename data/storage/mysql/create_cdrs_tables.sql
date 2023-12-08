9e56246ba1759fbeb45b96b229ef56035c30691c
ionutboangiu <ionut.boangiu@itsyscom.com>
2023-12-08 11:37:24 -0500

    Update storage sql scripts for setting up cdr tables

--- data/storage/mysql/create_cdrs_tables.sql
+++ data/storage/mysql/create_cdrs_tables.sql
@@ -4,47 +4,14 @@
 
 DROP TABLE IF EXISTS cdrs;
 CREATE TABLE cdrs (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  run_id  varchar(64) NOT NULL,
-  origin_host varchar(64) NOT NULL,
-  source varchar(64) NOT NULL,
-  origin_id varchar(128) NOT NULL,
-  tor  varchar(16) NOT NULL,
-  request_type varchar(24) NOT NULL,
-  tenant varchar(64) NOT NULL,
-  category varchar(64) NOT NULL,
-  account varchar(128) NOT NULL,
-  subject varchar(128) NOT NULL,
-  destination varchar(128) NOT NULL,
-  setup_time datetime NOT NULL,
-  answer_time datetime NULL,
-  `usage` BIGINT NOT NULL,
-  extra_fields text NOT NULL,
-  cost_source varchar(64) NOT NULL,
-  cost DECIMAL(20,4) NOT NULL,
-  cost_details MEDIUMTEXT,
-  extra_info text,
-  created_at TIMESTAMP NULL,
-  updated_at TIMESTAMP NULL,
-  deleted_at TIMESTAMP NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY cdrrun ( run_id)
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `tenant` VARCHAR(40) NOT NULL,
+ `opts` JSON NOT NULL,
+ `event` JSON NOT NULL,
+ `created_at` TIMESTAMP NULL,
+ `updated_at` TIMESTAMP NULL,
+ `deleted_at` TIMESTAMP NULL,
+  PRIMARY KEY (`id`)
 );
-
-DROP TABLE IF EXISTS session_costs;
-CREATE TABLE session_costs (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  run_id  varchar(64) NOT NULL,
-  origin_host varchar(64) NOT NULL,
-  origin_id varchar(128) NOT NULL,
-  cost_source varchar(64) NOT NULL,
-  `usage` BIGINT NOT NULL,
-  cost_details MEDIUMTEXT,
-  created_at TIMESTAMP NULL,
-  deleted_at TIMESTAMP NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY costid ( run_id),
-  KEY origin_idx (origin_host, origin_id),
-  KEY run_origin_idx (run_id, origin_id),
-  KEY deleted_at_idx (deleted_at)
-);
+ALTER TABLE cdrs ADD COLUMN cdrid VARCHAR(40) GENERATED ALWAYS AS ( JSON_VALUE(opts, '$."*cdrID"') );
+CREATE UNIQUE INDEX opts_cdrid_idx ON cdrs (cdrid);
