9b95bf230d4450becd7913dde3744d087c62463f
Bas Couwenberg <sebastic@xs4all.nl>
2024-01-26 13:23:15 +0100

    Pass commit date and hash to -ldflags separately.

--- cores/core_test.go
+++ cores/core_test.go
@@ -93,11 +93,12 @@ func TestCoreServiceStatus(t *testing.T) {
 	if !reflect.DeepEqual(expected[utils.NodeID], reply[utils.NodeID]) {
 		t.Errorf("Expected %+v, received %+v", utils.ToJSON(expected[utils.NodeID]), utils.ToJSON(reply[utils.NodeID]))
 	}
-	utils.GitLastLog = `Date: wrong format
-`
-	if err := cores.V1Status(nil, nil, &reply); err != nil {
+	utils.GitCommitDate = "wrong format"
+	utils.GitCommitHash = "73014DAA0C1D7EDCB532D5FE600B8A20D588CDF8"
+	if err := cores.V1Status(context.Background(), args, &reply); err != nil {
 		t.Error(err)
 	}
 
-	utils.GitLastLog = ""
+	utils.GitCommitDate = ""
+	utils.GitCommitHash = ""
 }
