cd2d0167d0c6661149a290c3d5ba32a8716b356a
ionutboangiu <ionut.boangiu@itsyscom.com>
2024-04-10 10:34:38 +0300

    Lower the min size we expect from mem/cpuprof files

--- apier/v1/resourcesv1_it_test.go
+++ apier/v1/resourcesv1_it_test.go
@@ -1179,7 +1179,7 @@ func testV1ResourceStopCPUProfiling(t *testing.T) {
 	size, err := file.Stat()
 	if err != nil {
 		t.Error(err)
-	} else if size.Size() < int64(415) {
+	} else if size.Size() < int64(300) {
 		t.Errorf("Size of CPUProfile %v is lower that expected", size.Size())
 	}
 	//after we checked that CPUProfile was made successfully, can delete it
