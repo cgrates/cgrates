07390d8624955235932c1f99cdc1c5030bbf6729
armirveliaj <armir.veliaj@itsyscom.com>
2024-08-21 10:23:11 -0400

    Add new unit tests on ees and sessions

---
+++ ees/sqs_test.go
@@ -0,0 +1,52 @@
+/*
+Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
+Copyright (C) ITsysCOM GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+package ees
+
+import (
+	"testing"
+
+	"github.com/cgrates/cgrates/config"
+	"github.com/cgrates/cgrates/utils"
+)
+
+func TestGetMetricsT(t *testing.T) {
+	metrics := &utils.SafeMapStorage{}
+	pstr := &SQSee{dc: metrics}
+	result := pstr.GetMetrics()
+	if result != metrics {
+		t.Errorf("Expected %v, but got %v", metrics, result)
+	}
+}
+
+func TestSqsClose(t *testing.T) {
+	pstr := &SQSee{}
+	err := pstr.Close()
+	if err != nil {
+		t.Errorf("Expected nil, but got %v", err)
+	}
+}
+
+func TestSqsCfg(t *testing.T) {
+	testCfg := &config.EventExporterCfg{}
+	pstr := &SQSee{cfg: testCfg}
+	result := pstr.Cfg()
+	if result != testCfg {
+		t.Errorf("Expected %v, but got %v", testCfg, result)
+	}
+}
