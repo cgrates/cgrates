35ae6ebd2720d01bd34db71b527686bf3927c4ef
armirveliaj <armir.veliaj@itsyscom.com>
2024-09-03 08:31:53 -0400

    Add new unit tests on apier/v1

---
+++ apier/v1/loaders_test.go
@@ -0,0 +1,65 @@
+/*
+Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
+Copyright (C) ITsysCOM GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+package v1
+
+import (
+	"testing"
+
+	"github.com/cgrates/birpc/context"
+	"github.com/cgrates/cgrates/loaders"
+	"github.com/cgrates/cgrates/utils"
+)
+
+func TestLoaderSv1Ping(t *testing.T) {
+	rsv1 := &LoaderSv1{}
+	ctx := context.Background()
+	var reply string
+	ign := &utils.CGREvent{}
+	err := rsv1.Ping(ctx, ign, &reply)
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+	if reply != utils.Pong {
+		t.Errorf("Expected reply to be '%s', but got '%s'", utils.Pong, reply)
+	}
+}
+
+func TestNewLoaderSv1(t *testing.T) {
+	mockLoaderService := &loaders.LoaderService{}
+	result := NewLoaderSv1(mockLoaderService)
+	if result == nil {
+		t.Errorf("Expected non-nil result, but got nil")
+	}
+	if result.ldrS != mockLoaderService {
+		t.Errorf("Expected ldrS to be %v, but got %v", mockLoaderService, result.ldrS)
+	}
+}
+
+func TestLoaderSv1Call(t *testing.T) {
+	ldrSv1 := &LoaderSv1{}
+	ctx := context.Background()
+	serviceMethod := "testMethod"
+	args := "testArgs"
+	var reply string
+	err := ldrSv1.Call(ctx, serviceMethod, args, &reply)
+	if err == nil {
+		t.Errorf("Expected error, but got: %v", err)
+	}
+
+}
