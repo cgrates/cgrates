f0598b050b54428329e53788b2f232b97688c762
gezimbll <gezim.blliku@itsyscom.com>
2024-10-02 12:01:42 +0200

    added unit test for Trend.cleanup && updated TestTrendSchedule

--- engine/libtrends_test.go
+++ engine/libtrends_test.go
@@ -90,3 +90,82 @@ func TestTrendGetTrendLabel(t *testing.T) {
 		t.Errorf("Expecting: <%q> got <%q>", expct, lbl)
 	}
 }
+
+func TestTrendCleanUp(t *testing.T) {
+	tests := []struct {
+		name     string
+		ttl      time.Duration
+		qLength  int
+		runtimes int
+		want     bool
+	}{
+		{
+			name:     "No Clean up",
+			ttl:      2 * time.Minute,
+			qLength:  10,
+			want:     false,
+			runtimes: 5,
+		},
+		{
+			name:     "Clean up with TTL",
+			ttl:      1 * time.Second,
+			qLength:  10,
+			want:     true,
+			runtimes: 3,
+		},
+		{
+			name:     "No clean up",
+			ttl:      1 * time.Second,
+			qLength:  10,
+			want:     false,
+			runtimes: 3,
+		},
+		{
+			name:     "Clean up with queue length",
+			ttl:      1 * time.Second,
+			qLength:  2,
+			want:     true,
+			runtimes: 2,
+		},
+		{
+			name:     "No clean up with negative ttl",
+			ttl:      -1,
+			qLength:  2,
+			want:     false,
+			runtimes: 2,
+		},
+	}
+	now := time.Now()
+	t1 := now.Add(-time.Minute)
+	t2 := now.Add(-time.Second)
+	t3 := now.Add(time.Second)
+	t4 := now.Add(2 * time.Second)
+	t5 := now.Add(time.Minute)
+	trend := &Trend{
+		tMux:   new(sync.RWMutex),
+		Tenant: "cgrates.org",
+		ID:     "TestTrendCleanUp",
+
+		RunTimes: []time.Time{t1, t2, t3, t4, t5},
+		Metrics: map[time.Time]map[string]*MetricWithTrend{
+			t1: {utils.MetaACC: {utils.MetaACC, 10.1, -1.0, utils.NotAvailable}, utils.MetaTCC: {utils.MetaTCC, 10.1, -1.0, utils.NotAvailable}},
+			t2: {utils.MetaACC: {utils.MetaACC, 15.1, 4.0, utils.MetaPositive}, utils.MetaTCC: {utils.MetaTCC, 25.1, 15.1, utils.MetaPositive}},
+			t3: {utils.MetaACC: {utils.MetaACC, 12.1, -1.0, utils.NotAvailable}, utils.MetaTCC: {utils.MetaTCC, 34, -1.0, utils.NotAvailable}},
+			t4: {utils.MetaACC: {utils.MetaACC, 19.1, 4.0, utils.MetaPositive}, utils.MetaTCC: {utils.MetaTCC, 48, 15.1, utils.MetaPositive}},
+			t5: {utils.MetaACC: {utils.MetaACC, 117.1, -1.0, utils.NotAvailable}, utils.MetaTCC: {utils.MetaTCC, 56, -1.0, utils.NotAvailable}},
+		}}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			altered := trend.cleanup(tt.ttl, tt.qLength)
+			if altered != tt.want {
+				t.Errorf("cleanup() = %v, want %v", altered, tt.want)
+				return
+			}
+			if len(trend.RunTimes) != tt.runtimes {
+				t.Errorf("runTimes length = %v, want %v", len(trend.RunTimes), tt.runtimes)
+			}
+		})
+
+	}
+}
