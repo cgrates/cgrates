001742c4a0cd56332eddfd3fe671bc46fe23a76b
Craig Askings <craig.askings@telair.com.au>
2026-02-09 14:36:14 +1000

    Add Craig Askings to CONTRIBUTORS.md and update database privileges in create_db_with_users.sh

--- data/storage/postgres/create_db_with_users.sh
+++ data/storage/postgres/create_db_with_users.sh
@@ -8,3 +8,4 @@ sudo -u postgres dropdb -e cgrates
 sudo -u postgres dropuser -e cgrates
 sudo -u postgres psql  -c "CREATE USER cgrates password 'CGRateS.org';"
 sudo -u postgres createdb -e -O cgrates cgrates
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE cgrates to cgrates;"
