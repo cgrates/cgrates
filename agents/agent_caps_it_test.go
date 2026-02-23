6f49024055179ad73b24bfebd269d40d10454388
ionutboangiu <ionut.boangiu@itsyscom.com>
2026-02-23 21:26:54 +0200

    agents: slightly improve caps integration test

--- agents/agent_caps_it_test.go
+++ agents/agent_caps_it_test.go
@@ -22,6 +22,9 @@ package agents
 
 import (
 	"fmt"
+	"io"
+	"net"
+	"net/http"
 	"sync"
 	"testing"
 	"time"
@@ -35,9 +38,10 @@ import (
 	"github.com/cgrates/go-diameter/diam/avp"
 	"github.com/cgrates/go-diameter/diam/datatype"
 	"github.com/cgrates/go-diameter/diam/dict"
+	"github.com/cgrates/sipingo"
 )
 
-func TestDiameterAgentCapsIT(t *testing.T) {
+func TestAgentCapsIT(t *testing.T) {
 	var dbCfg engine.DBCfg
 	switch *utils.DBType {
 	case utils.MetaInternal:
@@ -71,6 +75,23 @@ func TestDiameterAgentCapsIT(t *testing.T) {
 "diameter_agent": {
 	"enabled": true,
 	"synced_conn_requests": true
+},
+"http_agent": [
+	{
+		"id": "caps_test",
+		"url": "/caps_test",
+		"sessions_conns": ["*internal"],
+		"request_payload": "*url",
+		"reply_payload": "*xml",
+		"request_processors": []
+	}
+],
+"sip_agent": {
+	"enabled": true,
+	"listen": "127.0.0.1:5099",
+	"listen_net": "udp",
+	"sessions_conns": ["*internal"],
+	"request_processors": []
 }
 }`
 
@@ -92,6 +113,7 @@ func TestDiameterAgentCapsIT(t *testing.T) {
 
 	reqIdx := 0
 	sendCCR := func(t *testing.T, replyTimeout time.Duration, wg *sync.WaitGroup, wantResultCode string) {
+		t.Helper()
 		if wg != nil {
 			defer wg.Done()
 		}
@@ -128,6 +150,7 @@ func TestDiameterAgentCapsIT(t *testing.T) {
 		resultCode, err := diamAVPAsString(avps[0])
 		if err != nil {
 			t.Error(err)
+			return
 		}
 		if resultCode != wantResultCode {
 			t.Errorf("Result-Code=%s, want %s", resultCode, wantResultCode)
@@ -152,6 +175,93 @@ func TestDiameterAgentCapsIT(t *testing.T) {
 	<-doneCh
 
 	// TODO: Check caps functionality with async diameter requests.
+
+	t.Run("HTTPAgent", func(t *testing.T) {
+		httpURL := fmt.Sprintf("http://%s/caps_test", cfg.ListenCfg().HTTPListen)
+
+		// There is currently no traffic. Expecting 200 OK because
+		// there are no request processors enabled (empty reply).
+		sendHTTPReq(t, httpURL, http.StatusOK)
+
+		// Caps limit is 2, therefore expecting the same result.
+		doneCh := simulateCapsTraffic(t, client, 1, *cfg.CoreSCfg())
+		time.Sleep(time.Millisecond)
+		sendHTTPReq(t, httpURL, http.StatusOK)
+		<-doneCh
+
+		// With caps limit reached, 429 Too Many Requests is expected.
+		doneCh = simulateCapsTraffic(t, client, 2, *cfg.CoreSCfg())
+		time.Sleep(time.Millisecond)
+		sendHTTPReq(t, httpURL, http.StatusTooManyRequests)
+		<-doneCh
+	})
+
+	t.Run("SIPAgent", func(t *testing.T) {
+		sipAddr := cfg.SIPAgentCfg().Listen
+
+		// There is currently no traffic. Expecting 500 Internal Server
+		// Error because there are no request processors enabled.
+		sendSIPReq(t, sipAddr, "SIP/2.0 500 Internal Server Error")
+
+		// Caps limit is 2, therefore expecting the same result.
+		doneCh := simulateCapsTraffic(t, client, 1, *cfg.CoreSCfg())
+		time.Sleep(time.Millisecond)
+		sendSIPReq(t, sipAddr, "SIP/2.0 500 Internal Server Error")
+		<-doneCh
+
+		// With caps limit reached, 503 Service Unavailable is expected.
+		doneCh = simulateCapsTraffic(t, client, 2, *cfg.CoreSCfg())
+		time.Sleep(time.Millisecond)
+		sendSIPReq(t, sipAddr, "SIP/2.0 503 Service Unavailable")
+		<-doneCh
+	})
+}
+
+func sendHTTPReq(t *testing.T, url string, wantStatus int) {
+	t.Helper()
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Fatal(err)
+	}
+	io.Copy(io.Discard, resp.Body)
+	resp.Body.Close()
+	if resp.StatusCode != wantStatus {
+		t.Errorf("HTTP status=%d, want %d", resp.StatusCode, wantStatus)
+	}
+}
+
+func sendSIPReq(t *testing.T, addr, wantStatus string) {
+	t.Helper()
+	conn, err := net.Dial("udp", addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	invite := "INVITE sip:1002@cgrates.org SIP/2.0\r\n" +
+		"Call-ID: caps-test-" + fmt.Sprint(time.Now().UnixNano()) + "\r\n" +
+		"CSeq: 1 INVITE\r\n" +
+		"From: \"1001\" <sip:1001@cgrates.org>;tag=caps1\r\n" +
+		"To: <sip:1002@cgrates.org>\r\n" +
+		"Via: SIP/2.0/UDP 127.0.0.1:9999;branch=z9hG4bK-caps-test\r\n" +
+		"Max-Forwards: 70\r\n" +
+		"Content-Length: 0\r\n\r\n"
+	if _, err = conn.Write([]byte(invite)); err != nil {
+		t.Fatal(err)
+	}
+	buf := make([]byte, bufferSize)
+	conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+	n, err := conn.Read(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	received, err := sipingo.NewMessage(string(buf[:n]))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if received[requestHeader] != wantStatus {
+		t.Errorf("SIP status=%q, want %q", received[requestHeader], wantStatus)
+	}
 }
 
 func simulateCapsTraffic(t *testing.T, client *birpc.Client, amount int, coresCfg config.CoreSCfg) <-chan struct{} {
