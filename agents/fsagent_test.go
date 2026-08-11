ccfcf2048efcb7c6ee425d9c4b5704d0f54910b2
ionutboangiu <ionut.boangiu@itsyscom.com>
2026-08-12 00:56:57 +0300

    fsagent: read ProcessEvent usage by run ID
    ProcessEvent can only return one usage limit per run ID, so use the
    shortest one.

--- agents/fsagent_test.go
+++ agents/fsagent_test.go
@@ -5,10 +5,28 @@ package agents
 
 import (
 	"testing"
+	"time"
 
 	"github.com/cgrates/cgrates/sessions"
+	"github.com/cgrates/cgrates/utils"
 )
 
 func TestFAsSessionSClientIface(t *testing.T) {
 	_ = sessions.BiRPCClient(new(FSsessions))
+}
+
+func TestMinAccountUsageProcessEventReply(t *testing.T) {
+	rply := sessions.V1ProcessEventReply{
+		AccountsUsage: map[string]time.Duration{
+			utils.MetaPrimary: 2 * time.Minute,
+			utils.MetaDefault: time.Minute,
+		},
+	}
+	got, found := minAccountUsage(&utils.DataNode{
+		Type: utils.NMMapType,
+		Map:  rply.AsNavigableMap(),
+	})
+	if !found || got != time.Minute {
+		t.Fatalf("minAccountUsage() = %v, %t, want %v, true", got, found, time.Minute)
+	}
 }
